6510421e018c2aa8edcb039bb9eea18b01bb9bec
Hiroshi Yamauchi <yamauchi@google.com>
2013-12-17 11:54:23 -0800

    Fix an array copy benchmark regression.
    Add different page release modes to rosalloc.
    
    Bug: 12064551
    Change-Id: Ib837bbd1a2757741a4e2743e0a1272bf46a30252

--- runtime/gc/space/malloc_space.h
+++ runtime/gc/space/malloc_space.h
@@ -128,7 +128,7 @@ class MallocSpace : public ContinuousMemMapAllocSpace {
                                       byte* begin, byte* end, byte* limit, size_t growth_limit) = 0;
 
   // Turn ourself into a zygote space and return a new alloc space which has our unused memory.
-  MallocSpace* CreateZygoteSpace(const char* alloc_space_name);
+  MallocSpace* CreateZygoteSpace(const char* alloc_space_name, bool low_memory_mode);
 
   virtual uint64_t GetBytesAllocated() = 0;
   virtual uint64_t GetObjectsAllocated() = 0;
@@ -154,7 +154,8 @@ class MallocSpace : public ContinuousMemMapAllocSpace {
   static MemMap* CreateMemMap(const std::string& name, size_t starting_size, size_t* initial_size,
                               size_t* growth_limit, size_t* capacity, byte* requested_begin);
 
-  virtual void* CreateAllocator(void* base, size_t morecore_start, size_t initial_size) = 0;
+  virtual void* CreateAllocator(void* base, size_t morecore_start, size_t initial_size,
+                                bool low_memory_mode) = 0;
 
   void RegisterRecentFree(mirror::Object* ptr) EXCLUSIVE_LOCKS_REQUIRED(lock_);
 
