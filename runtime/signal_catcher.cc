e3f88c561d0d271b4de0f3600ead1f1b2e0f5d86
Mathieu Chartier <mathieuc@google.com>
2014-01-15 13:30:54 -0800

    Change thread to kNative after running checkpoints in signal catcher.
    Previously, we were setting it to native before running any pending
    checkpoints. This caused an issue with profiler checkpoints since
    these use a ScopedObjectAccess. Since the SIGQUIT handler has
    exclusive access to the mutator lock, this meant that we deadlocked
    when we tried to gain shared access to the mutator lock in the
    ScopedThreadStateChange.
    
    Bug: 12490061
    Change-Id: I5f922693a4bf5d18cf9d22f1f28a7a04b6bfc077

--- runtime/signal_catcher.cc
+++ runtime/signal_catcher.cc
@@ -144,10 +144,10 @@ void SignalCatcher::HandleSigQuit() {
     }
   }
   os << "----- end " << getpid() << " -----\n";
-  CHECK_EQ(self->SetStateUnsafe(old_state), kRunnable);
   if (self->ReadFlag(kCheckpointRequest)) {
     self->RunCheckpointFunction();
   }
+  CHECK_EQ(self->SetStateUnsafe(old_state), kRunnable);
   self->EndAssertNoThreadSuspension(old_cause);
   thread_list->ResumeAll();
 
