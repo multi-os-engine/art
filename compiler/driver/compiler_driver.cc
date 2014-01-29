793b3045704e9045abb3fd2a5025b431560a602e
Vladimir Marko <vmarko@google.com>
2014-01-29 10:31:55 +0000

    Annotate used fields.
    Annotate all fields used by a method early during the
    compilation, check acces rights and record field offset,
    volatility, etc. Use these annotations when generating code
    for IGET/IPUT/SGET/SPUT instructions.
    
    Change-Id: I4bbf5cca4fecf53c9bf9c93ac1793e2f40c16b5f

--- compiler/driver/compiler_driver.cc
+++ compiler/driver/compiler_driver.cc
@@ -33,6 +33,7 @@
 #include "jni_internal.h"
 #include "object_utils.h"
 #include "runtime.h"
+#include "dex/mir_annotations.h"
 #include "gc/accounting/card_table-inl.h"
 #include "gc/accounting/heap_bitmap.h"
 #include "gc/space/space.h"
@@ -184,6 +185,12 @@ class AOTCompilationStats {
     unresolved_instance_fields_++;
   }
 
+  void ResolvedInstanceFields(size_t resolved, size_t total) {
+    STATS_LOCK();
+    resolved_instance_fields_ += resolved;
+    unresolved_instance_fields_ += total - resolved;
+  }
+
   void ResolvedLocalStaticField() {
     STATS_LOCK();
     resolved_local_static_fields_++;
@@ -197,6 +204,13 @@ class AOTCompilationStats {
   void UnresolvedStaticField() {
     STATS_LOCK();
     unresolved_static_fields_++;
+  }
+
+  void ResolvedStaticFields(size_t local, size_t resolved, size_t total) {
+    STATS_LOCK();
+    resolved_local_static_fields_ += local;
+    resolved_static_fields_ += resolved;
+    unresolved_static_fields_ += total - local - resolved;
   }
 
   // Indicate that type information from the verifier led to devirtualization.
@@ -1056,6 +1070,64 @@ bool CompilerDriver::ComputeInstanceFieldInfo(uint32_t field_idx, const DexCompi
   return false;  // Incomplete knowledge needs slow path.
 }
 
+void CompilerDriver::ComputeInstanceFieldAnnotations(const DexCompilationUnit* mUnit,
+                                                     IFieldAnnotation* annotations, size_t count) {
+  if (kIsDebugBuild) {
+    CHECK(annotations != nullptr);
+    CHECK_NE(count, 0u);
+    for (auto it = annotations, end = annotations + count; it != end; ++it) {
+      IFieldAnnotation unresolved = IFieldAnnotation::Unresolved(it->field_idx);
+      DCHECK_EQ(memcmp(&unresolved, &*it, sizeof(*it)), 0);
+    }
+  }
+
+  const DexFile* dex_file = mUnit->GetDexFile();
+  ClassLinker* class_linker = mUnit->GetClassLinker();
+  uint32_t referrer_class_idx = dex_file->GetMethodId(mUnit->GetDexMethodIndex()).class_idx_;
+
+  // We're going to resolve fields and check access in a tight loop. It's better to hold
+  // the lock and needed references once than re-acquiring them again and again.
+  ScopedObjectAccess soa(Thread::Current());
+  SirtRef<mirror::DexCache> dex_cache(soa.Self(), class_linker->FindDexCache(*dex_file));
+  SirtRef<mirror::ClassLoader> class_loader(
+      soa.Self(), soa.Decode<mirror::ClassLoader*>(mUnit->GetClassLoader()));
+  SirtRef<mirror::Class> referrer_class(soa.Self(),
+      class_linker->ResolveType(*dex_file, referrer_class_idx, dex_cache, class_loader));
+  if (UNLIKELY(referrer_class.get() == nullptr)) {
+    // Clean up any exception left by type resolution
+    DCHECK(soa.Self()->IsExceptionPending());
+    soa.Self()->ClearException();
+    stats_->ResolvedInstanceFields(0u, count);
+    return;
+  }
+
+  size_t resolved = 0u;
+  for (auto it = annotations, end = annotations + count; it != end; ++it) {
+    uint32_t field_idx = it->field_idx;
+    mirror::ArtField* resolved_field =
+        class_linker->ResolveField(*dex_file, field_idx, dex_cache, class_loader, false);
+    if (UNLIKELY(resolved_field == nullptr)) {
+      DCHECK(soa.Self()->IsExceptionPending());
+      soa.Self()->ClearException();
+      continue;
+    }
+    DCHECK(!soa.Self()->IsExceptionPending());
+    if (UNLIKELY(resolved_field->IsStatic())) {
+      continue;
+    }
+    mirror::Class* fields_class = resolved_field->GetDeclaringClass();
+    if (referrer_class->CanAccessResolvedField(fields_class, resolved_field,
+                                               dex_cache.get(), field_idx)) {
+      it->fast_get = 1u;
+      it->is_volatile = resolved_field->IsVolatile() ? 1u : 0u;
+      it->field_offset = resolved_field->GetOffset().Int32Value();
+      it->fast_put = (!resolved_field->IsFinal() || fields_class == referrer_class.get()) ? 1u : 0u;
+      ++resolved;
+    }
+  }
+  stats_->ResolvedInstanceFields(resolved, count);
+}
+
 bool CompilerDriver::ComputeStaticFieldInfo(uint32_t field_idx, const DexCompilationUnit* mUnit,
                                             bool is_put, int* field_offset, int* storage_index,
                                             bool* is_referrers_class, bool* is_volatile,
@@ -1131,6 +1203,103 @@ bool CompilerDriver::ComputeStaticFieldInfo(uint32_t field_idx, const DexCompila
   }
   stats_->UnresolvedStaticField();
   return false;  // Incomplete knowledge needs slow path.
+}
+
+void CompilerDriver::ComputeStaticFieldAnnotations(const DexCompilationUnit* mUnit,
+                                                   SFieldAnnotation* annotations, size_t count) {
+  if (kIsDebugBuild) {
+    CHECK(annotations != nullptr);
+    CHECK_NE(count, 0u);
+    for (auto it = annotations, end = annotations + count; it != end; ++it) {
+      SFieldAnnotation unresolved = SFieldAnnotation::Unresolved(it->field_idx);
+      DCHECK_EQ(memcmp(&unresolved, &*it, sizeof(*it)), 0);
+    }
+  }
+
+  const DexFile* dex_file = mUnit->GetDexFile();
+  ClassLinker* class_linker = mUnit->GetClassLinker();
+  uint32_t referrer_class_idx = dex_file->GetMethodId(mUnit->GetDexMethodIndex()).class_idx_;
+
+  // We're going to resolve fields and check access in a tight loop. It's better to hold
+  // the lock and needed references once than re-acquiring them again and again.
+  ScopedObjectAccess soa(Thread::Current());
+  SirtRef<mirror::DexCache> dex_cache(soa.Self(), class_linker->FindDexCache(*dex_file));
+  SirtRef<mirror::ClassLoader> class_loader(
+      soa.Self(), soa.Decode<mirror::ClassLoader*>(mUnit->GetClassLoader()));
+  SirtRef<mirror::Class> referrer_class(soa.Self(),
+      class_linker->ResolveType(*dex_file, referrer_class_idx, dex_cache, class_loader));
+  if (UNLIKELY(referrer_class.get() == nullptr)) {
+    DCHECK(soa.Self()->IsExceptionPending());
+    soa.Self()->ClearException();
+    stats_->ResolvedStaticFields(0u, 0u, count);
+    return;
+  }
+
+  size_t local = 0u;
+  size_t resolved = 0u;
+  for (auto it = annotations, end = annotations + count; it != end; ++it) {
+    uint32_t field_idx = it->field_idx;
+    mirror::ArtField* resolved_field =
+        class_linker->ResolveField(*dex_file, field_idx, dex_cache, class_loader, false);
+    if (UNLIKELY(resolved_field == nullptr)) {
+      // Clean up the exception left by field resolution
+      DCHECK(soa.Self()->IsExceptionPending());
+      soa.Self()->ClearException();
+      continue;
+    }
+    DCHECK(!soa.Self()->IsExceptionPending());
+    if (UNLIKELY(!resolved_field->IsStatic())) {
+      continue;
+    }
+    mirror::Class* fields_class = resolved_field->GetDeclaringClass();
+    if (fields_class == referrer_class.get()) {
+      it->fast_get = 1u;
+      it->fast_put = 1u;
+      it->is_referrers_class = 1u;  // implies no worrying about class initialization
+      it->is_initialized = 1u;
+      it->is_volatile = resolved_field->IsVolatile() ? 1u : 0u;
+      it->field_offset = resolved_field->GetOffset().Int32Value();
+      // Leave the unused it->storage_index as -1 just as ComputeStaticFieldInfo() does.
+      ++local;
+      continue;
+    }
+    if (referrer_class->CanAccessResolvedField(fields_class, resolved_field,
+                                               dex_cache.get(), field_idx)) {
+      // We have the resolved field, we must make it into a index for the referrer
+      // in its static storage (which may fail if it doesn't have a slot for it)
+      // TODO: for images we can elide the static storage base null check
+      // if we know there's a non-null entry in the image
+      if (LIKELY(fields_class->GetDexCache() == dex_cache.get())) {
+        // common case where the dex cache of both the referrer and the field are the same,
+        // no need to search the dex file
+        it->storage_index = fields_class->GetDexTypeIndex();
+      } else {
+        // Search dex file for localized ssb index, may fail if field's class is a parent
+        // of the class mentioned in the dex file and there is no dex cache entry.
+        const DexFile::StringId* string_id =
+            dex_file->FindStringId(FieldHelper(resolved_field).GetDeclaringClassDescriptor());
+        if (string_id == nullptr) {
+          continue;
+        }
+        const DexFile::TypeId* type_id =
+           dex_file->FindTypeId(dex_file->GetIndexForStringId(*string_id));
+        if (type_id == nullptr) {
+          continue;
+        }
+        // medium path, needs check of static storage base being initialized
+        it->storage_index = dex_file->GetIndexForTypeId(*type_id);
+      }
+      it->fast_get = 1u;
+      it->fast_put = resolved_field->IsFinal() ? 0u : 1u;
+      DCHECK_EQ(it->is_referrers_class, 0u);
+      it->is_volatile = resolved_field->IsVolatile() ? 1u : 0u;
+      it->field_offset = resolved_field->GetOffset().Int32Value();
+      it->is_initialized = fields_class->IsInitialized() &&
+          CanAssumeTypeIsPresentInDexCache(*dex_file, it->storage_index);
+      ++resolved;
+    }
+  }
+  stats_->ResolvedStaticFields(local, resolved, count);
 }
 
 void CompilerDriver::GetCodeAndMethodForDirectCall(InvokeType* type, InvokeType sharp_type,
