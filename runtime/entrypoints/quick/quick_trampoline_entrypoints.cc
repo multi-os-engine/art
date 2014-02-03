1c48ef55ce505db577e04743820b8481f35c27b2
Vladimir Marko <vmarko@google.com>
2014-02-03 14:08:42 +0000

    Speed up method lookup in a different dex file.
    Use already known name and proto.
    
    Change-Id: I4505b81724bd6a0f3cf21ee6fed44774d38c4e15

--- runtime/entrypoints/quick/quick_trampoline_entrypoints.cc
+++ runtime/entrypoints/quick/quick_trampoline_entrypoints.cc
@@ -561,9 +561,12 @@ extern "C" const void* artQuickResolutionTrampoline(mirror::ArtMethod* called,
         caller->GetDexCacheResolvedMethods()->Set(called->GetDexMethodIndex(), called);
       } else {
         // Calling from one dex file to another, need to compute the method index appropriate to
-        // the caller's dex file.
+        // the caller's dex file. Since we get here only if called->IsRuntimeMethod(), we've got
+        // the correct dex_file and a hint dex_method_idx from above.
+        DCHECK(called->IsRuntimeMethod());
+        DCHECK(&MethodHelper(caller).GetDexFile() == dex_file);
         uint32_t method_index =
-            MethodHelper(called).FindDexMethodIndexInOtherDexFile(MethodHelper(caller).GetDexFile());
+            MethodHelper(called).FindDexMethodIndexInOtherDexFile(*dex_file, dex_method_idx);
         if (method_index != DexFile::kDexNoIndex) {
           caller->GetDexCacheResolvedMethods()->Set(method_index, called);
         }
