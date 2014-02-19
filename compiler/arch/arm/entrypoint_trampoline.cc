f805574b560e32c012ae10b2b0fca5f8e84b6607
Dave Allison <dallison@google.com>
2014-02-19 14:05:39 -0800

    Use trampolines for calls to helpers
    This is an ARM specific optimization to the compiler
    that uses trampoline islands to make calls to runtime
    helper functions.  The intention is to reduce the size
    of the generated code (by 2 bytes per call) without
    affecting performance.
    
    By default this is on when generating an OAT file.  It is
    off when compiling to memory.
    
    To switch this off in dex2oat, use the command line option:
    --no-helper-trampolines
    
    Enhances disassembler to print the trampoline entry on the
    BL instruction like this:
    
    0xb6a850c0: f7ffff9e  bl      -196 (0xb6a85000)  ; pTestSuspend
    
    Bug: 12607709
    Change-Id: I9202bdb7cf21252ad807bd48701f1f6ce8e3d0fe

---
+++ compiler/arch/arm/entrypoint_trampoline.cc
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "driver/compiler_driver.h"
+
+namespace art {
+  void CompilerDriver::BuildArmEntrypointTrampolineCall(uint32_t offset) {
+    // Thumb2 instruction encoding of:
+    // ldr pc,[r9,#offset]
+
+    uint32_t instruction = 0xf8d0f000 | (9 << 16) | (offset & 0xfff);
+    entrypoint_trampoline_code_.push_back((instruction >> 16) & 0xff);
+    entrypoint_trampoline_code_.push_back((instruction >> 24) & 0xff);
+    entrypoint_trampoline_code_.push_back((instruction >> 0) & 0xff);
+    entrypoint_trampoline_code_.push_back((instruction >> 8) & 0xff);
+  }
+}   // namespace art
