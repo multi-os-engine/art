5a0e64601b0ac5d81c5acec5eddda9f871f82ab4
Brian Carlstrom <bdc@google.com>
2014-02-20 13:50:05 -0800

    Try to make 036-finalizer less flaky by moving output to one thread
    Bug: 13108062
    Change-Id: I946044d007dda4efa85794e2361ecb2ce102be64

--- test/036-finalizer/src/FinalizerTest.java
+++ test/036-finalizer/src/FinalizerTest.java
@@ -20,18 +20,15 @@ public class FinalizerTest {
     public static FinalizerTest mNothing = new FinalizerTest("nothing");
     public static FinalizerTest mReborn = mNothing;
 
-    public String mMsg = "default";
+    public final String mMsg;
+    public boolean finalized = false;
 
     public FinalizerTest(String msg) {
         mMsg = msg;
     }
 
-    public String toString() {
-        return mMsg;
-    }
-
     protected void finalize() {
-        System.out.println("finalizer executed: " + mMsg);
+        finalized = true;
         mReborn = this;
     }
 }
