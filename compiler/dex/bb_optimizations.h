fa3b6f701bc92f69870385e005c6477bba2e110a
Vladimir Marko <vmarko@google.com>
2014-02-26 11:24:15 +0000

    Cache field lowering info in mir_graph.
    Change-Id: I9f9d76e3ae6c31e88bdf3f59820d31a625da020f

--- compiler/dex/bb_optimizations.h
+++ compiler/dex/bb_optimizations.h
@@ -23,6 +23,20 @@
 namespace art {
 
 /**
+ * @class AnnotateUsedFields
+ * @brief Cache the lowering info for fields used by IGET/IPUT/SGET/SPUT insns.
+ */
+class CacheFieldLoweringInfo : public Pass {
+ public:
+  CacheFieldLoweringInfo() : Pass("CacheFieldLoweringInfo", kNoNodes) {
+  }
+
+  void Start(CompilationUnit* cUnit) const {
+    cUnit->mir_graph->DoAnnotateUsedFields();
+  }
+};
+
+/**
  * @class CodeLayout
  * @brief Perform the code layout pass.
  */
