5a8eecdb3fc21ac8cc8a77caacf4d0d7a1144ac8
Nicolas Geoffray <ngeoffray@google.com>
2014-04-11 17:43:50 +0100

    Long support in optimizing compiler.
    - Add stack locations to the Location class.
    - Change logic of parameter passing/setup by setting the
      location of such instructions the ones for the calling
      convention.
    
    Change-Id: I4730ad58732813dcb9c238f44f55dfc0baa18799

--- runtime/utils.h
+++ runtime/utils.h
@@ -421,6 +421,61 @@ class VoidFunctor {
   }
 };
 
+static const uword kUwordOne = 1U;
+
+// BitField is a template for encoding and decoding a bit field inside
+// an unsigned machine word.
+template<typename T, int position, int size>
+class BitField {
+ public:
+  // Tells whether the provided value fits into the bit field.
+  static bool is_valid(T value) {
+    return (static_cast<uword>(value) & ~((kUwordOne << size) - 1)) == 0;
+  }
+
+  // Returns a uword mask of the bit field.
+  static uword mask() {
+    return (kUwordOne << size) - 1;
+  }
+
+  // Returns a uword mask of the bit field which can be applied directly to
+  // to the raw unshifted bits.
+  static uword mask_in_place() {
+    return ((kUwordOne << size) - 1) << position;
+  }
+
+  // Returns the shift count needed to right-shift the bit field to
+  // the least-significant bits.
+  static int shift() {
+    return position;
+  }
+
+  // Returns the size of the bit field.
+  static int bitsize() {
+    return size;
+  }
+
+  // Returns a uword with the bit field value encoded.
+  static uword encode(T value) {
+    DCHECK(is_valid(value));
+    return static_cast<uword>(value) << position;
+  }
+
+  // Extracts the bit field from the value.
+  static T decode(uword value) {
+    return static_cast<T>((value >> position) & ((kUwordOne << size) - 1));
+  }
+
+  // Returns a uword with the bit field value encoded based on the
+  // original value. Only the bits corresponding to this bit field
+  // will be changed.
+  static uword update(T value, uword original) {
+    DCHECK(is_valid(value));
+    return (static_cast<uword>(value) << position) |
+        (~mask_in_place() & original);
+  }
+};
+
 }  // namespace art
 
 #endif  // ART_RUNTIME_UTILS_H_
