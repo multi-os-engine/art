a588f32bab5ae5658e94f5b0de21939bc6b6631b
Jean Christophe Beyler <jean.christophe.beyler@intel.com>
2014-04-15 16:39:21 -0700

    ART: Adding Use-Def Chain functionality
    Adding the functionality of Use-Def chains to ART. Though it is not yet called,
    therefore cannot be used yet, it is going to be called during the clean-up
    phase.
    
    Change-Id: I53cc6303846baeda823131e8faead525f6691704
    Signed-off-by: Jean Christophe Beyler <jean.christophe.beyler@intel.com>
    Signed-off-by: Razvan A Lupusoru <razvan.a.lupusoru@intel.com>
    Signed-off-by: Yixin Shou <yixin.shou@intel.com>
    Signed-off-by: Chao-ying Fu <chao-ying.fu@intel.com>
    Signed-off-by: Udayan Banerji <udayan.banerji@intel.com>

---
+++ compiler/dex/ssa_walkdata.cc
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "ssa_walkdata.h"
+#include "compiler_internals.h"
+
+namespace art {
+
+SSAWalkData::SSAWalkData(MIRGraph* mir_graph) {
+  // Set up the free chain list.
+  free_chains_list_ = &(mir_graph->global_def_use_chain_);
+  free_chains_ = mir_graph->global_def_use_chain_;
+  arena_ = mir_graph->GetArena();
+
+  int num = mir_graph->GetNumSSARegs();
+
+  definitions_.resize(num);
+  last_chain_.resize(num);
+
+  for (int i = 0; i < num; i++) {
+    definitions_[i] = nullptr;
+    last_chain_[i] = nullptr;
+  }
+}
+
+UsedChain *SSAWalkData::GetUsedChain(void) {
+  UsedChain* res = free_chains_;
+
+  // If we don't have one.
+  if (res == nullptr) {
+    res = static_cast<UsedChain *>(arena_->Alloc(sizeof(*res), kArenaAllocDFInfo));
+
+    // Attach it to global free chains.
+    res->next_chain_ = *free_chains_list_;
+    *free_chains_list_ = res;
+  } else {
+    // We can move forward in the freeChains list.
+    free_chains_ = free_chains_->next_chain_;
+  }
+
+  // Reset it.
+  res->next_use_ = nullptr;
+  res->prev_use_ = nullptr;
+  res->mir_ = nullptr;
+
+  return res;
+}
+
+UsedChain *SSAWalkData::GetLastChain(int ssa_reg) {
+  return last_chain_[ssa_reg];
+}
+
+void SSAWalkData::SetLastChain(UsedChain* chain, int ssa_reg) {
+  last_chain_[ssa_reg] = chain;
+}
+
+void SSAWalkData::SetDefinition(MIR* insn, int ssa_reg) {
+  definitions_[ssa_reg] = insn;
+}
+
+MIR *SSAWalkData::GetDefinition(int ssa_reg) const {
+  return definitions_[ssa_reg];
+}
+
+void SSAWalkData::AddUseToDefChain(int use_idx, MIR* used, MIR* defined) {
+  SSARepresentation* ssa_rep = used->ssa_rep;
+
+  // Set def_where for this instruction.
+  ssa_rep->def_where_[use_idx] = defined;
+
+  if (defined == nullptr) {
+    return;
+  }
+
+  // We have a need of a new chain element.
+  UsedChain* elem = GetUsedChain();
+
+  elem->mir_ = used;
+  int ssa_reg = ssa_rep->uses[use_idx];
+
+  // Get the last use for this element and set it.
+  UsedChain* last = GetLastChain(ssa_reg);
+  SetLastChain(elem, ssa_reg);
+
+  // If we already have one, just chain.
+  if (last != nullptr) {
+    last->next_use_ = elem;
+    elem->prev_use_ = last;
+    return;
+  }
+
+  // It's the first, tell defined about it.
+  SSARepresentation* def_ssa = defined->ssa_rep;
+
+  // Paranoid.
+  DCHECK(def_ssa != nullptr);
+
+  // Go through the defines and find ssa_reg.
+  int max = def_ssa->num_defs;
+  for (int j = 0; j < max; j++) {
+    if (def_ssa->defs[j] == ssa_reg) {
+      def_ssa->used_next_[j] = elem;
+      return;
+    }
+  }
+
+  // We should have found it.
+  DCHECK(false);
+}
+
+void SSAWalkData::HandleNoDefinitions(void) {
+  // Go through each no definition.
+  for (const WalkDataNoDefine& info: no_define_) {
+    MIR* mir = info.mir_;
+    int idx = info.index_;
+
+    SSARepresentation* ssa_rep = mir->ssa_rep;
+
+    // Paranoid.
+    DCHECK(ssa_rep != nullptr);
+    DCHECK(0 <= idx && idx < ssa_rep->num_uses);
+
+    // Get use ssa_reg.
+    int ssa_reg = ssa_rep->uses[idx];
+
+    // Set def_where for this instruction.
+    MIR* defined = GetDefinition(ssa_reg);
+
+    AddUseToDefChain(idx, mir, defined);
+  }
+}
+
+void SSAWalkData::AddNoDefine(MIR* mir, int idx) {
+  // Create the data and Set it.
+  WalkDataNoDefine info = {mir, idx};
+  no_define_.push_back(info);
+}
+
+}  // namespace art
---
+++ compiler/dex/ssa_walkdata.h
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef ART_COMPILER_DEX_SSA_WALKDATA_H_
+#define ART_COMPILER_DEX_SSA_WALKDATA_H_
+
+#include <vector>
+#include "compiler_internals.h"
+#include "mir_graph.h"
+
+namespace art {
+
+    /**
+     * @class WalkDataNoDefine
+     * @brief Used as a helper structure to handle SSA registers without a definition during the parsing.
+     */
+    struct WalkDataNoDefine {
+        MIR* mir_;   /**< @brief the MIR containing the use without a definition. */
+        int index_;  /**< @brief the index in the ssaRep->uses array for the SSA register. */
+    };
+
+    /**
+     * @class SSAWalkData
+     * @brief SSAWalkData contains any data required inter BasicBlock.
+     */
+    class SSAWalkData {
+    public:
+        explicit SSAWalkData(MIRGraph* mir_graph);
+        ~SSAWalkData(void);
+
+        UsedChain* GetUsedChain(void);
+
+        /**
+         * @brief Get last chain node for a particular SSA register.
+         * @return the last chain node for the value SSA register or nullptr.
+         */
+        UsedChain* GetLastChain(int ssa_reg);
+
+        /**
+         * @brief Set the last chain for a given SSA register.
+         */
+        void SetLastChain(UsedChain* chain, int ssa_reg);
+
+        /**
+         * @brief Associate a defined register and the instruction.
+         */
+        void SetDefinition(MIR* insn, int ssa_reg);
+
+        /**
+         * @brief Get the instruction containing the definition.
+         * @return the MIR containing the definition, nullptr if none found.
+         */
+        MIR* GetDefinition(int ssa_reg) const;
+
+        /**
+         * @brief Handle the SSA registers without a definition during the parsing.
+         */
+        void HandleNoDefinitions(void);
+
+        /**
+         * @brief Add a SSA register that does not have a definition during the parsing.
+         * @param mir the MIR containing the SSA register without definition.
+         * @param idx the index in the ssaRep->uses array for the register.
+         */
+        void AddNoDefine(MIR* mir, int idx);
+
+        /**
+         * @brief update Def chain with new use.
+         * @param use_idx the uses index.
+         * @param used the MIR corresponding to Use.
+         * @param defined the MIR corresponding to Def.
+         */
+        void AddUseToDefChain(int use_idx, MIR* used, MIR* defined);
+
+    protected:
+        /** @brief Association SSA register <-> where it is defined. */
+        std::vector<MIR*> definitions_;
+
+        /** @brief Association SSA register <-> the last use chain node. */
+        std::vector<UsedChain*> last_chain_;
+
+        /** @brief Free chain node list. */
+        UsedChain** free_chains_list_;
+
+        /** @brief Current free chain node. */
+        UsedChain* free_chains_;
+
+        /** @brief Any MIR not having a definition during the parsing. */
+        std::vector<WalkDataNoDefine> no_define_;
+
+        /** @brief Local copy of a pointer to the MIRGraph arena. */
+        ArenaAllocator* arena_;
+    };
+}  //  namespace art
+
+#endif  // ART_COMPILER_DEX_SSA_WALKDATA_H_
