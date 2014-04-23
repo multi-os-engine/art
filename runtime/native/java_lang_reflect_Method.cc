087abbb439a5aa57473f31c8d05ce7a19e5712af
Jeff Hao <jeffhao@google.com>
2014-04-23 14:51:46 -0700

    Fix Class.newInstance to not wrap its exceptions.
    Unlike Constructor.newInstance, Class.newInstance should not wrap
    exceptions it throws.
    
    Art half of this change. Also has a corresponding change in libcore.
    
    Bug: 14115585
    
    Change-Id: I47d78904e72f299284d1382bc715ab120d3b1a7f

--- runtime/native/java_lang_reflect_Method.cc
+++ runtime/native/java_lang_reflect_Method.cc
@@ -32,7 +32,7 @@ namespace art {
 static jobject Method_invoke(JNIEnv* env, jobject javaMethod, jobject javaReceiver,
                              jobject javaArgs, jboolean accessible) {
   ScopedFastNativeObjectAccess soa(env);
-  return InvokeMethod(soa, javaMethod, javaReceiver, javaArgs, (accessible == JNI_TRUE));
+  return InvokeMethod(soa, javaMethod, javaReceiver, javaArgs, (accessible == JNI_TRUE), true);
 }
 
 static jobject Method_getExceptionTypesNative(JNIEnv* env, jobject javaMethod) {
