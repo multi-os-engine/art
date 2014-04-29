81cfd6cb9fb2921cca059f19a73c5e9a326468d4
Jean Christophe Beyler <jean.christophe.beyler@intel.com>
2014-04-29 14:40:41 -0700

    ART: Topological Sort Traversal Implementation
    - Added a topological sort implementation for traversal.
    - Added a function to BasicBlock to detect if it is an exception block.
    
    Change-Id: I573da1768a635c6fd0259573dbb46b112132e129
    Signed-off-by: Jean Christophe Beyler <jean.christophe.beyler@intel.com>
    Signed-off-by: Razvan A Lupusoru <razvan.a.lupusoru@intel.com>
    Signed-off-by: Yixin Shou <yixin.shou@intel.com>
    Signed-off-by: Chao-ying Fu <chao-ying.fu@intel.com>
    Signed-off-by: Udayan Banerji <udayan.banerji@intel.com>

--- compiler/dex/mir_graph.h
+++ compiler/dex/mir_graph.h
@@ -329,11 +329,13 @@ struct BasicBlock {
    * @return Returns the following MIR if one can be found.
    */
   MIR* GetNextUnconditionalMir(MIRGraph* mir_graph, MIR* current);
+
+  bool IsExceptionBlock() const;
 };
 
 /*
  * The "blocks" field in "successor_block_list" points to an array of elements with the type
- * "SuccessorBlockInfo".  For catch blocks, key is type index for the exception.  For swtich
+ * "SuccessorBlockInfo".  For catch blocks, key is type index for the exception.  For switch
  * blocks, key is the case value.
  */
 struct SuccessorBlockInfo {
@@ -572,6 +574,10 @@ class MIRGraph {
   void DumpRegLocTable(RegLocation* table, int count);
 
   void BasicBlockOptimization();
+
+  GrowableArray<BasicBlockId>* GetTopologicalSortOrder() {
+    return topological_order_;
+  }
 
   bool IsConst(int32_t s_reg) const {
     return is_constant_v_->IsBitSet(s_reg);
@@ -839,6 +845,7 @@ class MIRGraph {
   MIR* AdvanceMIR(BasicBlock** p_bb, MIR* mir);
   BasicBlock* NextDominatedBlock(BasicBlock* bb);
   bool LayoutBlocks(BasicBlock* bb);
+  void ComputeTopologicalSortOrder();
 
   bool InlineCallsGate();
   void InlineCallsStart();
@@ -976,6 +983,7 @@ class MIRGraph {
   GrowableArray<BasicBlockId>* dfs_order_;
   GrowableArray<BasicBlockId>* dfs_post_order_;
   GrowableArray<BasicBlockId>* dom_post_order_traversal_;
+  GrowableArray<BasicBlockId>* topological_order_;
   int* i_dom_list_;
   ArenaBitVector** def_block_matrix_;    // num_dalvik_register x num_blocks.
   ArenaBitVector* temp_dalvik_register_v_;
