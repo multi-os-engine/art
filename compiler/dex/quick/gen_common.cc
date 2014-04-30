3e289f754f82647a998f9e38c5a2cc2d135d8997
Mark Mendell <mark.p.mendell@intel.com>
2014-04-30 14:13:18 -0400

    Allow X86 QBE to be extended
    Enhancements and updates to allow X86Mir2LIR Backend to be subclassed
    for experimentation.  Add virtual in a whole bunch of places, and make
    some other changes to get this to work.
    
    Change-Id: I0980a19bc5d5725f91660f98c95f1f51c17ee9b6
    Signed-off-by: Mark Mendell <mark.p.mendell@intel.com>

--- compiler/dex/quick/gen_common.cc
+++ compiler/dex/quick/gen_common.cc
@@ -1937,6 +1937,15 @@ void Mir2Lir::GenArithOpLong(Instruction::Code opcode, RegLocation rl_dest,
   }
 }
 
+void Mir2Lir::GenConst(RegLocation rl_dest, int value) {
+  RegLocation rl_result = EvalLoc(rl_dest, kAnyReg, true);
+  LoadConstantNoClobber(rl_result.reg, value);
+  StoreValue(rl_dest, rl_result);
+  if (value == 0) {
+    Workaround7250540(rl_dest, rl_result.reg);
+  }
+}
+
 void Mir2Lir::GenConversionCall(ThreadOffset<4> func_offset,
                                 RegLocation rl_dest, RegLocation rl_src) {
   /*
