5dff337eacd401581ce3cf5c01dbcfae9ce46b4b
Jean Christophe Beyler <jean.christophe.beyler@intel.com>
2014-05-01 15:36:22 -0700

    ART: Better SSA Allocation when recreating SSA
    The SSA calculation is not allocation friendly. This makes the
    SSARepresentation remember how much is allocated and not reallocate
    if SSA should be recalculated.
    
    Also added some allocation friendly code for the dominance code.
    
    Change-Id: Icd5586b7e2fefae8e1535975ab400b1ca95b500f
    Signed-off-by: Jean Christophe Beyler <jean.christophe.beyler@intel.com>

--- compiler/dex/mir_graph.h
+++ compiler/dex/mir_graph.h
@@ -223,7 +223,7 @@ struct BasicBlockDataFlow {
   ArenaBitVector* def_v;
   ArenaBitVector* live_in_v;
   ArenaBitVector* phi_v;
-  int32_t* vreg_to_ssa_map;
+  int32_t* vreg_to_ssa_map_exit;
   ArenaBitVector* ending_check_v;  // For null check and class init check elimination.
 };
 
@@ -236,8 +236,10 @@ struct BasicBlockDataFlow {
  * we may want to revisit in the future.
  */
 struct SSARepresentation {
-  int16_t num_uses;
-  int16_t num_defs;
+  int8_t num_uses_allocated;
+  int8_t num_defs_allocated;
+  int8_t num_uses;
+  int8_t num_defs;
   int32_t* uses;
   bool* fp_use;
   int32_t* defs;
@@ -1020,6 +1022,10 @@ class MIRGraph {
   void CombineBlocks(BasicBlock* bb);
 
   void ClearAllVisitedFlags();
+
+  void AllocateSSAUseData(MIR *mir, int num_uses);
+  void AllocateSSADefData(MIR *mir, int num_defs);
+
   /*
    * IsDebugBuild sanity check: keep track of the Dex PCs for catch entries so that later on
    * we can verify that all catch entries have native PC entries.
@@ -1105,6 +1111,7 @@ class MIRGraph {
   GrowableArray<uint32_t> use_counts_;      // Weighted by nesting depth
   GrowableArray<uint32_t> raw_use_counts_;  // Not weighted
   unsigned int num_reachable_blocks_;
+  unsigned int max_num_reachable_blocks_;
   GrowableArray<BasicBlockId>* dfs_order_;
   GrowableArray<BasicBlockId>* dfs_post_order_;
   GrowableArray<BasicBlockId>* dom_post_order_traversal_;
