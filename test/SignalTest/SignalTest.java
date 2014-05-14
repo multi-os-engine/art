2b94b2272066308c83f30035fb47bd0f8cfdbe49
Dave Allison <dallison@google.com>
2014-05-14 15:41:25 -0700

    Make use of sigchainlib for signal chaining for implicit checks
    This adds a preload library that overrides sigaction and sigprocmask
    to implement signal chaining.  Signal chaining allows us to chain
    any signal so that the ART runtime receives it before any signal
    handler registered in native code by an application.  If the
    ART signal handler doesn't want it, it will pass it on to the
    user's handler.
    
    ART uses signals for null pointer checks, stack overflow checks and
    suspend points.
    
    Also adds an OAT test to test this in isolation.
    
    Change-Id: I9545f9f7343774c091410eb810504d9855fd399f

---
+++ test/SignalTest/SignalTest.java
@@ -0,0 +1,44 @@
+class SignalTest {
+    private static native void initSignalTest();
+    private static native void terminateSignalTest();
+    private static native int testSignal();
+
+    private static void stackOverflow() {
+       stackOverflow();
+    }
+
+    public static void main(String[] args) {
+        System.loadLibrary("arttest");
+
+        System.out.println("init signal test");
+        initSignalTest();
+        try {
+            Object o = null;
+            int hash = o.hashCode();
+
+            // Should never get here.
+            System.out.println("hash: " + hash);
+            throw new AssertionError();
+        } catch (NullPointerException e) {
+            System.out.println("Caught NullPointerException");
+        }
+        try {
+            stackOverflow();
+
+            // Should never get here.
+            throw new AssertionError();
+        } catch (StackOverflowError e) {
+            System.out.println("Caught StackOverflowError");
+        }
+
+        // Test that a signal in native code works.  This will return
+        // the value 1234 if the signal is caught.
+        int x = testSignal();
+        if (x != 1234) {
+            throw new AssertionError();
+        }
+
+        terminateSignalTest();
+        System.out.println("Signal test OK");
+    }
+}
