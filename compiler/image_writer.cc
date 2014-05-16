22f7be95ccd7533c7ee06480c43a6d2e08336134
Mathieu Chartier <mathieuc@google.com>
2014-05-16 10:59:25 -0700

    Delete ClassHelper and fix compaction bug in GetDirectInterface
    Cleanup helps to prevent compaction bugs. Fixed a fairly serious
    compaction error caused by calling ClassHelper::GetDirectInterface
    without handling the case where it causes thread suspension due to
    ResolveType.
    
    Bug: 8981901
    
    Change-Id: I82b3bb6dd48d21eb6ece7aae0733c4a23c2bc408

--- compiler/image_writer.cc
+++ compiler/image_writer.cc
@@ -285,7 +285,7 @@ void ImageWriter::ComputeEagerResolvedStrings() SHARED_LOCKS_REQUIRED(Locks::mut
 }
 
 bool ImageWriter::IsImageClass(Class* klass) {
-  return compiler_driver_.IsImageClass(ClassHelper(klass).GetDescriptor());
+  return compiler_driver_.IsImageClass(klass->GetDescriptor().c_str());
 }
 
 struct NonImageClasses {
@@ -339,7 +339,7 @@ void ImageWriter::PruneNonImageClasses() {
 bool ImageWriter::NonImageClassesVisitor(Class* klass, void* arg) {
   NonImageClasses* context = reinterpret_cast<NonImageClasses*>(arg);
   if (!context->image_writer->IsImageClass(klass)) {
-    context->non_image_classes->insert(ClassHelper(klass).GetDescriptor());
+    context->non_image_classes->insert(klass->GetDescriptor());
   }
   return true;
 }
@@ -359,7 +359,7 @@ void ImageWriter::CheckNonImageClassesRemovedCallback(Object* obj, void* arg) {
     Class* klass = obj->AsClass();
     if (!image_writer->IsImageClass(klass)) {
       image_writer->DumpImageClasses();
-      CHECK(image_writer->IsImageClass(klass)) << ClassHelper(klass).GetDescriptor()
+      CHECK(image_writer->IsImageClass(klass)) << klass->GetDescriptor()
                                                << " " << PrettyDescriptor(klass);
     }
   }
