5b1a2c576bbe88fbe81cc62bcc692e4ec7634638
Dmitry Petrochenko <dmitry.petrochenko@intel.com>
2014-05-17 01:02:18 +0700

    [WIP] x86_64: Hard Float ABI support in QCG
    This patch shows our efforts on resolving the ART limitations:
     - passing "float"/"double" arguments via FPR
     - passing "long" arguments via single GPR, not pair
     - passing more than 3 agruments via GPR.
    
    Work done:
     - Extended SpecialTargetRegister enum with kARG4, kARG5, fARG4..fARG7.
     - Created initial LoadArgRegs/GenDalvikX/FlushIns version in X86Mir2Lir.
     - Unlimited number of long/double/float arguments support
    
    The testD10 fails (first argument is incorrect).
    
    Change-Id: I5deadd320b4341d5b2f50ba6fa4a98031abc3902
    Signed-off-by: Dmitry Petrochenko <dmitry.petrochenko@intel.com>

--- runtime/entrypoints/quick/quick_trampoline_entrypoints.cc
+++ runtime/entrypoints/quick/quick_trampoline_entrypoints.cc
@@ -172,7 +172,11 @@ class QuickArgumentVisitor {
   // | Padding         |
   // | RDI/Method*     |  <- sp
   static constexpr bool kQuickSoftFloatAbi = false;  // This is a hard float ABI.
-  static constexpr size_t kNumQuickGprArgs = 5;  // 3 arguments passed in GPRs.
+#ifdef TARGET_REX_SUPPORT
+  static constexpr size_t kNumQuickGprArgs = 5;  // 5 arguments passed in GPRs.
+#else
+  static constexpr size_t kNumQuickGprArgs = 3;  // 3 arguments passed in GPRs if r8..r15 not enabled.
+#endif
   static constexpr size_t kNumQuickFprArgs = 8;  // 0 arguments passed in FPRs.
   static constexpr size_t kQuickCalleeSaveFrame_RefAndArgs_Fpr1Offset = 16;  // Offset of first FPR arg.
   static constexpr size_t kQuickCalleeSaveFrame_RefAndArgs_Gpr1Offset = 80;  // Offset of first GPR arg.
