b5d755d007e0893f6f3f4d45713dcd74f69ca22c
Mathieu Chartier <mathieuc@google.com>
2014-05-21 17:43:44 -0700

    Change MethodHelper to use a Handle.
    Added ConstHandle to help prevent errors where you modify the value
    stored in the handle of the caller. Also fixed compaction bugs
    related to not knowing MethodHelper::GetReturnType can resolve types.
    This bug was present in interpreter RETURN_OBJECT.
    
    Bug: 13077697
    
    Change-Id: I71f964d4d810ab4debda1a09bc968af8f3c874a3

--- test/StackWalk/stack_walk_jni.cc
+++ test/StackWalk/stack_walk_jni.cc
@@ -31,7 +31,7 @@
 namespace art {
 
 #define REG(mh, reg_bitmap, reg) \
-    (((reg) < mh.GetCodeItem()->registers_size_) && \
+    (((reg) < m->GetCodeItem()->registers_size_) && \
      ((*((reg_bitmap) + (reg)/8) >> ((reg) % 8) ) & 0x01))
 
 #define CHECK_REGS(...) if (!IsShadowFrame()) { \
@@ -64,8 +64,7 @@ struct TestReferenceMapVisitor : public StackVisitor {
       NativePcOffsetToReferenceMap map(m->GetNativeGcMap());
       reg_bitmap = map.FindBitMap(GetNativePcOffset());
     }
-    MethodHelper mh(m);
-    StringPiece m_name(mh.GetName());
+    StringPiece m_name(m->GetName());
 
     // Given the method name and the number of times the method has been called,
     // we know the Dex registers with live reference values. Assert that what we
