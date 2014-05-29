ce56a03733ff605c7894855b50ef208d5a4a92a9
Dave Allison <dallison@google.com>
2014-05-29 08:20:04 -0700

    Add implicit null and stack checks for x86
    This adds compiler and runtime changes for x86
    implicit checks.  32 bit only.
    
    Both host and target are supported.
    By default, on the host, the implicit checks are null pointer and
    stack overflow.  Suspend is implemented but not switched on.
    
    Change-Id: I88a609e98d6bf32f283eaa4e6ec8bbf8dc1df78a
    
    Conflicts:
    	compiler/dex/quick/x86/assemble_x86.cc
    	compiler/dex/quick/x86/codegen_x86.h
    	test/Android.mk

--- test/Android.mk
+++ test/Android.mk
@@ -143,6 +143,7 @@ test-art-host-oat-default-$(1): $(HOST_OUT_JAVA_LIBRARIES)/$(ART_HOST_ARCH)/oat-
 	mkdir -p /tmp/android-data/test-art-host-oat-default-$(1)
 	ANDROID_DATA=/tmp/android-data/test-art-host-oat-default-$(1) \
 	  ANDROID_ROOT=$(HOST_OUT) \
+ 	  LD_PRELOAD=libsigchain.so \
 	  LD_LIBRARY_PATH=$(HOST_LIBRARY_PATH) \
 	  $(HOST_OUT_EXECUTABLES)/dalvikvm $(DALVIKVM_FLAGS) -XXlib:libartd$(HOST_SHLIB_SUFFIX) -Ximage:$(HOST_CORE_IMG_LOCATION) -classpath $(HOST_OUT_JAVA_LIBRARIES)/oat-test-dex-$(1).jar -Djava.library.path=$(HOST_LIBRARY_PATH) $(1) $(2) \
           && echo test-art-host-oat-default-$(1) PASSED || (echo test-art-host-oat-default-$(1) FAILED && exit 1)
@@ -153,6 +154,7 @@ test-art-host-oat-interpreter-$(1): $(HOST_OUT_JAVA_LIBRARIES)/$(ART_HOST_ARCH)/
 	mkdir -p /tmp/android-data/test-art-host-oat-interpreter-$(1)
 	ANDROID_DATA=/tmp/android-data/test-art-host-oat-interpreter-$(1) \
 	  ANDROID_ROOT=$(HOST_OUT) \
+ 	  LD_PRELOAD=libsigchain.so \
 	  LD_LIBRARY_PATH=$(HOST_LIBRARY_PATH) \
 	  $(HOST_OUT_EXECUTABLES)/dalvikvm -XXlib:libartd$(HOST_SHLIB_SUFFIX) -Ximage:$(HOST_CORE_IMG_LOCATION) $(DALVIKVM_FLAGS) -Xint -classpath $(HOST_OUT_JAVA_LIBRARIES)/oat-test-dex-$(1).jar -Djava.library.path=$(HOST_LIBRARY_PATH) $(1) $(2) \
           && echo test-art-host-oat-interpreter-$(1) PASSED || (echo test-art-host-oat-interpreter-$(1) FAILED && exit 1)
