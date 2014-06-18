58c2806eb3217d4ffd1312ee3ae034dcf83230c0
Alex Light <allight@google.com>
2014-06-18 10:35:45 -0700

    Fixed gdb support and added some ElfFile functions
    Fixed gdb support so that it would continue working even when debug
    symbols or other sections are included in the elf file. Also made it
    actually read parts of the DWARF information so it should work even if
    there are minor changes to how and where DWARF information is written
    out.
    
    Added a dwarf.h file with the dwarf constants.
    
    Added a FindSectionByName function, a FindDynamicSymbol function, and
    the ability to specify the mmap protection and flags directly if we are
    mapping in the whole file.
    
    Modified elf_writer_quick.cc to use the dwarf constants from dwarf.h.
    
    Change-Id: I09e15c425fab252b331a2e4719863552e8b6b137

--- compiler/elf_writer_quick.cc
+++ compiler/elf_writer_quick.cc
@@ -20,6 +20,7 @@
 #include "base/unix_file/fd_file.h"
 #include "buffered_output_stream.h"
 #include "driver/compiler_driver.h"
+#include "dwarf.h"
 #include "elf_utils.h"
 #include "file_output_stream.h"
 #include "globals.h"
@@ -865,31 +866,6 @@ static void PushHalf(std::vector<uint8_t>*buf, int data) {
   buf->push_back(data & 0xff);
   buf->push_back((data >> 8) & 0xff);
 }
-
-// DWARF constants needed to generate CFI information.
-enum {
-  // Tag encodings.
-  DW_TAG_compile_unit = 0x11,
-  DW_TAG_subprogram = 0X2e,
-
-  // Attribute encodings.
-  DW_AT_name = 0x03,
-  DW_AT_low_pc = 0x11,
-  DW_AT_high_pc = 0x12,
-  DW_AT_language = 0x13,
-
-  // Constant encoding.
-  DW_CHILDREN_no = 0x00,
-  DW_CHILDREN_yes = 0x01,
-
-  // Attribute form encodings.
-  DW_FORM_addr = 0x01,
-  DW_FORM_data1 = 0x0b,
-  DW_FORM_strp = 0x0e,
-
-  // Language encoding.
-  DW_LANG_Java = 0x000b
-};
 
 void ElfWriterQuick::FillInCFIInformation(OatWriter* oat_writer,
                                           std::vector<uint8_t>* dbg_info,
