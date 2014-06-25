d414fe13ded542872bd8f27fbc9e6ecd95765c72
Yevgeny Rouban <yevgeny.y.rouban@intel.com>
2014-06-25 17:08:41 +0700

    ART VTune JIT API support
    Intel Amplifier XE 2014 is implementing ART support by reading
    methods from OAT files. But the OAT versions are changing very often.
    That is why it is hard for the Amplifier team to maintain an OAT
    reading tool and users cannot see ART methods with newer OAT format.
    
    This solution is to integrate VTune JIT API support in ART.
    Once an OAT file is mapped into memory, all its methods are reported
    with VTune JIT API. The VTune JIT agent (libittnotify.so) writes
    the methods to a JIT file (/data/amplxe/results/localhost.PID.jit),
    which can be read by Intel Amplifier.
    
    This patch introduces basic functionality and needs a decision on
    how to implement options:
    1. filter by app package (process nice name)
    2. filter by dex/oat/class/method
    3. output map type: none/dex/java
    4. vtune jit api library path (to set INTEL_JIT_PROFILER env variable)
    5. set any other environment vars for the profiler lib (output path, ...)
    
    By now the options are passed using environment variables:
    
      OAT_FILE_VTUNE - full path to OAT file
    
      OAT_FILE_VTUNE_SRC - kind of source mapping information.
           One of dex, none, java. Default is java.
    
      INTEL_JIT_PROFILER32, INTEL_JIT_PROFILER64 - path to a VTune JIT agent
           32- and 64-bit library respectively.
           Default is /data/intel/libittnotify.so
    
    But environment variables are not suitable for user level performance
    profiling of Android applications.
    
    I would introduce a set of process's nice name specific system
    properties dalvik.vm.vtune.<nice_name>.*.
    For example:
      dalvik.vm.vtune.zygote.oat_files=/system/framework/x86/boot.oat
      dalvik.vm.vtune.com.android.dialer.oat_file_src=dex
      dalvik.vm.vtune.com.android.dialer.profiler=/data/intel/libittnotify.so
    
    This patch tested on x86, x86_64 and ARM emulators.
    
    4 files runtime/vtune/* are taken from Intel Amplifier XE with permission
    to distribute them under Apache License 2.0.
    
    Change-Id: I659b58d34bf9611d0a7f1fa9f41b53e732998b60

---
+++ runtime/vtune_support.cc
@@ -0,0 +1,270 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vector>
+#include <algorithm>
+
+#include "vtune_support.h"
+#include "mapping_table.h"
+#include "oat_file-inl.h"
+#include "dex_file-inl.h"
+#include "dex_instruction.h"
+#include "vtune/jitprofiling.h"
+
+namespace art {
+
+typedef std::vector<LineNumberInfo> LineInfoTable;
+
+struct SortLineNumberInfoByOffset {
+  bool operator()(LineNumberInfo const& lhs, LineNumberInfo const& rhs) {
+    return lhs.Offset < rhs.Offset;
+  }
+
+  static void sort(LineInfoTable &table) {
+    std::sort(table.begin(), table.end(), SortLineNumberInfoByOffset());
+  }
+};
+
+struct SortLineNumberInfoByLine {
+  static const unsigned NOT_FOUND = 0 - 1;
+
+  bool operator()(LineNumberInfo const& lhs, LineNumberInfo const& rhs) {
+    return lhs.LineNumber < rhs.LineNumber;
+  }
+
+  static void sort(LineInfoTable &table) {
+    std::sort(table.begin(), table.end(), SortLineNumberInfoByLine());
+  }
+
+  static int find(LineInfoTable &table, unsigned line, bool strict = false) {
+    LineInfoTable::iterator lower =
+           std::lower_bound(table.begin(), table.end(), LineNumberInfo( {0, line}), SortLineNumberInfoByLine());
+    return lower == table.end() || (strict && lower->LineNumber != line)
+           ? NOT_FOUND
+           : lower->Offset;
+  }
+};
+
+// Reads debug information from dex file to get BC to Java line mapping
+static void getLineInfoForJava(const uint8_t *dbgstream, LineInfoTable& table, LineInfoTable& pc2dex) {
+  SortLineNumberInfoByLine::sort(pc2dex);
+
+  int adjopcode;
+  uint32_t address = 0;
+  uint32_t line = DecodeUnsignedLeb128(&dbgstream);
+
+  // skip parameters
+  for(uint32_t param_count = DecodeUnsignedLeb128(&dbgstream); param_count != 0; --param_count) {
+    DecodeUnsignedLeb128(&dbgstream);
+  }
+
+  for(bool is_end = false; is_end == false; ) {
+    uint8_t opcode = *dbgstream++;
+    switch (opcode) {
+    case DexFile::DBG_END_SEQUENCE:
+      is_end = true;
+      break;
+
+    case DexFile::DBG_ADVANCE_PC:
+      address += DecodeUnsignedLeb128(&dbgstream);
+      break;
+
+    case DexFile::DBG_ADVANCE_LINE:
+      line += DecodeSignedLeb128(&dbgstream);
+      break;
+
+    case DexFile::DBG_START_LOCAL:
+    case DexFile::DBG_START_LOCAL_EXTENDED:
+      DecodeUnsignedLeb128(&dbgstream);
+      DecodeUnsignedLeb128(&dbgstream);
+      DecodeUnsignedLeb128(&dbgstream);
+
+      if (opcode == DexFile::DBG_START_LOCAL_EXTENDED) {
+        DecodeUnsignedLeb128(&dbgstream);
+      }
+      break;
+
+    case DexFile::DBG_END_LOCAL:
+    case DexFile::DBG_RESTART_LOCAL:
+      DecodeUnsignedLeb128(&dbgstream);
+      break;
+
+    case DexFile::DBG_SET_PROLOGUE_END:
+    case DexFile::DBG_SET_EPILOGUE_BEGIN:
+    case DexFile::DBG_SET_FILE:
+      break;
+
+    default:
+      adjopcode = opcode - DexFile::DBG_FIRST_SPECIAL;
+      address += adjopcode / DexFile::DBG_LINE_RANGE;
+      line += DexFile::DBG_LINE_BASE + (adjopcode % DexFile::DBG_LINE_RANGE);
+
+      unsigned offset;
+      offset = SortLineNumberInfoByLine::find(pc2dex, address);
+      if (offset != SortLineNumberInfoByLine::NOT_FOUND) {
+        table.push_back( {offset, line});
+      }
+      break;
+    }
+  }
+}
+
+// convert dex offsets to dex disassembly line numbers in pc2dex
+static void getLineInfoForDex(const DexFile::CodeItem* code_item, LineInfoTable& pc2dex) {
+  SortLineNumberInfoByLine::sort(pc2dex);
+
+  size_t offset = 0; // in code units
+  size_t end = code_item->insns_size_in_code_units_;
+  int line_no = 1;
+  auto li = pc2dex.begin();
+  for ( ; offset < end && li != pc2dex.end(); ++line_no) {
+    const Instruction* instr = Instruction::At(&code_item->insns_[offset]);
+    for ( ; li != pc2dex.end() && li->LineNumber <= offset; li++) {
+      // REVIEW: to be strict we have to remove li if li->lineNumber != offset
+      li->LineNumber = line_no;
+    }
+    offset += instr->SizeInCodeUnits();
+  }
+}
+
+void SendOatFileToVTune(OatFile &oat_file) {
+  // TODO: come up with another way to get options:
+  // 1. filter by app package (process nice name)
+  // 2. filter by dex/oat/class/method
+  // 3. output map type: none/dex/java
+  // 4. vtune jit api library path (to set INTEL_JIT_PROFILER environment variable)
+  // 5. set any other environment vars for the profiler lib (output path, ...)
+  static const char * OAT_FILE_VTUNE = getenv("OAT_FILE_VTUNE");
+  static const char * OAT_FILE_VTUNE_SRC = getenv("OAT_FILE_VTUNE_SRC");
+
+  // filter output
+  if (OAT_FILE_VTUNE == NULL || 0 != strcmp(OAT_FILE_VTUNE, oat_file.GetLocation().c_str())) {
+    return ;
+  }
+
+  iJIT_Method_Load jit_method;
+  memset(&jit_method, 0, sizeof(jit_method));
+
+  // dump dex files
+  std::vector<const OatFile::OatDexFile*> dex_files = oat_file.GetOatDexFiles();
+  for(auto it = dex_files.begin(); it != dex_files.end(); ++it) {
+    const OatFile::OatDexFile* oat_dex_file = *it;
+    jit_method.class_file_name = const_cast<char *>(oat_dex_file->GetDexFileLocation().c_str());
+
+    std::string error_msg;
+    std::unique_ptr<const DexFile> dex_file(oat_dex_file->OpenDexFile(&error_msg));
+    if (dex_file.get() == nullptr) {
+      return;
+    }
+
+    // dump classes
+    for (size_t class_def_index = 0; class_def_index < dex_file->NumClassDefs(); class_def_index++) {
+      const DexFile::ClassDef& class_def = dex_file->GetClassDef(class_def_index);
+      const OatFile::OatClass oat_class = oat_dex_file->GetOatClass(class_def_index);
+      jit_method.source_file_name = const_cast<char *>(dex_file->GetSourceFile(class_def));
+
+      const byte* class_data = dex_file->GetClassData(class_def);
+      if (class_data == NULL) {  // empty class such as a marker interface?
+        return;
+      }
+      ClassDataItemIterator it(*dex_file, class_data);
+
+      // SkipAllFields
+      while (it.HasNextStaticField() || it.HasNextInstanceField()) {
+        it.Next();
+      }
+
+      // dump methods
+      for (uint32_t class_method_idx = 0;
+           it.HasNextDirectMethod() || it.HasNextVirtualMethod();
+           class_method_idx++, it.Next()) {
+        const OatFile::OatMethod oat_method = oat_class.GetOatMethod(class_method_idx);
+        const DexFile::CodeItem* code_item = it.GetMethodCodeItem();
+
+        const void* code = oat_method.GetQuickCode();
+        uint32_t code_size = oat_method.GetQuickCodeSize();
+        if (code == nullptr) {
+          continue; // portable is currently not supported.
+        }
+
+        static unsigned next_method_id = 1000;
+        next_method_id++;
+
+        std::string method_name = PrettyMethod(it.GetMemberIndex(), *dex_file, true);
+        const char *method_name_c = strchr(method_name.c_str(), ' '); // skip return type
+        jit_method.method_name = const_cast<char*>(method_name_c == nullptr ? method_name.c_str() : (method_name_c + 1));
+        jit_method.method_id = next_method_id;
+        jit_method.method_load_address = const_cast<void*>(code);
+        jit_method.method_size = code_size;
+
+        // dump positions
+        MappingTable table(oat_method.GetMappingTable());
+
+        // prepare pc2src to point to either pc2java or pc2dex
+        LineInfoTable *pc2src = nullptr;
+        LineInfoTable pc2dex;
+        LineInfoTable pc2java;
+
+        if ((OAT_FILE_VTUNE_SRC == NULL || strcmp(OAT_FILE_VTUNE_SRC, "none") != 0)
+             && table.TotalSize() != 0 && table.PcToDexSize() != 0) {
+          for (MappingTable::PcToDexIterator cur = table.PcToDexBegin(), end = table.PcToDexEnd(); cur != end; ++cur) {
+            pc2dex.push_back( {cur.NativePcOffset(), cur.DexPc()});
+          }
+
+          if (OAT_FILE_VTUNE_SRC != NULL && strcmp(OAT_FILE_VTUNE_SRC, "dex") == 0) {
+            pc2src = &pc2dex;
+            getLineInfoForDex(code_item, pc2dex);
+            // TODO: set dexdump file name for this method
+          } else { // default is pc -> java
+            pc2src = &pc2java;
+            const byte* dbgstream = dex_file->GetDebugInfoStream(it.GetMethodCodeItem());
+            getLineInfoForJava(dbgstream, pc2java, pc2dex);
+          }
+        }
+
+        if (pc2src == nullptr || pc2src->size() == 0) {
+          jit_method.line_number_size = 0;
+          jit_method.line_number_table = nullptr;
+        } else {
+          // shift offsets
+          SortLineNumberInfoByOffset::sort(*pc2src);
+          for (unsigned i = 1; i < pc2src->size(); ++i) {
+            (*pc2src)[i - 1].Offset = (*pc2src)[i].Offset;
+          }
+          (*pc2src)[pc2src->size() - 1].Offset = code_size;
+
+          jit_method.line_number_size = pc2src->size();
+          jit_method.line_number_table = pc2src->size() == 0 ? nullptr : &((*pc2src)[0]);
+        }
+
+        int is_notified = iJIT_NotifyEvent(iJVM_EVENT_TYPE_METHOD_LOAD_FINISHED, (void*)&jit_method);
+        if (is_notified) {
+          LOG(DEBUG) << "JIT API: method '" << jit_method.method_name
+                     << "' is written successfully: id=" << jit_method.method_id
+                     << ", address=" << jit_method.method_load_address
+                     << ", size=" << jit_method.method_size;
+        } else if (true) {
+          LOG(WARNING) << "JIT API: failed to write method '" << jit_method.method_name
+                       << "': id=" << jit_method.method_id
+                       << ", address=" << jit_method.method_load_address
+                       << ", size=" << jit_method.method_size;
+        }
+      }
+    }
+  }
+}
+
+}  // namespace art
