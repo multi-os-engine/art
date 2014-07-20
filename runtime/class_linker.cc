45bdeaa7112f69ddffe4cb9893f6f637c6054a9f
Dan Albert <danalbert@google.com>
2014-07-19 22:54:27 -0700

    Needed to fix build without NDEBUG.
    Needed by the CHECK_EQ() on runtime/class_linker.cc:3591.
    
    Change-Id: I92ebecb86ca19906efdac39c94eb74f63c633d7d

--- runtime/class_linker.cc
+++ runtime/class_linker.cc
@@ -41,6 +41,7 @@
 #include "interpreter/interpreter.h"
 #include "leb128.h"
 #include "method_helper.h"
+#include "method_helper-inl.h"
 #include "oat.h"
 #include "oat_file.h"
 #include "object_lock.h"
