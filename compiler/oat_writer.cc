b9fbb6d35e72c842e9857ba6be65ad0a9d1e3bcc
Andreas Gampe <agampe@google.com>
2014-07-21 22:56:08 -0700

    ART: Improve error message on oat writer failure
    Try to log the pending exception to help resolve issues.
    
    Bug: 16406811
    Change-Id: I035ae9e59a5ee02b9e90c35c0644ec088d3c7d12

--- compiler/oat_writer.cc
+++ compiler/oat_writer.cc
@@ -539,7 +539,18 @@ class OatWriter::InitImageMethodVisitor : public OatDexMethodVisitor {
                                                       NullHandle<mirror::ClassLoader>(),
                                                       NullHandle<mirror::ArtMethod>(),
                                                       invoke_type);
-    CHECK(method != NULL) << PrettyMethod(it.GetMemberIndex(), *dex_file_, true);
+    if (method == nullptr) {
+      LOG(FATAL) << PrettyMethod(it.GetMemberIndex(), *dex_file_, true);
+      CHECK(soa.Self()->IsExceptionPending());
+      mirror::Throwable* exc = soa.Self()->GetException(nullptr);
+      std::string dump = exc->Dump();
+      std::stringstream ss(dump);
+      std::string line;
+      while (std::getline(ss, line)) {
+        LOG(FATAL) << line;
+      }
+      CHECK(false);
+    }
     // Portable code offsets are set by ElfWriterMclinker::FixupCompiledCodeOffset after linking.
     method->SetQuickOatCodeOffset(offsets.code_offset_);
     method->SetOatNativeGcMapOffset(offsets.gc_map_offset_);
