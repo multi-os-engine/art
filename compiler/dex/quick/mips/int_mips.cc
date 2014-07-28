f9d153b74dbccea035f0588ec1617eb22b33717b
Andreas Gampe <agampe@google.com>
2014-07-27 19:44:50 -0700

    ART: Rework quick entrypoint code in Mir2Lir, cleanup
    To reduce the complexity of calling trampolines in generic code,
    introduce an enumeration for entrypoints. Introduce a header that lists
    the entrypoint enum and exposes a templatized method that translates an
    enum value to the corresponding thread offset value.
    
    Call helpers are rewritten to have an enum parameter instead of the
    thread offset. Also rewrite LoadHelper and GenConversionCall this way.
    It is now LoadHelper's duty to select the right thread offset size.
    
    Introduce InvokeTrampoline virtual method to Mir2Lir. This allows to
    further simplify the call helpers, as well as make OpThreadMem specific
    to X86 only (removed from Mir2Lir).
    
    Make GenInlinedCharAt virtual, move a copy to X86 backend, and simplify
    both copies. Remove LoadBaseIndexedDisp and OpRegMem from Mir2Lir, as they
    are now specific to X86 only.
    
    Remove StoreBaseIndexedDisp from Mir2Lir, as it was only ever used in the
    X86 backend.
    
    Remove OpTlsCmp from Mir2Lir, as it was only ever used in the X86 backend.
    
    Remove GenImmedCheck from Mir2Lir as it was neither used nor implemented.
    
    Change-Id: If0a6182288c5d57653e3979bf547840a4c47626e

--- compiler/dex/quick/mips/int_mips.cc
+++ compiler/dex/quick/mips/int_mips.cc
@@ -278,14 +278,6 @@ void MipsMir2Lir::OpLea(RegStorage r_base, RegStorage reg1, RegStorage reg2, int
   LOG(FATAL) << "Unexpected use of OpLea for Arm";
 }
 
-void MipsMir2Lir::OpTlsCmp(ThreadOffset<4> offset, int val) {
-  LOG(FATAL) << "Unexpected use of OpTlsCmp for Arm";
-}
-
-void MipsMir2Lir::OpTlsCmp(ThreadOffset<8> offset, int val) {
-  UNIMPLEMENTED(FATAL) << "Should not be called.";
-}
-
 bool MipsMir2Lir::GenInlinedCas(CallInfo* info, bool is_long, bool is_object) {
   DCHECK_NE(cu_->instruction_set, kThumb2);
   return false;
