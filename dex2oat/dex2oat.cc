8aa84ca4cf1917f99b2ceb39ea5ba33a0a564953
James C Scott <james.c.scott@intel.com>
2014-08-22 07:11:49 -0700

    Selectivity system
    Adds registration and execution hooks for CompilerDriver.
    - With the static function pointers, other libraries like plugin
        loader can add custom logic.
    - Global variables allow to get/set original and used filter level.
    -- Original filter is the level passed to dex2oat.
    -- Used filter is the level to be used after analysis in the selective system.
    
    Adds CompilationUnit to Base PassDriver
    - Creates path for pass_driver to get CompilationUnit in order
       for special_selection function pointer in base class to get data
       about current method being compiled.
    
    Adds three additional compiler filter levels: O1, O2, O3.
    Signed-off-by: James C Scott <james.c.scott@intel.com>
    
    Change-Id: I08645ef1c3db2cbae9ec20226309766660fbfd92

--- dex2oat/dex2oat.cc
+++ dex2oat/dex2oat.cc
@@ -44,6 +44,7 @@
 #include "dex/quick/dex_file_to_method_inliner_map.h"
 #include "driver/compiler_driver.h"
 #include "driver/compiler_options.h"
+#include "driver/selectivity.h"
 #include "elf_fixup.h"
 #include "elf_patcher.h"
 #include "elf_stripper.h"
@@ -852,6 +853,10 @@ static int dex2oat(int argc, char** argv) {
   std::string profile_file;
   double top_k_profile_threshold = CompilerOptions::kDefaultTopKProfileThreshold;
 
+  // Pass logic and data.
+  std::string disable_passes;
+  bool print_pass_names = false;
+
   bool is_host = false;
   bool dump_stats = false;
   bool dump_timing = false;
@@ -862,6 +867,7 @@ static int dex2oat(int argc, char** argv) {
   bool dump_slow_timing = kIsDebugBuild;
   bool watch_dog_enabled = true;
   bool generate_gdb_information = kIsDebugBuild;
+  bool use_selectivity_analysis = false;
 
   // Checks are all explicit until we know the architecture.
   bool implicit_null_checks = false;
@@ -1030,11 +1036,12 @@ static int dex2oat(int argc, char** argv) {
       // No profile
     } else if (option.starts_with("--top-k-profile-threshold=")) {
       ParseDouble(option.data(), '=', 0.0, 100.0, &top_k_profile_threshold);
+    } else if (option == "--use-selectivity-analysis") {
+      use_selectivity_analysis = true;
     } else if (option == "--print-pass-names") {
-      PassDriverMEOpts::PrintPassNames();
+      print_pass_names = true;
     } else if (option.starts_with("--disable-passes=")) {
-      std::string disable_passes = option.substr(strlen("--disable-passes=")).data();
-      PassDriverMEOpts::CreateDefaultPassList(disable_passes);
+      disable_passes = option.substr(strlen("--disable-passes=")).data();
     } else if (option.starts_with("--print-passes=")) {
       std::string print_passes = option.substr(strlen("--print-passes=")).data();
       PassDriverMEOpts::SetPrintPassList(print_passes);
@@ -1178,6 +1185,17 @@ static int dex2oat(int argc, char** argv) {
     Usage("Unknown --compiler-filter value %s", compiler_filter_string);
   }
 
+  // Store the compiler_filter for the selectivity system.
+  Selectivity::GetInstance()->SetOriginalCompilerFilter(compiler_filter);
+
+  if (!disable_passes.empty()) {
+      PassDriverMEOpts::CreateDefaultPassList(disable_passes);
+  }
+
+  if (print_pass_names) {
+      PassDriverMEOpts::PrintPassNames();
+  }
+
   // Set the compilation target's implicit checks options.
   switch (instruction_set) {
     case kArm:
@@ -1262,6 +1280,7 @@ static int dex2oat(int argc, char** argv) {
 
   std::unique_ptr<VerificationResults> verification_results(new VerificationResults(
                                                             compiler_options.get()));
+  Selectivity::GetInstance()->ToggleAnalysis(use_selectivity_analysis, disable_passes);
   DexFileToMethodInlinerMap method_inliner_map;
   QuickCompilerCallbacks callbacks(verification_results.get(), &method_inliner_map);
   runtime_options.push_back(std::make_pair("compilercallbacks", &callbacks));
