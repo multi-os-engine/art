3ad8f42e0766424912a8d43b724d2c4d9367558c
Sebastien Hertz <shertz@google.com>
2014-08-28 18:25:09 +0200

    [WIP] Enable computed goto interpreter on the host
    We disabled the computed goto interpreter when compiling with clang because of
    compilation issues. Since we compile with clang on the host, we used the
    switch-based interpreter.
    
    Now that clang is able to compile the computed goto interpreter, we can use it by
    default on the host.
    
    Change-Id: Ifa2a2d3c171b1116ecd3b3f213382c3c85e93f66

--- runtime/Android.mk
+++ runtime/Android.mk
@@ -80,6 +80,7 @@ LIBART_COMMON_SRC_FILES := \
   intern_table.cc \
   interpreter/interpreter.cc \
   interpreter/interpreter_common.cc \
+  interpreter/interpreter_goto_table_impl.cc \
   interpreter/interpreter_switch_impl.cc \
   java_vm_ext.cc \
   jdwp/jdwp_event.cc \
@@ -201,8 +202,7 @@ LIBART_COMMON_SRC_FILES += \
   entrypoints/quick/quick_trampoline_entrypoints.cc
 
 # Source files that only compile with GCC.
-LIBART_GCC_ONLY_SRC_FILES := \
-  interpreter/interpreter_goto_table_impl.cc
+LIBART_GCC_ONLY_SRC_FILES :=
 
 LIBART_TARGET_LDFLAGS :=
 LIBART_HOST_LDFLAGS :=
