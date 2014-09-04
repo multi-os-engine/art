c2fafc5a40642c0fd78c0dcff9d742afc60e7e32
Tong Shen <endlessroad@google.com>
2014-09-03 17:24:56 -0700

    ART: Prepare for ELF64.
    Only expose necessary interface in ElfFile, and move all details into template class ElfFileImpl.
    
    Change-Id: I9df2bbc55f32ba0ba91f4f3d5d0009e84a2ddf74

--- compiler/Android.mk
+++ compiler/Android.mk
@@ -120,9 +120,7 @@ LIBART_COMPILER_SRC_FILES := \
 	utils/scoped_arena_allocator.cc \
 	buffered_output_stream.cc \
 	compiler.cc \
-	elf_fixup.cc \
 	elf_patcher.cc \
-	elf_stripper.cc \
 	elf_writer.cc \
 	elf_writer_quick.cc \
 	file_output_stream.cc \
