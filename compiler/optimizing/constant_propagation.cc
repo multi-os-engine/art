0f34301c61fb9b37b1eff0e4342e56fd97ad6481
Roland Levillain <rpl@google.com>
2014-09-05 15:31:51 +0100

    Initiate a simple constant propagation pass.
    - Perform constant folding on int and long additions and subtractions in
      the optimizing compiler.
    - Introduce a HArithmeticBinaryOperation node type on top of HAdd and
      HSub in the control-flow graphs of the optimizing compiler, just
      like HCondition for condition nodes (HEqual, HNotEqual, etc.).
    - Apply constant folding to conditions.
    
    Change-Id: Ic88783a3c975fda777c74c531e257fa777be42eb

---
+++ compiler/optimizing/constant_propagation.cc
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "constant_propagation.h"
+
+namespace art {
+
+void ConstantPropagation::Run() {
+  // Process nodes (basic blocks) in reverse (forward) post-order in
+  // the dominator tree.
+  for (HReversePostOrderIterator it(*graph_); !it.Done(); it.Advance()) {
+    HBasicBlock* block = it.Current();
+    // Traverse this block's instructions in (forward) order and
+    // replace the ones that can be statically evaluated by a
+    // compile-time counterpart.
+    for (HInstructionIterator it(block->GetInstructions());
+         !it.Done(); it.Advance()) {
+      HInstruction* inst = it.Current();
+
+      /* Constant folding: replace `c <- a op b' with a compile-time
+         evaluation of `a op b' if `a' and `b' are constant.  */
+      if (inst != nullptr && inst->IsArithmeticBinaryOperation()) {
+        HArithmeticBinaryOperation* binop = inst->AsArithmeticBinaryOperation();
+        HConstant* constant = binop->TryStaticEvaluation();
+        if (constant != nullptr) {
+          // Replace `inst` with a compile-time constant.
+          inst->GetBlock()->InsertInstructionBefore(constant, inst);
+          inst->ReplaceWith(constant);
+          inst->GetBlock()->RemoveInstruction(inst);
+        }
+      }
+
+      // Constant condition.
+      if (inst != nullptr && inst->IsCondition()) {
+        HCondition* binop = inst->AsCondition();
+        HConstant* constant = binop->TryStaticEvaluation();
+        if (constant != nullptr) {
+          // Replace `inst` with a compile-time constant.
+          inst->GetBlock()->InsertInstructionBefore(constant, inst);
+          inst->ReplaceWith(constant);
+          inst->GetBlock()->RemoveInstruction(inst);
+        }
+      }
+    }
+  }
+}
+
+}  // namespace art
