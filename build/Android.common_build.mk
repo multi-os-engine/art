fa0ad3116fe101be7dda6dbe92d9dd0f117b7544
Chih-Hung Hsieh <chh@google.com>
2014-09-10 14:05:06 -0700

    Increase stack frame size limit to 3200.
    BUG: 17444504
    Change-Id: I1db6573b9c1794fa6c48e0326889fe14b8ff58d2

--- build/Android.common_build.mk
+++ build/Android.common_build.mk
@@ -193,7 +193,7 @@ art_target_non_debug_cflags := \
 ifeq ($(HOST_OS),linux)
   # Larger frame-size for host clang builds today
   art_host_non_debug_cflags += -Wframe-larger-than=2600
-  art_target_non_debug_cflags += -Wframe-larger-than=1728
+  art_target_non_debug_cflags += -Wframe-larger-than=3200
 endif
 
 # FIXME: upstream LLVM has a vectorizer bug that needs to be fixed
