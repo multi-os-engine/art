b4de537e2769eb9f7ce3fefe880c816d8c20e643
Roland Levillain <rpl@google.com>
2014-09-16 14:48:16 +0100

    Start visitors checking CFGs and SSA forms in the optimizing compiler.
    CFG checks:
    - Ensure that the predecessors and successors of a basic block are
      consistent within a control-flow graph.
    - Ensure basic blocks end with a branch instruction.
    - Detect phi functions listed in non-phi instruction lists and vice
      versa.
    - Ensure a block's instructions (and phi functions) are associated
      with this very block.
    
    SSA form checks:
    - Ensure an instruction dominates all its uses.
    - Ensure there is no critical edge.
    
    Change-Id: I1c12b4a61ecf608682152c897980ababa7eca847

--- compiler/optimizing/optimizing_unit_test.h
+++ compiler/optimizing/optimizing_unit_test.h
@@ -17,6 +17,10 @@
 #ifndef ART_COMPILER_OPTIMIZING_OPTIMIZING_UNIT_TEST_H_
 #define ART_COMPILER_OPTIMIZING_OPTIMIZING_UNIT_TEST_H_
 
+#include "nodes.h"
+#include "builder.h"
+#include "dex_file.h"
+#include "dex_instruction.h"
 #include "ssa_liveness_analysis.h"
 
 namespace art {
@@ -46,6 +50,15 @@ LiveInterval* BuildInterval(const size_t ranges[][2],
   }
   interval->SetRegister(reg);
   return interval;
+}
+
+// Create a control-flow graph from Dex instructions.
+inline HGraph* CreateCFG(ArenaAllocator* allocator, const uint16_t* data) {
+  HGraphBuilder builder(allocator);
+  const DexFile::CodeItem* item =
+    reinterpret_cast<const DexFile::CodeItem*>(data);
+  HGraph* graph = builder.BuildGraph(*item);
+  return graph;
 }
 
 }  // namespace art
