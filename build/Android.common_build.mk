34d6dd967b3c542e5a607e8ef442819f9aa6db45
Duane Sand <duane.sand@imgtec.com>
2014-09-22 10:16:28 -0700

    Workaround for strict-aliasing violation
    Disable -fstrict-aliasing code optimizations until
    violations of strict-aliasing rules by runtime source
    are corrected.
    
    Mips 4.9 gcc (with mips32r1 target) mis-optimizes
    runtime/mirror/array, leading to a garbage reg value returned
    by new_array.Get(), followed by failed IsExceptionPending
    assertion.  The bad load/store scheduling optimization was
    enabled by alias-set analysis, common to all front ends.
    ObjectReference<Array> and ObjectReference<Object> are two
    different classes and become two different aliasing sets.
    The bad codeopt disappears with -fno-strict-aliasing.
    
    A simplified example of the source code also fails on x86.
    See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=63412
    for more details.  This particular violation of strict-aliasing
    rules was not detected by -wstrict-aliasing.
    
    This alias analysis problem can affect all gcc targets and
    both 4.8 and 4.9 toolchains.  When and where it results in an
    actual codeopt error depends on the machine and details of
    the optimizations.  (The surfacing of problems in 4.9 mips
    was perhaps due to some degraded optimization.)
    
    Change-Id: Ia06384832ddee0bb13db1add4a50d60dbe474078

--- build/Android.common_build.mk
+++ build/Android.common_build.mk
@@ -157,7 +157,7 @@ art_cflags := \
   -Wno-sign-promo \
   -Wno-unused-parameter \
   -Wstrict-aliasing \
-  -fstrict-aliasing \
+  -fno-strict-aliasing \
   -fvisibility=protected
 
 ART_TARGET_CLANG_CFLAGS :=
