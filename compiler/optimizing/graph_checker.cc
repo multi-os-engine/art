3c18603979200e7e2e13cf1f5e6d89f452baf551
Roland Levillain <rpl@google.com>
2014-10-01 10:48:29 +0100

    Have SSAChecker::VisitInstruction match its documentation.
    Actually inspect the uses of an instruction to ensure the
    latter dominates all of the former, instead of browsing the
    inputs of this instruction (to ensure they dominate the
    instruction).
    
    Change-Id: I967f34a45f48930607bf9683180d02e7c27b4e06

--- compiler/optimizing/graph_checker.cc
+++ compiler/optimizing/graph_checker.cc
@@ -264,18 +264,16 @@ void SSAChecker::CheckLoop(HBasicBlock* loop_header) {
 void SSAChecker::VisitInstruction(HInstruction* instruction) {
   super_type::VisitInstruction(instruction);
 
-  // Ensure an instruction dominates all its uses (or in the present
-  // case, that all uses of an instruction (used as input) are
-  // dominated by its definition).
-  for (HInputIterator input_it(instruction); !input_it.Done();
-       input_it.Advance()) {
-    HInstruction* input = input_it.Current();
-    if (!input->Dominates(instruction)) {
+  // Ensure an instruction dominates all its uses.
+  for (HUseIterator<HInstruction> use_it(instruction->GetUses());
+       !use_it.Done(); use_it.Advance()) {
+    HInstruction* use = use_it.Current()->GetUser();
+    if (!instruction->Dominates(use)) {
       std::stringstream error;
-      error << "Instruction " << input->GetId()
-            << " in block " << input->GetBlock()->GetBlockId()
-            << " does not dominate use " << instruction->GetId()
-            << " in block " << current_block_->GetBlockId() << ".";
+      error << "Instruction " << instruction->GetId()
+            << " in block " << current_block_->GetBlockId()
+            << " does not dominate use " << use->GetId()
+            << " in block " << use->GetBlock()->GetBlockId() << ".";
       errors_.Insert(error.str());
     }
   }
