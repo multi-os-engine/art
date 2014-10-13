7b8c92b705ce310f4cc35deed5e7d6fc06623564
Yevgeny Rouban <yevgeny.y.rouban@intel.com>
2014-10-13 19:10:20 +0700

    ART: fix 32-bit wide GenInlinedMinMax for unused result
    As the number of registers is tight in the 32-bit wide
    GenInlinedMinMax case, the lowering starts with reading VR assigned
    to the result of the min/max call to compare it with VRs of src1 and
    src2. But the result may be unassigned to any VR. And the SRegToVReg
    call is invalid in this case.
    
    The patch just sets the result's VR to -1 making it different from
    sources' VRs, which cannot be INVALID as the min/max call arguments.
    
    Another option would be to not generate anything at all if the
    result is unassigned to a VR, but it could be unsafe for those who
    expects the result in the AX:DX pair.
    
    Change-Id: I172bf9facd62ac4df16bd9e0b11fe472e04424f5
    Signed-off-by: Serguei Katkov <serguei.i.katkov@intel.com>
    Signed-off-by: Yevgeny Rouban <yevgeny.y.rouban@intel.com>

--- compiler/dex/quick/x86/int_x86.cc
+++ compiler/dex/quick/x86/int_x86.cc
@@ -864,7 +864,8 @@ bool X86Mir2Lir::GenInlinedMinMax(CallInfo* info, bool is_min, bool is_long) {
      * inadvertently clobber the second element with the first one thus
      * yielding the wrong result. Thus we do a swap in that case.
      */
-    res_vreg = mir_graph_->SRegToVReg(rl_dest.s_reg_low);
+    res_vreg = rl_dest.s_reg_low == INVALID_SREG ?
+               -1 : mir_graph_->SRegToVReg(rl_dest.s_reg_low);
     src2_vreg = mir_graph_->SRegToVReg(rl_src2.s_reg_low);
     if (res_vreg == src2_vreg) {
       std::swap(rl_src1, rl_src2);
