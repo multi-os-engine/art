74518d436d4379c8697c4db1dbed0f1c40b50460
Igor Murashkin <iam@google.com>
2014-10-22 11:37:02 -0700

    ART: Add support for patching and loading OAT files compiled with PIC
    * Images (.art) compiled with pic now have a new field added.
    * isDexOptNeeded will now skip patch-ing for apps compiled PIC
    * First-boot patching now only copies boot.art, boot.oat is linked
    
    As a result, all system preopted dex files (with --compile-pic) no
    longer take up any space in /data/dalvik-cache/<isa>.
    
    Bug: 18035729
    Change-Id: Ie1acad81a0fd8b2f24e1f3f07a06e6fdb548be62

--- runtime/image.h
+++ runtime/image.h
@@ -39,7 +39,8 @@ class PACKED(4) ImageHeader {
               uint32_t oat_file_begin,
               uint32_t oat_data_begin,
               uint32_t oat_data_end,
-              uint32_t oat_file_end);
+              uint32_t oat_file_end,
+              bool compile_pic_);
 
   bool IsValid() const;
   const char* GetMagic() const;
@@ -121,6 +122,10 @@ class PACKED(4) ImageHeader {
 
   void RelocateImage(off_t delta);
 
+  bool CompilePic() const {
+      return compile_pic_;
+  }
+
  private:
   static const uint8_t kImageMagic[4];
   static const uint8_t kImageVersion[4];
@@ -161,6 +166,9 @@ class PACKED(4) ImageHeader {
 
   // Absolute address of an Object[] of objects needed to reinitialize from an image.
   uint32_t image_roots_;
+
+  // Boolean (0 or 1) to denote if the image was compiled with --compile-pic option
+  uint32_t compile_pic_;
 
   friend class ImageWriter;
 };
