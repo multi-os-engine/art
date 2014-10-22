b01e9d368b930c43ac6e874690dc9f7e6bbb6dcf
Nicolas Geoffray <ngeoffray@google.com>
2014-10-22 16:07:05 +0100

    Add support for static fields in optimizing compiler.
    Change-Id: Id2f010589e2bd6faf42c05bb33abf6816ebe9fa9

--- compiler/utils/arm/constants_arm.h
+++ compiler/utils/arm/constants_arm.h
@@ -47,6 +47,16 @@ namespace arm {
 #error "Exactly one of VFPv3_D16 or VFPv3_D32 can be defined at a time."
 #endif
 
+// Options for the dmb instruction.
+// Order is based on the description order of the options in the reference manual.
+enum DmbOptions {
+  SY = 0xf,
+  ST = 0xe,
+  ISH = 0xb,
+  ISHST = 0xa,
+  NSH = 0x7,
+  NSHST = 0x6
+};
 
 enum ScaleFactor {
   TIMES_1 = 0,
