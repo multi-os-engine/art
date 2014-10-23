c339c2ab07b8bd82e045e548b97690521d87cffa
Ningsheng Jian <ningsheng.jian@arm.com>
2014-10-23 13:48:36 +0800

    ARM: Strength reduction for floating-point division
    For floating-point division by power of two constants, generate
    multiplication by the reciprocal instead.
    
    Change-Id: I39c79eeb26b60cc754ad42045362b79498c755be

--- compiler/dex/quick/mips/fp_mips.cc
+++ compiler/dex/quick/mips/fp_mips.cc
@@ -113,6 +113,18 @@ void MipsMir2Lir::GenArithOpDouble(Instruction::Code opcode,
   StoreValueWide(rl_dest, rl_result);
 }
 
+void MipsMir2Lir::GenMultiplyByConstantFloat(RegLocation rl_dest, RegLocation rl_src1,
+                                             int32_t constant) {
+  // TODO: need mips implementation.
+  LOG(FATAL) << "Unimplemented GenMultiplyByConstantFloat in mips";
+}
+
+void MipsMir2Lir::GenMultiplyByConstantDouble(RegLocation rl_dest, RegLocation rl_src1,
+                                              int64_t constant) {
+  // TODO: need mips implementation.
+  LOG(FATAL) << "Unimplemented GenMultiplyByConstantDouble in mips";
+}
+
 void MipsMir2Lir::GenConversion(Instruction::Code opcode, RegLocation rl_dest,
                                 RegLocation rl_src) {
   int op = kMipsNop;
