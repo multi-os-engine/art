58f44adc2b04bde2a73be60b5c1ba173d1a0dbfa
Calin Juravle <calin@google.com>
2014-11-04 16:40:20 +0000

    [optimizing compiler] Add div-int and exception handling.
    - for backends: arm, x86, x86_64
    - fixed a register allocator bug: the request for a fixed register for
    the first input was ignored if the output was kSameAsFirstInput
    - added divide by zero exception
    - more tests
    
    Change-Id: Id3a515e02bfbc66cd9d16cb9746f7551bdab3d42

--- compiler/optimizing/nodes.h
+++ compiler/optimizing/nodes.h
@@ -476,6 +476,7 @@ class HBasicBlock : public ArenaObject {
   M(Compare, BinaryOperation)                                           \
   M(Condition, BinaryOperation)                                         \
   M(Div, BinaryOperation)                                               \
+  M(DivZeroCheck, Instruction)                                          \
   M(DoubleConstant, Constant)                                           \
   M(Equal, Condition)                                                   \
   M(Exit, Instruction)                                                  \
@@ -1705,6 +1706,28 @@ class HDiv : public HBinaryOperation {
 
  private:
   DISALLOW_COPY_AND_ASSIGN(HDiv);
+};
+
+class HDivZeroCheck : public HExpression<1> {
+ public:
+  HDivZeroCheck(HInstruction* value, uint32_t dex_pc)
+      : HExpression(value->GetType(), SideEffects::None()), dex_pc_(dex_pc) {
+    SetRawInputAt(0, value);
+  }
+
+  bool CanBeMoved() const OVERRIDE { return true; }
+  bool InstructionDataEquals(HInstruction* other) const OVERRIDE { return true; }
+  bool NeedsEnvironment() const OVERRIDE { return true; }
+  bool CanThrow() const OVERRIDE { return true; }
+
+  uint32_t GetDexPc() const { return dex_pc_; }
+
+  DECLARE_INSTRUCTION(DivZeroCheck);
+
+ private:
+  const uint32_t dex_pc_;
+
+  DISALLOW_COPY_AND_ASSIGN(HDivZeroCheck);
 };
 
 // The value of a parameter in this method. Its location depends on
