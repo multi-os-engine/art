498741141448b9463d7ea829d40570062a83a6c4
Serguei Katkov <serguei.i.katkov@intel.com>
2014-11-06 14:45:44 +0600

    Use correct register class for refs
    LoadValue requires thar ref location should reguest kRefReg
    register class. The patch fixes GenFilledNewArray to specify
    the register class correctly.
    
    This is a fix for the crash of dex2oat on 412-new-array unit test.
    This is a second attempt with an additional fix for arm64.
    
    Change-Id: I9f0bb098cd1d1721ef03e8976c1460f8fa49aa2a
    Signed-off-by: Serguei Katkov <serguei.i.katkov@intel.com>

--- compiler/dex/quick/gen_common.cc
+++ compiler/dex/quick/gen_common.cc
@@ -481,9 +481,16 @@ void Mir2Lir::GenFilledNewArray(CallInfo* info) {
   } else if (!info->is_range) {
     // TUNING: interleave
     for (int i = 0; i < elems; i++) {
-      RegLocation rl_arg = LoadValue(info->args[i], kCoreReg);
-      Store32Disp(ref_reg,
-                  mirror::Array::DataOffset(component_size).Int32Value() + i * 4, rl_arg.reg);
+      RegLocation rl_arg;
+      if (info->args[i].ref) {
+        rl_arg = LoadValue(info->args[i], kRefReg);
+        StoreRefDisp(ref_reg,
+                    mirror::Array::DataOffset(component_size).Int32Value() + i * 4, rl_arg.reg, kNotVolatile);
+      } else {
+        rl_arg = LoadValue(info->args[i], kCoreReg);
+        Store32Disp(ref_reg,
+                    mirror::Array::DataOffset(component_size).Int32Value() + i * 4, rl_arg.reg);
+      }
       // If the LoadValue caused a temp to be allocated, free it
       if (IsTemp(rl_arg.reg)) {
         FreeTemp(rl_arg.reg);
