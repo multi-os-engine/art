fff1b4ef9358ed973ce215f18d8f36d421ad026a
Roland Levillain <rpl@google.com>
2014-11-26 16:41:00 +0000

    Fix neg-float & neg-double for null values in opt. compiler.
    - Implement float and double negation as an exclusive or
      with a bit sign mask in x86 and x86-64 code generators.
    - Enable requests of temporary FPU (double) registers during
      code generation and register allocation.
    - Fix art::x86::X86Assembler::LoadLongConstant and extend
      it to int64_t values.
    - Update test cases in test/415-optimizing-arith-neg.
    
    Change-Id: I9572c24b27c645ba698825e60cd5b3956b4895fa

--- compiler/optimizing/register_allocator.cc
+++ compiler/optimizing/register_allocator.cc
@@ -189,11 +189,29 @@ void RegisterAllocator::ProcessInstruction(HInstruction* instruction) {
       BlockRegister(temp, position, position + 1);
     } else {
       DCHECK(temp.IsUnallocated());
-      DCHECK(temp.GetPolicy() == Location::kRequiresRegister);
-      LiveInterval* interval = LiveInterval::MakeTempInterval(allocator_, Primitive::kPrimInt);
-      temp_intervals_.Add(interval);
-      interval->AddRange(position, position + 1);
-      unhandled_core_intervals_.Add(interval);
+      switch (temp.GetPolicy()) {
+        case Location::kRequiresRegister: {
+          LiveInterval* interval =
+              LiveInterval::MakeTempInterval(allocator_, Primitive::kPrimInt);
+          temp_intervals_.Add(interval);
+          interval->AddRange(position, position + 1);
+          unhandled_core_intervals_.Add(interval);
+          break;
+        }
+
+        case Location::kRequiresFpuRegister: {
+          LiveInterval* interval =
+              LiveInterval::MakeTempInterval(allocator_, Primitive::kPrimDouble);
+          temp_intervals_.Add(interval);
+          interval->AddRange(position, position + 1);
+          unhandled_fp_intervals_.Add(interval);
+          break;
+        }
+
+        default:
+          LOG(FATAL) << "Unexpected policy for temporary location "
+                     << temp.GetPolicy();
+      }
     }
   }
 
@@ -1250,7 +1268,8 @@ void RegisterAllocator::Resolve() {
       current = at;
     }
     LocationSummary* locations = at->GetLocations();
-    DCHECK(temp->GetType() == Primitive::kPrimInt);
+    DCHECK(temp->GetType() == Primitive::kPrimInt
+           || temp->GetType() == Primitive::kPrimDouble);
     locations->SetTempAt(
         temp_index++, Location::RegisterLocation(temp->GetRegister()));
   }
