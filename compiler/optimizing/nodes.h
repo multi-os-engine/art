d395e8cb3c4cdb62ecbaab7c8a381176d353356f
Calin Juravle <calin@google.com>
2014-12-02 18:58:03 +0000

    Fix new-instance node.
    new-instance may throw when called on abstract classes or interfaces.
    
    Change-Id: Id55dbb95b906a58c946b14adad934ee0e3498c0a

--- compiler/optimizing/nodes.h
+++ compiler/optimizing/nodes.h
@@ -1669,7 +1669,10 @@ class HNewInstance : public HExpression<0> {
   uint16_t GetTypeIndex() const { return type_index_; }
 
   // Calls runtime so needs an environment.
-  virtual bool NeedsEnvironment() const { return true; }
+  bool NeedsEnvironment() const OVERRIDE { return true; }
+  // Will throw InstantiationError when called on abstract classes or interfaces.
+  // TODO: optimize when possible.
+  bool CanThrow() const OVERRIDE { return true; }
 
   DECLARE_INSTRUCTION(NewInstance);
 
