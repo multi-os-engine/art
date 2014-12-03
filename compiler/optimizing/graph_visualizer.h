b8188ffcd4fdb05c677c546ec89c3d8ae43f894d
Andreas Gampe <agampe@google.com>
2014-12-02 20:03:41 -0800

    ART: Make graph visualizer thread-safe
    Make visualizer printing thread-safe. This allows to use it with
    more than -j1.
    
    Add printing of constant values.
    
    Change-Id: Idc778d39890dbf5162de8debe2b291b432b6b295

--- compiler/optimizing/graph_visualizer.h
+++ compiler/optimizing/graph_visualizer.h
@@ -18,7 +18,9 @@
 #define ART_COMPILER_OPTIMIZING_GRAPH_VISUALIZER_H_
 
 #include <ostream>
+#include <sstream>
 
+#include "base/mutex.h"
 #include "base/value_object.h"
 
 namespace art {
@@ -58,20 +60,34 @@ class HGraphVisualizer : public ValueObject {
                    const CodeGenerator& codegen,
                    const char* name);
 
+  ~HGraphVisualizer() {
+    Finalize();
+  }
+
   /**
    * If this visualizer is enabled, emit the compilation information
-   * in `output_`.
+   * into `oss_`. Actual writing to `output_` will happen in Finalize().
    */
-  void DumpGraph(const char* pass_name) const;
+  void DumpGraph(const char* pass_name);
+
+  /**
+   * Write the temporary buffer from oss_ to output_.
+   */
+  void Finalize() const LOCKS_EXCLUDED(dump_mutex);
 
  private:
+  // This is the final output stream.
   std::ostream* const output_;
+  // This is for temporary internal output, so we can write everything in one go at the end.
+  std::ostringstream oss_;
   HGraph* const graph_;
   const CodeGenerator& codegen_;
 
   // Is true when `output_` is not null, and the compiled method's name
   // contains the string_filter given in the constructor.
   bool is_enabled_;
+
+  static Mutex dump_mutex;
 
   DISALLOW_COPY_AND_ASSIGN(HGraphVisualizer);
 };
