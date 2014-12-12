1bae7803a2271806e4f40b8cee7f044df66495c8
Mathieu Chartier <mathieuc@google.com>
2014-12-12 13:05:33 -0800

    Fix possible deadlock caused by Thread.getAllStackTraces
    Thread.getAllStackTraces suspends all of the threads. If one of
    the threads is a thread which holds the GC daemon lock then we
    may deadlock when we allocate the stack trace. This happens if we
    get a concurrent GC request when we are allocating the stack trace
    elements. To fix the deadlocks we now send the concurrent GC
    requests to a dedicated thread pool.
    
    Also added stress test for Thread.getAllStackTraces. The test calls
    Thread.getAllStackTraces from multiple threads to stress race
    conditions which can cause deadlocks.
    
    Bug: 18661622
    Change-Id: Ideee7073fdcce784414808d08eca9ea310240073

--- runtime/gc/heap.cc
+++ runtime/gc/heap.cc
@@ -699,10 +699,13 @@ void Heap::UpdateProcessState(ProcessState process_state) {
 }
 
 void Heap::CreateThreadPool() {
+  // Need at least one thread for concurrent GC reuqests.
   const size_t num_threads = std::max(parallel_gc_threads_, conc_gc_threads_);
   if (num_threads != 0) {
     thread_pool_.reset(new ThreadPool("Heap thread pool", num_threads));
   }
+  concurrent_gc_request_thread_pool_.reset(
+      new ThreadPool("Concurrent GC request thread pool", 1U));
 }
 
 void Heap::VisitObjects(ObjectCallback callback, void* arg) {
@@ -739,6 +742,7 @@ void Heap::MarkAllocStackAsLive(accounting::ObjectStack* stack) {
 
 void Heap::DeleteThreadPool() {
   thread_pool_.reset(nullptr);
+  concurrent_gc_request_thread_pool_.reset(nullptr);
 }
 
 void Heap::AddSpace(space::Space* space) {
@@ -3025,10 +3029,26 @@ void Heap::AddFinalizerReference(Thread* self, mirror::Object** object) {
   *object = soa.Decode<mirror::Object*>(arg.get());
 }
 
+class ConcurrentGCRequestTask : public Task {
+ public:
+  virtual void Finalize() {
+    delete this;
+  }
+
+  // Scans all of the objects
+  virtual void Run(Thread* self) OVERRIDE {
+    ScopedObjectAccess soa(self);
+    Runtime::Current()->GetHeap()->RequestConcurrentGC(self);
+  }
+};
+
 void Heap::RequestConcurrentGCAndSaveObject(Thread* self, mirror::Object** obj) {
   StackHandleScope<1> hs(self);
   HandleWrapper<mirror::Object> wrapper(hs.NewHandleWrapper(obj));
-  RequestConcurrentGC(self);
+  // Careful, this can deadlock the GC if the GC uses Thread:Wait without do_work = true
+  // since there may be a task which is blocked on the mutator lock.
+  concurrent_gc_request_thread_pool_->AddTask(self, new ConcurrentGCRequestTask());
+  concurrent_gc_request_thread_pool_->StartWorkers(self);
 }
 
 void Heap::RequestConcurrentGC(Thread* self) {
