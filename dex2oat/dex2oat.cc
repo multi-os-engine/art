06e374f15db3624c3550003772d9a8b8b85597c7
Douglas Leung <douglas.leung@imgtec.com>
2015-01-06 20:50:25 -0800

    ART: Initial mips32r6 and mips64r6 support.
    This patch does not break mips32r2 functionality.
    mips32r6 and mips64r6 are working in interpreter mode.
    mips32r6 and mips64r6 quick mode are WIP.
    
    Change-Id: I5079120f41756b74b2e5a9c3dad0900688af77e9

--- dex2oat/dex2oat.cc
+++ dex2oat/dex2oat.cc
@@ -34,6 +34,8 @@
 #include <cutils/trace.h>
 
 #include "arch/instruction_set_features.h"
+#include "arch/mips/instruction_set_features_mips.h"
+#include "arch/mips64/instruction_set_features_mips64.h"
 #include "base/dumpable.h"
 #include "base/stl_util.h"
 #include "base/stringpiece.h"
@@ -147,7 +149,7 @@ static void UsageError(const char* fmt, ...) {
   UsageError("      Example: --android-root=out/host/linux-x86");
   UsageError("      Default: $ANDROID_ROOT");
   UsageError("");
-  UsageError("  --instruction-set=(arm|arm64|mips|x86|x86_64): compile for a particular");
+  UsageError("  --instruction-set=(arm|arm64|mips|mips64|x86|x86_64): compile for a particular");
   UsageError("      instruction set.");
   UsageError("      Example: --instruction-set=x86");
   UsageError("      Default: arm");
@@ -852,7 +854,14 @@ class Dex2Oat FINAL {
     }
 
     if (compiler_filter_string == nullptr) {
-      if (instruction_set_ == kMips64) {
+      if (instruction_set_ == kMips &&
+          reinterpret_cast<const MipsInstructionSetFeatures*>(instruction_set_features_.get())->
+          IsR6()) {
+        // For R6, only interpreter mode is working.
+        // TODO: fix compiler for Mips32r6.
+        compiler_filter_string = "interpret-only";
+      } else if (instruction_set_ == kMips64) {
+        // For Mips64, only interpreter mode is working (regardless of R6).
         // TODO: fix compiler for Mips64.
         compiler_filter_string = "interpret-only";
       } else if (image_) {
