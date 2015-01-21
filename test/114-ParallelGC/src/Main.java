61d1df6fddd76bac401b00720d0c400b062323b2
Andreas Gampe <agampe@google.com>
2015-01-20 21:26:43 -0800

    ART: Rewrite ParallelGC test
    Make the setup simpler and less prone to OOME problems.
    
    Change-Id: Ie2dbe1579a45d554f207a9bf84d53303acbe79d5

--- test/114-ParallelGC/src/Main.java
+++ test/114-ParallelGC/src/Main.java
@@ -16,54 +16,45 @@
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main implements Runnable {
 
     // Timeout in minutes. Make it larger than the run-test timeout to get a native thread dump by
     // ART on timeout when running on the host.
-    public final static long TIMEOUT_VALUE = 12;
+    public final static long TIMEOUT_VALUE = 7;
 
     public final static long MAX_SIZE = 1000;  // Maximum size of array-list to allocate.
 
+    public final static int THREAD_COUNT = 16;
+    public final static AtomicInteger counter = new AtomicInteger();
+    public final static Object gate = new Object();
+
     public static void main(String[] args) throws Exception {
-        Thread[] threads = new Thread[16];
+        Thread[] threads = new Thread[THREAD_COUNT];
 
-        // Use a cyclic system of synchronous queues to pass a boolean token around.
-        //
-        // The combinations are:
-        //
-        // Worker receives:    true     false    false    true
-        // Worker has OOM:     false    false    true     true
-        //    |
-        //    v
-        // Value to pass:      true     false    false    false
-        // Exit out of loop:   false    true     true     true
-        // Wait on in queue:   true     false    false    true
-        //
-        // Finally, the workers are supposed to wait on the barrier to synchronize the GC run.
-
-        CyclicBarrier barrier = new CyclicBarrier(threads.length);
-        List<SynchronousQueue<Boolean>> queues = new ArrayList<SynchronousQueue<Boolean>>(
-            threads.length);
-        for (int i = 0; i < threads.length; i++) {
-            queues.add(new SynchronousQueue<Boolean>());
-        }
+        // We use two barriers. The first one ensures that the threads can start their allocation
+        // run around approximately the same time. The second one ensures that they all wait before
+        // ending.
+        // Note: We could reuse the first barrier, but it's simpler to just have two.
 
         for (int i = 0; i < threads.length; i++) {
-            threads[i] = new Thread(new Main(i, queues.get(i), queues.get((i + 1) % threads.length),
-                                             barrier));
+            threads[i] = new Thread(new Main());
         }
         for (Thread thread : threads) {
             thread.start();
         }
-
-        // Push off the cycle.
-        checkTimeout(queues.get(0).offer(Boolean.TRUE, TIMEOUT_VALUE, TimeUnit.MINUTES));
+        // Sleep a little bit to give the threads time to gate.wait.
+        try {
+            Thread.sleep(250);
+        } catch (Exception exc) {
+            // Any exception is bad.
+            exc.printStackTrace(System.err);
+            System.exit(1);
+        }
+        synchronized (gate) {
+            gate.notifyAll();
+        }
 
         // Wait for the threads to finish.
         for (Thread thread : threads) {
@@ -72,85 +63,77 @@ public class Main implements Runnable {
 
         // Allocate objects to definitely run GC before quitting.
         try {
-            for (int i = 0; i < 1000; i++) {
-                new ArrayList<Object>(i);
+            ArrayList<Object> l = new ArrayList<Object>();
+            for (int i = 0; i < 100000; i++) {
+                l.add(new ArrayList<Object>(i));
             }
         } catch (OutOfMemoryError oom) {
         }
+        new ArrayList<Object>(50);
     }
 
-    private static void checkTimeout(Object o) {
-        checkTimeout(o != null);
+    private Main() {
     }
 
-    private static void checkTimeout(boolean b) {
-        if (!b) {
-            // Something went wrong.
-            System.out.println("Bad things happened, timeout.");
-            System.exit(1);
-        }
-    }
-
-    private final int id;
-    private final SynchronousQueue<Boolean> waitOn;
-    private final SynchronousQueue<Boolean> pushTo;
-    private final CyclicBarrier finalBarrier;
-
-    private Main(int id, SynchronousQueue<Boolean> waitOn, SynchronousQueue<Boolean> pushTo,
-        CyclicBarrier finalBarrier) {
-        this.id = id;
-        this.waitOn = waitOn;
-        this.pushTo = pushTo;
-        this.finalBarrier = finalBarrier;
-    }
+    private ArrayList<Object> store;
 
     public void run() {
+        ArrayList<Object> l = new ArrayList<Object>();
+        store = l;  // Keep it alive.
+
+        // Wait for the start signal.
+        synchronized (gate) {
+            try {
+                gate.wait(TIMEOUT_VALUE * 1000 * 60);
+            } catch (Exception exc) {
+                // Any exception is bad.
+                exc.printStackTrace(System.err);
+                System.exit(1);
+            }
+        }
+
+        // Allocate.
         try {
-            work();
+            for (int i = 0; i < MAX_SIZE; i++) {
+                l.add(new ArrayList<Object>(i));
+            }
+        } catch (OutOfMemoryError oome) {
+            // Fine, we're done.
         } catch (Exception exc) {
-            // Any exception is bad.
+            // Any other exception is bad.
             exc.printStackTrace(System.err);
             System.exit(1);
         }
-    }
 
-    public void work() throws BrokenBarrierException, InterruptedException, TimeoutException {
-        ArrayList<Object> l = new ArrayList<Object>();
+        // Atomically increment the counter and check whether we were last.
+        int number = counter.incrementAndGet();
 
-        // Main loop.
-        for (int i = 0; ; i++) {
-          Boolean receivedB = waitOn.poll(TIMEOUT_VALUE, TimeUnit.MINUTES);
-          checkTimeout(receivedB);
-          boolean received = receivedB;
-
-          // This is the first stage, try to allocate up till MAX_SIZE.
-          boolean oom = i >= MAX_SIZE;
-          try {
-            l.add(new ArrayList<Object>(i));
-          } catch (OutOfMemoryError oome) {
-            oom = true;
-          }
-
-          if (!received || oom) {
-            // First stage, always push false.
-            checkTimeout(pushTo.offer(Boolean.FALSE, TIMEOUT_VALUE, TimeUnit.MINUTES));
-
-            // If we received true, wait for the false to come around.
-            if (received) {
-              checkTimeout(waitOn.poll(TIMEOUT_VALUE, TimeUnit.MINUTES));
+        if (number < THREAD_COUNT) {
+            // Not last, wait.
+            synchronized (gate) {
+                try {
+                    gate.wait(TIMEOUT_VALUE * 1000 * 60);
+                } catch (Exception exc) {
+                    // Any exception is bad.
+                    exc.printStackTrace(System.err);
+                    System.exit(1);
+                }
             }
-
-            // Break out of the loop.
-            break;
-          } else {
-            // Pass on true.
-            checkTimeout(pushTo.offer(Boolean.TRUE, TIMEOUT_VALUE, TimeUnit.MINUTES));
-          }
+        } else {
+            // Last. Sleep for a bit so the other threads can get to the gate.wait...
+            try {
+                Thread.sleep(250);
+            } catch (Exception exc) {
+             // Any exception is bad.
+                exc.printStackTrace(System.err);
+                System.exit(1);
+            }
+            // Now wake up everyone.
+            synchronized (gate) {
+                gate.notifyAll();
+            }
         }
 
-        // We have reached the final point. Wait on the barrier, but at most a minute.
-        finalBarrier.await(TIMEOUT_VALUE, TimeUnit.MINUTES);
-
-        // Done.
+        store = null;  // Allow GC to reclaim it.
     }
 }
