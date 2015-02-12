5a9c0c0383e40f15cf3f10cd408464fb3e156d89
Calin Juravle <calin@google.com>
2015-02-12 15:25:22 +0000

    Reference type propagation
    - propagate reference types between instructions
    - remove checked casts when possible
    - add StackHandleScopeCollection to manage an arbitrary number of stack
    handles (see comments)
    
    Change-Id: I31200067c5e7375a5ea8e2f873c4374ebdb5ee60

--- compiler/optimizing/nodes.h
+++ compiler/optimizing/nodes.h
@@ -18,8 +18,11 @@
 #define ART_COMPILER_OPTIMIZING_NODES_H_
 
 #include "entrypoints/quick/quick_entrypoints_enum.h"
+#include "handle.h"
+#include "handle_scope.h"
 #include "invoke_type.h"
 #include "locations.h"
+#include "mirror/class.h"
 #include "offsets.h"
 #include "primitive.h"
 #include "utils/arena_object.h"
@@ -864,6 +867,97 @@ class HEnvironment : public ArenaObject<kArenaAllocMisc> {
   DISALLOW_COPY_AND_ASSIGN(HEnvironment);
 };
 
+class ReferenceTypeInfo : ValueObject {
+ public:
+  ReferenceTypeInfo() : is_known_(false), is_exact_(true), is_top_(false) {}
+  explicit ReferenceTypeInfo(Handle<mirror::Class> type_handle) :
+      type_handle_(type_handle), is_known_(true), is_exact_(true), is_top_(false) {}
+
+  bool IsExact() const { return is_exact_; }
+  bool IsTop() const { return is_top_; }
+  bool IsKnown() const { return is_known_; }
+
+  Handle<mirror::Class> GetTypeHandle() const { return type_handle_; }
+
+  void SetTop() {
+    is_top_ = true;
+    is_exact_ = false;
+    is_known_ = true;
+    type_handle_ = Handle<mirror::Class>();
+  }
+  void SetInexact() { is_exact_ = false; }
+  void SetTypeHandle(Handle<mirror::Class> type_handle) {
+    type_handle_ = type_handle;
+    is_known_ = true;
+  }
+
+  bool IsSupertypeOf(ReferenceTypeInfo rti) SHARED_LOCKS_REQUIRED(Locks::mutator_lock_) {
+    if (IsTop()) {
+      // Top (equivalent for java.lang.Object) is supertype of anything.
+      return true;
+    }
+    if (!IsKnown() || !rti.IsKnown()) {
+      // Can't decide when the types are not known.
+      return false;
+    }
+    if (GetTypeHandle()->IsObjectClass()) {
+      // java.lang.Object is a supertype of anything.
+      return true;
+    }
+    if (rti.IsTop()) {
+      // If we get here `this` is not Top() so it can't be a supertype.
+      return false;
+    }
+    return GetTypeHandle()->IsAssignableFrom(rti.GetTypeHandle().Get());
+  }
+
+  // Returns true if the type information are equivalent, i.e. they provide the
+  // same amount of details about the types. Note that it does not mean that the
+  // instructions have the same type (e.g. if neither types are known the RTIs
+  // are equivalent).
+  bool IsEquivalent(ReferenceTypeInfo rti) SHARED_LOCKS_REQUIRED(Locks::mutator_lock_) {
+    if (!IsKnown() && !rti.IsKnown()) {
+      // `Unknown`s are equivalent.
+      return true;
+    }
+    if (IsKnown() ^ rti.IsKnown()) {
+      // If only one is unknown then they are different.
+      return false;
+    }
+
+    if (IsExact() != rti.IsExact()) {
+      return false;
+    }
+
+    bool top_or_object = IsTop() || GetTypeHandle()->IsObjectClass();
+    bool rti_top_or_object = rti.IsTop() || rti.GetTypeHandle()->IsObjectClass();
+    if (top_or_object && rti_top_or_object) {
+      // `Top`s and `java.lang.Object`s are equivalent
+      return true;
+    }
+    if (top_or_object ^ rti_top_or_object) {
+      // If only one is top or object than they are not equivalent.
+      return false;
+    }
+
+    // Finally check the types.
+    return GetTypeHandle().Get() == rti.GetTypeHandle().Get();
+  }
+
+ private:
+  // The class of the object.
+  Handle<mirror::Class> type_handle_;
+  // Whether or not we have any information about this type.
+  bool is_known_;
+  // Whether or not the type is exact (not a subclass/superclass)
+  bool is_exact_;
+  // When true it means that the object type should be java.lang.Object.
+  // We don't have access to the corresponding mirror object every time so this
+  // flag acts as a substitute. When true the TypeHandle will reference a null
+  // pointer.
+  bool is_top_;
+};
+
 class HInstruction : public ArenaObject<kArenaAllocMisc> {
  public:
   explicit HInstruction(SideEffects side_effects)
@@ -917,6 +1011,12 @@ class HInstruction : public ArenaObject<kArenaAllocMisc> {
   virtual bool CanBeNull() const { return true; }
 
   virtual bool CanDoImplicitNullCheck() const { return false; }
+
+  void SetReferenceTypeInfo(ReferenceTypeInfo reference_type_info) {
+    reference_type_info_ = reference_type_info;
+  }
+
+  ReferenceTypeInfo GetReferenceTypeInfo() const { return reference_type_info_; }
 
   void AddUseAt(HInstruction* user, size_t index) {
     uses_.AddUse(user, index, GetBlock()->GetGraph()->GetArena());
@@ -1061,6 +1161,9 @@ class HInstruction : public ArenaObject<kArenaAllocMisc> {
   size_t lifetime_position_;
 
   const SideEffects side_effects_;
+
+  // TODO: for primitive types this should be marked as invalid.
+  ReferenceTypeInfo reference_type_info_;
 
   friend class HBasicBlock;
   friend class HGraph;
@@ -2658,6 +2761,14 @@ class HLoadClass : public HExpression<0> {
     return !is_referrers_class_;
   }
 
+  ReferenceTypeInfo GetLoadedClassRTI() {
+    return loaded_class_rti_;
+  }
+
+  void SetLoadedClassRTI(ReferenceTypeInfo rti) {
+    loaded_class_rti_ = rti;
+  }
+
   DECLARE_INSTRUCTION(LoadClass);
 
  private:
@@ -2667,6 +2778,8 @@ class HLoadClass : public HExpression<0> {
   // Whether this instruction must generate the initialization check.
   // Used for code generation.
   bool generate_clinit_check_;
+
+  ReferenceTypeInfo loaded_class_rti_;
 
   DISALLOW_COPY_AND_ASSIGN(HLoadClass);
 };
--- compiler/optimizing/reference_type_propagation.cc
+++ compiler/optimizing/reference_type_propagation.cc
@@ -16,16 +16,34 @@
 
 #include "reference_type_propagation.h"
 
+#include "class_linker.h"
+#include "mirror/class-inl.h"
+#include "mirror/dex_cache.h"
+#include "scoped_thread_state_change.h"
+
 namespace art {
 
 // TODO: Only do the analysis on reference types. We currently have to handle
 // the `null` constant, that is represented as a `HIntConstant` and therefore
 // has the Primitive::kPrimInt type.
 
-void ReferenceTypePropagation::Run() {
-  // Compute null status for instructions.
+// TODO: handle:
+//  public Main ifNullTest(int count, Main a) {
+//    Main m = new Main();
+//    if (a == null) {
+//      a = m;
+//    }
+//    return a.g();
+//  }
+//  public Main ifNotNullTest(Main a) {
+//    if (a != null) {
+//      return a.g();
+//    }
+//    return new Main();
+//  }
 
-  // To properly propagate not-null info we need to visit in the dominator-based order.
+void ReferenceTypePropagation::Run() {
+  // To properly propagate type info we need to visit in the dominator-based order.
   // Reverse post order guarantees a node's dominators are visited first.
   // We take advantage of this order in `VisitBasicBlock`.
   for (HReversePostOrderIterator it(*graph_); !it.Done(); it.Advance()) {
@@ -47,8 +65,89 @@ bool ReferenceTypePropagation::UpdateNullability(HPhi* phi) {
   return existing_can_be_null != new_can_be_null;
 }
 
+bool ReferenceTypePropagation::UpdateReferenceTypeInfo(HPhi* phi)
+    SHARED_LOCKS_REQUIRED(Locks::mutator_lock_) {
+  ReferenceTypeInfo existing_rti = phi->GetReferenceTypeInfo();
+  if (existing_rti.IsTop()) {
+    return false;
+  }
+
+  ReferenceTypeInfo new_rti = existing_rti;
+
+  for (size_t i = 0; i < phi->InputCount(); i++) {
+    ReferenceTypeInfo input_rti = phi->InputAt(i)->GetReferenceTypeInfo();
+
+    if (!new_rti.IsKnown()) {
+      new_rti = input_rti;
+      continue;
+    }
+    if (!input_rti.IsKnown()) {
+      continue;  // Keep the existing type
+    }
+    if (input_rti.IsTop()) {
+      new_rti.SetTop();
+      break;
+    }
+
+    if (!input_rti.IsExact()) {
+      new_rti.SetInexact();
+    }
+
+    Handle<mirror::Class> phi_type = new_rti.GetTypeHandle();
+    Handle<mirror::Class> input_type = input_rti.GetTypeHandle();
+    DCHECK(phi_type.Get() != nullptr);
+    DCHECK(input_type.Get() != nullptr);
+    if (phi_type.Get() == input_type.Get()) {
+      // nothing to do if we have the same type
+    } else if (input_type->IsAssignableFrom(phi_type.Get())) {
+      new_rti.SetTypeHandle(input_rti.GetTypeHandle());
+      new_rti.SetInexact();
+    } else if (phi_type->IsAssignableFrom(input_type.Get())) {
+      new_rti.SetInexact();
+    } else {
+      new_rti.SetTop();
+    }
+  }
+  phi->SetReferenceTypeInfo(new_rti);
+
+  return !new_rti.IsEquivalent(existing_rti);
+}
+
+void ReferenceTypePropagation::VisitNewInstance(HNewInstance* instr) {
+  ScopedObjectAccess soa(Thread::Current());
+  mirror::DexCache* dex_cache = dex_compilation_unit_.GetClassLinker()->FindDexCache(dex_file_);
+  // Get type from dex cache assuming it was populated by the verifier.
+  mirror::Class* resolved_class = dex_cache->GetResolvedType(instr->GetTypeIndex());
+  if (resolved_class != nullptr) {
+    MutableHandle<mirror::Class> handle = handles_->NewHandle(resolved_class);
+    instr->SetReferenceTypeInfo(ReferenceTypeInfo(handle));
+  }
+}
+
+void ReferenceTypePropagation::VisitLoadClass(HLoadClass* instr) {
+  ScopedObjectAccess soa(Thread::Current());
+  mirror::DexCache* dex_cache = dex_compilation_unit_.GetClassLinker()->FindDexCache(dex_file_);
+  // Get type from dex cache assuming it was populated by the verifier.
+  mirror::Class* resolved_class = dex_cache->GetResolvedType(instr->GetTypeIndex());
+  if (resolved_class != nullptr) {
+    Handle<mirror::Class> handle = handles_->NewHandle(resolved_class);
+    instr->SetLoadedClassRTI(ReferenceTypeInfo(handle));
+  }
+  Handle<mirror::Class> class_handle = handles_->NewHandle(mirror::Class::GetJavaLangClass());
+  instr->SetReferenceTypeInfo(ReferenceTypeInfo(class_handle));
+}
 
 void ReferenceTypePropagation::VisitBasicBlock(HBasicBlock* block) {
+  // TODO: handle other instructions that give type info
+  // (NewArray/Call/Field accesses/array accesses)
+  for (HInstructionIterator it(block->GetInstructions()); !it.Done(); it.Advance()) {
+    HInstruction* instr = it.Current();
+    if (instr->IsNewInstance()) {
+      VisitNewInstance(instr->AsNewInstance());
+    } else if (instr->IsLoadClass()) {
+      VisitLoadClass(instr->AsLoadClass());
+    }
+  }
   if (block->IsLoopHeader()) {
     for (HInstructionIterator it(block->GetPhis()); !it.Done(); it.Advance()) {
       // Set the initial type for the phi. Use the non back edge input for reaching
@@ -56,23 +155,28 @@ void ReferenceTypePropagation::VisitBasicBlock(HBasicBlock* block) {
       HPhi* phi = it.Current()->AsPhi();
       AddToWorklist(phi);
       phi->SetCanBeNull(phi->InputAt(0)->CanBeNull());
+      phi->SetReferenceTypeInfo(phi->InputAt(0)->GetReferenceTypeInfo());
     }
   } else {
+    ScopedObjectAccess soa(Thread::Current());
     for (HInstructionIterator it(block->GetPhis()); !it.Done(); it.Advance()) {
       // Eagerly compute the type of the phi, for quicker convergence. Note
       // that we don't need to add users to the worklist because we are
       // doing a reverse post-order visit, therefore either the phi users are
       // non-loop phi and will be visited later in the visit, or are loop-phis,
       // and they are already in the work list.
-      UpdateNullability(it.Current()->AsPhi());
+      HPhi* phi = it.Current()->AsPhi();
+      UpdateNullability(phi);
+      UpdateReferenceTypeInfo(phi);
     }
   }
 }
 
 void ReferenceTypePropagation::ProcessWorklist() {
+  ScopedObjectAccess soa(Thread::Current());
   while (!worklist_.IsEmpty()) {
     HPhi* instruction = worklist_.Pop();
-    if (UpdateNullability(instruction)) {
+    if (UpdateNullability(instruction) || UpdateReferenceTypeInfo(instruction)) {
       AddDependentInstructionsToWorklist(instruction);
     }
   }
