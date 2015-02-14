e5665b84b3c787b387df8c89359d4df5b178b0af
Douglas Leung <douglas.leung@imgtec.com>
2015-02-13 16:58:33 -0800

    Add mips32r6 instruction support and don't use deprecated mips32r2 instructions if running in mips32r6 mode.
    Change-Id: I54e689aa8c026ccb75c4af515aa2794f471c9f67

--- compiler/dex/quick/mips/utility_mips.cc
+++ compiler/dex/quick/mips/utility_mips.cc
@@ -182,7 +182,12 @@ LIR* MipsMir2Lir::OpRegRegReg(OpKind op, RegStorage r_dest, RegStorage r_src1, R
       opcode = kMipsAnd;
       break;
     case kOpMul:
-      opcode = kMipsMul;
+      if (cu_->compiler_driver->GetInstructionSetFeatures()->AsMipsInstructionSetFeatures()
+          ->IsR6()) {
+          opcode = kMipsR6Mul;
+      } else {
+          opcode = kMipsMul;
+      }
       break;
     case kOpOr:
       opcode = kMipsOr;
@@ -271,7 +276,12 @@ LIR* MipsMir2Lir::OpRegRegImm(OpKind op, RegStorage r_dest, RegStorage r_src1, i
       break;
     case kOpMul:
       short_form = false;
-      opcode = kMipsMul;
+      if (cu_->compiler_driver->GetInstructionSetFeatures()->AsMipsInstructionSetFeatures()
+          ->IsR6()) {
+          opcode = kMipsR6Mul;
+      } else {
+          opcode = kMipsMul;
+      }
       break;
     default:
       LOG(FATAL) << "Bad case in OpRegRegImm";
