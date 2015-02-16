91d45a43fb4e418d6d6b7cc48e3be82d3ab31a7c
Calin Juravle <calin@google.com>
2015-02-16 13:13:29 +0000

    Improve type propagation with if-contexts
    This works by adding a new instruction (HBoundType) after each `if (a
    instanceof ClassA) {}` to bound the type that `a` can take in the True-
    dominated blocks.
    
    Change-Id: Iae6a150b353486d4509b0d9b092164675732b90c

--- compiler/optimizing/reference_type_propagation.cc
+++ compiler/optimizing/reference_type_propagation.cc
@@ -27,21 +27,6 @@ namespace art {
 // the `null` constant, that is represented as a `HIntConstant` and therefore
 // has the Primitive::kPrimInt type.
 
-// TODO: handle:
-//  public Main ifNullTest(int count, Main a) {
-//    Main m = new Main();
-//    if (a == null) {
-//      a = m;
-//    }
-//    return a.g();
-//  }
-//  public Main ifNotNullTest(Main a) {
-//    if (a != null) {
-//      return a.g();
-//    }
-//    return new Main();
-//  }
-
 void ReferenceTypePropagation::Run() {
   // To properly propagate type info we need to visit in the dominator-based order.
   // Reverse post order guarantees a node's dominators are visited first.
@@ -52,64 +37,103 @@ void ReferenceTypePropagation::Run() {
   ProcessWorklist();
 }
 
-// Re-computes and updates the nullability of the instruction. Returns whether or
-// not the nullability was changed.
-bool ReferenceTypePropagation::UpdateNullability(HPhi* phi) {
-  bool existing_can_be_null = phi->CanBeNull();
-  bool new_can_be_null = false;
-  for (size_t i = 0; i < phi->InputCount(); i++) {
-    new_can_be_null |= phi->InputAt(i)->CanBeNull();
-  }
-  phi->SetCanBeNull(new_can_be_null);
-
-  return existing_can_be_null != new_can_be_null;
+void ReferenceTypePropagation::VisitBasicBlock(HBasicBlock* block) {
+  InitializeExactTypes(block);
+  HandlePhis(block);
+  BoundTypeForIfInstanceOf(block);
 }
 
-bool ReferenceTypePropagation::UpdateReferenceTypeInfo(HPhi* phi) {
-  ScopedObjectAccess soa(Thread::Current());
+void ReferenceTypePropagation::InitializeExactTypes(HBasicBlock* block) {
+  // TODO: handle other instructions that give type info
+  // (NewArray/Call/Field accesses/array accesses)
+  for (HInstructionIterator it(block->GetInstructions()); !it.Done(); it.Advance()) {
+    HInstruction* instr = it.Current();
+    if (instr->IsNewInstance()) {
+      VisitNewInstance(instr->AsNewInstance());
+    } else if (instr->IsLoadClass()) {
+      VisitLoadClass(instr->AsLoadClass());
+    }
+  }
+}
 
-  ReferenceTypeInfo existing_rti = phi->GetReferenceTypeInfo();
-  ReferenceTypeInfo new_rti = phi->InputAt(0)->GetReferenceTypeInfo();
+void ReferenceTypePropagation::HandlePhis(HBasicBlock* block) {
+  if (block->IsLoopHeader()) {
+    for (HInstructionIterator it(block->GetPhis()); !it.Done(); it.Advance()) {
+      // Set the initial type for the phi. Use the non back edge input for reaching
+      // a fixed point faster.
+      HPhi* phi = it.Current()->AsPhi();
+      AddToWorklist(phi);
+      phi->SetCanBeNull(phi->InputAt(0)->CanBeNull());
+      phi->SetReferenceTypeInfo(phi->InputAt(0)->GetReferenceTypeInfo());
+    }
+  } else {
+    for (HInstructionIterator it(block->GetPhis()); !it.Done(); it.Advance()) {
+      // Eagerly compute the type of the phi, for quicker convergence. Note
+      // that we don't need to add users to the worklist because we are
+      // doing a reverse post-order visit, therefore either the phi users are
+      // non-loop phi and will be visited later in the visit, or are loop-phis,
+      // and they are already in the work list.
+      HPhi* phi = it.Current()->AsPhi();
+      UpdateNullability(phi);
+      UpdateReferenceTypeInfo(phi);
+    }
+  }
+}
 
-  if (new_rti.IsTop() && !new_rti.IsExact()) {
-    // Early return if we are Top and inexact.
-    phi->SetReferenceTypeInfo(new_rti);
-    return !new_rti.IsEqual(existing_rti);;
+// Detects if `block` is the True block for the pattern
+// `if (x instanceof ClassX) { }`
+// If that's the case insert an HBoundType instruction to bound the type of `x`
+// to `ClassX` in the scope of the dominated blocks.
+void ReferenceTypePropagation::BoundTypeForIfInstanceOf(HBasicBlock* block) {
+  GrowableArray<HBasicBlock*> predecessors = block->GetPredecessors();
+  if (predecessors.IsEmpty() || predecessors.Get(0)->IsEntryBlock()) {
+    return;
+  }
+  HBasicBlock* previousBlock = predecessors.Get(0);
+  HInstruction* previousIf = previousBlock->GetLastInstruction();
+  // InstanceOf returns 0 when True.
+  // TODO: fix this non intuitive case.
+  if (!previousIf->IsIf()
+      || (previousIf->AsIf()->IfFalseSuccessor() != block)) {
+    return;
+  }
+  HInstruction* ifInput = previousIf->InputAt(0);
+  if (!ifInput->IsEqual()) {
+    return;
+  }
+  HInstruction* instanceOf = ifInput->InputAt(0);
+  HInstruction* zero = ifInput->InputAt(1);
+  if (!instanceOf->IsInstanceOf() || !zero->IsConstant()
+      || (zero->AsIntConstant()->GetValue() != 0)) {
+    return;
   }
 
-  for (size_t i = 1; i < phi->InputCount(); i++) {
-    ReferenceTypeInfo input_rti = phi->InputAt(i)->GetReferenceTypeInfo();
+  HInstruction* obj = instanceOf->InputAt(0);
+  HLoadClass* load_class = instanceOf->InputAt(1)->AsLoadClass();
 
-    if (!input_rti.IsExact()) {
-      new_rti.SetInexact();
-    }
+  ReferenceTypeInfo obj_rti = obj->GetReferenceTypeInfo();
+  ReferenceTypeInfo class_rti = load_class->GetLoadedClassRTI();
+  HBoundType* bound_type = new (graph_->GetArena()) HBoundType(obj, class_rti);
 
-    if (input_rti.IsTop()) {
-      new_rti.SetTop(new_rti.IsExact() && input_rti.IsExact());
-    }
-
-    if (new_rti.IsTop()) {
-      if (!new_rti.IsExact()) {
-        break;
-      } else {
-        continue;
-      }
-    }
-
-    if (new_rti.GetTypeHandle().Get() == input_rti.GetTypeHandle().Get()) {
-      // nothing to do if we have the same type
-    } else if (input_rti.IsSupertypeOf(new_rti)) {
-      new_rti.SetTypeHandle(input_rti.GetTypeHandle(), false);
-    } else if (new_rti.IsSupertypeOf(input_rti)) {
-      new_rti.SetInexact();
+  // Narrow the type as much as possible.
+  {
+    ScopedObjectAccess soa(Thread::Current());
+    if (!class_rti.IsExact() || class_rti.IsSupertypeOf(obj_rti)) {
+      bound_type->SetReferenceTypeInfo(obj_rti);
     } else {
-      // TODO: Find common parent.
-      new_rti.SetTop(false);
+      class_rti.SetInexact();
+      bound_type->SetReferenceTypeInfo(class_rti);
     }
   }
 
-  phi->SetReferenceTypeInfo(new_rti);
-  return !new_rti.IsEqual(existing_rti);
+  previousBlock->InsertInstructionBefore(bound_type, previousIf);
+
+  for (HUseIterator<HInstruction*> it(obj->GetUses()); !it.Done(); it.Advance()) {
+    HInstruction* user = it.Current()->GetUser();
+    if (block->Dominates(user->GetBlock())) {
+      user->ReplaceInput(bound_type, it.Current()->GetIndex());
+    }
+  }
 }
 
 void ReferenceTypePropagation::VisitNewInstance(HNewInstance* instr) {
@@ -136,59 +160,119 @@ void ReferenceTypePropagation::VisitLoadClass(HLoadClass* instr) {
   instr->SetReferenceTypeInfo(ReferenceTypeInfo(class_handle, true));
 }
 
-void ReferenceTypePropagation::VisitBasicBlock(HBasicBlock* block) {
-  // TODO: handle other instructions that give type info
-  // (NewArray/Call/Field accesses/array accesses)
-  for (HInstructionIterator it(block->GetInstructions()); !it.Done(); it.Advance()) {
-    HInstruction* instr = it.Current();
-    if (instr->IsNewInstance()) {
-      VisitNewInstance(instr->AsNewInstance());
-    } else if (instr->IsLoadClass()) {
-      VisitLoadClass(instr->AsLoadClass());
-    }
+// Merges new_rti with input_rti.
+void ReferenceTypePropagation::MergeTypes(ReferenceTypeInfo& new_rti,
+                                          const ReferenceTypeInfo& input_rti) {
+  if (!input_rti.IsExact()) {
+    new_rti.SetInexact();
   }
-  if (block->IsLoopHeader()) {
-    for (HInstructionIterator it(block->GetPhis()); !it.Done(); it.Advance()) {
-      // Set the initial type for the phi. Use the non back edge input for reaching
-      // a fixed point faster.
-      HPhi* phi = it.Current()->AsPhi();
-      AddToWorklist(phi);
-      phi->SetCanBeNull(phi->InputAt(0)->CanBeNull());
-      phi->SetReferenceTypeInfo(phi->InputAt(0)->GetReferenceTypeInfo());
+
+  if (new_rti.IsTop()) {
+    // nothing todo, we are already at top.
+  } else if (input_rti.IsTop()) {
+    new_rti.SetTop(new_rti.IsExact() && input_rti.IsExact());
+  } else if (new_rti.GetTypeHandle().Get() == input_rti.GetTypeHandle().Get()) {
+    // nothing to do if we have the same type
+  } else if (input_rti.IsSupertypeOf(new_rti)) {
+    new_rti.SetTypeHandle(input_rti.GetTypeHandle(), false);
+  } else if (new_rti.IsSupertypeOf(input_rti)) {
+    new_rti.SetInexact();
+  } else {
+    // TODO: Find common parent.
+    new_rti.SetTop(false);
+  }
+}
+
+bool ReferenceTypePropagation::UpdateReferenceTypeInfo(HInstruction* instr) {
+  DCHECK(instr->IsPhi() || instr->IsBoundType());
+
+  ScopedObjectAccess soa(Thread::Current());
+
+  ReferenceTypeInfo existing_rti = instr->GetReferenceTypeInfo();
+  ReferenceTypeInfo new_rti = instr->InputAt(0)->GetReferenceTypeInfo();
+
+  if (instr->IsBoundType()) {
+    // Be sure that we don't go over the bounded type.
+    ReferenceTypeInfo top_rti = instr->AsBoundType()->GetTopType();
+    if (!top_rti.IsSupertypeOf(new_rti)) {
+      new_rti = top_rti;
     }
   } else {
-    ScopedObjectAccess soa(Thread::Current());
-    for (HInstructionIterator it(block->GetPhis()); !it.Done(); it.Advance()) {
-      // Eagerly compute the type of the phi, for quicker convergence. Note
-      // that we don't need to add users to the worklist because we are
-      // doing a reverse post-order visit, therefore either the phi users are
-      // non-loop phi and will be visited later in the visit, or are loop-phis,
-      // and they are already in the work list.
-      HPhi* phi = it.Current()->AsPhi();
-      UpdateNullability(phi);
-      UpdateReferenceTypeInfo(phi);
+    DCHECK(instr->IsPhi());
+    if (new_rti.IsTop() && !new_rti.IsExact()) {
+      // Early return if we are Top and inexact.
+      instr->SetReferenceTypeInfo(new_rti);
+      return !new_rti.IsEqual(existing_rti);;
+    }
+    for (size_t i = 1; i < instr->InputCount(); i++) {
+      MergeTypes(new_rti, instr->InputAt(i)->GetReferenceTypeInfo());
+      if (new_rti.IsTop()) {
+        if (!new_rti.IsExact()) {
+          break;
+        } else {
+          continue;
+        }
+      }
     }
   }
+
+  instr->SetReferenceTypeInfo(new_rti);
+  return !new_rti.IsEqual(existing_rti);
+}
+
+// TODO: handle:
+//  public Main ifNullTest(int count, Main a) {
+//    Main m = new Main();
+//    if (a == null) {
+//      a = m;
+//    }
+//    return a.g();
+//  }
+//  public Main ifNotNullTest(Main a) {
+//    if (a != null) {
+//      return a.g();
+//    }
+//    return new Main();
+//  }
+
+// Re-computes and updates the nullability of the instruction. Returns whether or
+// not the nullability was changed.
+bool ReferenceTypePropagation::UpdateNullability(HInstruction* instr) {
+  DCHECK(instr->IsPhi() || instr->IsBoundType());
+
+  if (!instr->IsPhi()) {
+    return false;
+  }
+
+  HPhi* phi = instr->AsPhi();
+  bool existing_can_be_null = phi->CanBeNull();
+  bool new_can_be_null = false;
+  for (size_t i = 0; i < phi->InputCount(); i++) {
+    new_can_be_null |= phi->InputAt(i)->CanBeNull();
+  }
+  phi->SetCanBeNull(new_can_be_null);
+
+  return existing_can_be_null != new_can_be_null;
 }
 
 void ReferenceTypePropagation::ProcessWorklist() {
   while (!worklist_.IsEmpty()) {
-    HPhi* instruction = worklist_.Pop();
+    HInstruction* instruction = worklist_.Pop();
     if (UpdateNullability(instruction) || UpdateReferenceTypeInfo(instruction)) {
       AddDependentInstructionsToWorklist(instruction);
     }
   }
 }
 
-void ReferenceTypePropagation::AddToWorklist(HPhi* instruction) {
+void ReferenceTypePropagation::AddToWorklist(HInstruction* instruction) {
   worklist_.Add(instruction);
 }
 
-void ReferenceTypePropagation::AddDependentInstructionsToWorklist(HPhi* instruction) {
+void ReferenceTypePropagation::AddDependentInstructionsToWorklist(HInstruction* instruction) {
   for (HUseIterator<HInstruction*> it(instruction->GetUses()); !it.Done(); it.Advance()) {
-    HPhi* phi = it.Current()->GetUser()->AsPhi();
-    if (phi != nullptr) {
-      AddToWorklist(phi);
+    HInstruction* user = it.Current()->GetUser();
+    if (user->IsPhi() || user->IsBoundType()) {
+      AddToWorklist(user);
     }
   }
 }
