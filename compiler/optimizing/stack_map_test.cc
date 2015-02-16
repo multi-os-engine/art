6adb3500a14e2f8516e335f0e5aa0f667d552340
Roland Levillain <rpl@google.com>
2015-02-16 18:23:16 +0000

    Implement a stack map dictionary-based Dex register encoding.
    - As an alternative to the current list-based Dex register
      encoding in stack maps emitted by the optimizing compiler,
      implement a per-method dictionary-based Dex register
      encoding.
    - Add the (static) configuration setting
      art::dex_register_map_encoding to select the encoding
      stategy at build time between the original encoding
      (kDexRegisterLocationList) or the new one
      (kDexRegisterLocationDictionary, set by this CL).
    - Adjust art::StackMapStream.
    - Extend oatdump to support the new Dex register encoding.
    - Update compiler/optimizing/stack_map_test.cc.
    - Add AddUnique and Find methods to art::GrowableArray.
    
    Change-Id: Iab230aa1f0197e2f405f59e92a95130ce3d11222

--- compiler/optimizing/stack_map_test.cc
+++ compiler/optimizing/stack_map_test.cc
@@ -62,11 +62,28 @@ TEST(StackMapTest, Test1) {
   ASSERT_TRUE(SameBits(stack_mask, sp_mask));
 
   ASSERT_TRUE(stack_map.HasDexRegisterMap());
-  DexRegisterMap dex_registers = code_info.GetDexRegisterMapOf(stack_map, 2);
-  ASSERT_EQ(DexRegisterMap::kInStack, dex_registers.GetLocationKind(0));
-  ASSERT_EQ(DexRegisterMap::kConstant, dex_registers.GetLocationKind(1));
-  ASSERT_EQ(0, dex_registers.GetValue(0));
-  ASSERT_EQ(-2, dex_registers.GetValue(1));
+  switch (dex_register_map_encoding) {
+    case kDexRegisterLocationList: {
+      DexRegisterMap dex_registers = code_info.GetDexRegisterMapOf(stack_map, 2);
+      ASSERT_EQ(DexRegisterMap::kInStack, dex_registers.GetLocationKind(0));
+      ASSERT_EQ(DexRegisterMap::kConstant, dex_registers.GetLocationKind(1));
+      ASSERT_EQ(0, dex_registers.GetValue(0));
+      ASSERT_EQ(-2, dex_registers.GetValue(1));
+      break;
+    }
+
+    case kDexRegisterLocationDictionary: {
+      DexRegisterDictionary dictionary = code_info.GetDexRegisterDictionary();
+      DexRegisterTable table = code_info.GetDexRegisterTableOf(stack_map, 2);
+      DexRegisterTable::EntryIndex index0 = table.GetEntryIndex(0);
+      DexRegisterTable::EntryIndex index1 = table.GetEntryIndex(1);
+      ASSERT_EQ(DexRegisterMap::kInStack, dictionary.GetLocationKind(index0));
+      ASSERT_EQ(DexRegisterMap::kConstant, dictionary.GetLocationKind(index1));
+      ASSERT_EQ(0, dictionary.GetValue(index0));
+      ASSERT_EQ(-2, dictionary.GetValue(index1));
+      break;
+    }
+  }
 }
 
 TEST(StackMapTest, Test2) {
@@ -109,11 +126,28 @@ TEST(StackMapTest, Test2) {
   ASSERT_TRUE(SameBits(stack_mask, sp_mask1));
 
   ASSERT_TRUE(stack_map.HasDexRegisterMap());
-  DexRegisterMap dex_registers = code_info.GetDexRegisterMapOf(stack_map, 2);
-  ASSERT_EQ(DexRegisterMap::kInStack, dex_registers.GetLocationKind(0));
-  ASSERT_EQ(DexRegisterMap::kConstant, dex_registers.GetLocationKind(1));
-  ASSERT_EQ(0, dex_registers.GetValue(0));
-  ASSERT_EQ(-2, dex_registers.GetValue(1));
+  switch (dex_register_map_encoding) {
+    case kDexRegisterLocationList: {
+      DexRegisterMap dex_registers = code_info.GetDexRegisterMapOf(stack_map, 2);
+      ASSERT_EQ(DexRegisterMap::kInStack, dex_registers.GetLocationKind(0));
+      ASSERT_EQ(DexRegisterMap::kConstant, dex_registers.GetLocationKind(1));
+      ASSERT_EQ(0, dex_registers.GetValue(0));
+      ASSERT_EQ(-2, dex_registers.GetValue(1));
+      break;
+    }
+
+    case kDexRegisterLocationDictionary: {
+      DexRegisterDictionary dictionary = code_info.GetDexRegisterDictionary();
+      DexRegisterTable table = code_info.GetDexRegisterTableOf(stack_map, 2);
+      DexRegisterTable::EntryIndex index0 = table.GetEntryIndex(0);
+      DexRegisterTable::EntryIndex index1 = table.GetEntryIndex(1);
+      ASSERT_EQ(DexRegisterMap::kInStack, dictionary.GetLocationKind(index0));
+      ASSERT_EQ(DexRegisterMap::kConstant, dictionary.GetLocationKind(index1));
+      ASSERT_EQ(0, dictionary.GetValue(index0));
+      ASSERT_EQ(-2, dictionary.GetValue(index1));
+      break;
+    }
+  }
 
   InlineInfo inline_info = code_info.GetInlineInfoOf(stack_map);
   ASSERT_EQ(2u, inline_info.GetDepth());
