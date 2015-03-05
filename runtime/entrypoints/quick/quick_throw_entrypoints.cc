027bfd892064f489f4a185b3b45bdf215e3014de
Nicolas Geoffray <ngeoffray@google.com>
2015-03-05 10:40:17 +0000

    Compute the right catch location for the debugger.
    Also remove tls ThrowLocation, it is not needed anymore.
    
    Change-Id: I78fddf09ce968ca475e39c17fa76d699c589c8d9

--- runtime/entrypoints/quick/quick_throw_entrypoints.cc
+++ runtime/entrypoints/quick/quick_throw_entrypoints.cc
@@ -41,12 +41,11 @@ extern "C" void artDeliverExceptionFromCode(mirror::Throwable* exception, Thread
    * exception_ in thread and delivering the exception.
    */
   ScopedQuickEntrypointChecks sqec(self);
-  ThrowLocation throw_location = self->GetCurrentLocationForThrow();
   if (exception == nullptr) {
-    self->ThrowNewException(throw_location, "Ljava/lang/NullPointerException;",
+    self->ThrowNewException(ThrowLocation(), "Ljava/lang/NullPointerException;",
                             "throw with null exception");
   } else {
-    self->SetException(throw_location, exception);
+    self->SetException(exception);
   }
   self->QuickDeliverException();
 }
