791ef0581e69a99c6112b189cd55c97cbdbd4eaf
Andreas Gampe <agampe@google.com>
2015-03-09 12:57:41 -0700

    ART: More cutouts for unstarted runtime
    Dex objects keep byte buffers to mapped dex files. For annotation
    access we create them when compiling, but they must be cleared before
    writing an image.
    
    Add some Memory functions.
    
    Bug: 19542228
    Change-Id: I995791b43f2b09192d88645821d9e55b45f1b606

--- compiler/image_writer.cc
+++ compiler/image_writer.cc
@@ -71,6 +71,18 @@ namespace art {
 // Separate objects into multiple bins to optimize dirty memory use.
 static constexpr bool kBinObjects = true;
 
+static void CheckNoDexObjectsCallback(Object* obj, void* arg ATTRIBUTE_UNUSED)
+    SHARED_LOCKS_REQUIRED(Locks::mutator_lock_) {
+  Class* klass = obj->GetClass();
+  CHECK_NE(PrettyClass(klass), "com.android.dex.Dex");
+}
+
+static void CheckNoDexObjects() {
+  Thread::Current()->TransitionFromSuspendedToRunnable();
+  Runtime::Current()->GetHeap()->VisitObjects(CheckNoDexObjectsCallback, nullptr);
+  Thread::Current()->TransitionFromRunnableToSuspended(kNative);
+}
+
 bool ImageWriter::PrepareImageAddressSpace() {
   target_ptr_size_ = InstructionSetPointerSize(compiler_driver_.GetInstructionSet());
   {
@@ -82,6 +94,16 @@ bool ImageWriter::PrepareImageAddressSpace() {
   }
   gc::Heap* heap = Runtime::Current()->GetHeap();
   heap->CollectGarbage(false);  // Remove garbage.
+
+  // Dex caches must not have their dex fields set in the image. These are memory buffers of mapped
+  // dex files.
+  //
+  // We may open them in the unstarted-runtime code for class metadata. They fields should all be
+  // reset in PruneNonImageClasses and the objects reclaimed in the GC. Make sure that's actually
+  // true.
+  if (kIsDebugBuild) {
+    CheckNoDexObjects();
+  }
 
   if (!AllocMemory()) {
     return false;
@@ -644,6 +666,9 @@ void ImageWriter::PruneNonImageClasses() {
         dex_cache->SetResolvedField(i, NULL);
       }
     }
+    // Clean the dex field. It might have been populated during the initialization phase, but
+    // contains data only valid during a real run.
+    dex_cache->SetFieldObject<false>(mirror::DexCache::DexOffset(), nullptr);
   }
 }
 
