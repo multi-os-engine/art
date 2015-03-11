24f6443c739eee56448daf8105a0dd7aea62fd56
Guillaume "Vermeille" Sanchez <guillaumesa@google.com>
2015-03-11 14:00:30 +0000

    Forbid the use of shifts in ShifterOperand in Thumb2
    Change-Id: Ib141f53e52cce3032a42fbc991303e227c98820b

--- compiler/utils/arm/assembler_thumb2.cc
+++ compiler/utils/arm/assembler_thumb2.cc
@@ -921,6 +921,7 @@ void Thumb2Assembler::Emit16BitDataProcessing(Condition cond,
       use_immediate = true;
       immediate = so.GetImmediate();
     } else {
+      CHECK(so.IsRegister() && so.IsShift() && so.GetSecondRegister() != kNoRegister);
       // Adjust rn and rd: only two registers will be emitted.
       switch (opcode) {
         case AND:
