5783748a1ccbb06670a4e8dbd774df3d21ceed7f
Guillaume "Vermeille" Sanchez <guillaumesa@google.com>
2015-03-11 14:30:31 +0000

    Make subs alter flags when rn is an immediate
    Change-Id: I7ea84fca5f97713871744b8ac4265a84fd6a5248

--- compiler/utils/arm/assembler_thumb2.cc
+++ compiler/utils/arm/assembler_thumb2.cc
@@ -826,7 +826,9 @@ void Thumb2Assembler::Emit32BitDataProcessing(Condition cond ATTRIBUTE_UNUSED,
     // Check special cases.
     if ((opcode == SUB || opcode == ADD) && (so.GetImmediate() < (1u << 12))) {
       if (opcode == SUB) {
-        thumb_opcode = 5U /* 0b0101 */;
+        if (!set_cc) {
+          thumb_opcode = 5U /* 0b0101 */;
+        }
       } else {
         thumb_opcode = 0;
       }
@@ -836,13 +838,15 @@ void Thumb2Assembler::Emit32BitDataProcessing(Condition cond ATTRIBUTE_UNUSED,
       uint32_t imm3 = (imm >> 8) & 7U /* 0b111 */;
       uint32_t imm8 = imm & 0xff;
 
-      encoding = B31 | B30 | B29 | B28 | B25 |
-           thumb_opcode << 21 |
-           rn << 16 |
-           rd << 8 |
-           i << 26 |
-           imm3 << 12 |
-           imm8;
+        encoding = B31 | B30 | B29 | B28 |
+            (set_cc ? B20 : B25) |
+            thumb_opcode << 21 |
+            rn << 16 |
+            rd << 8 |
+            i << 26 |
+            imm3 << 12 |
+            imm8;
+      }
     } else {
       // Modified immediate.
       uint32_t imm = ModifiedImmediate(so.encodingThumb());
