cad08ac070334c44e66a9b9ee4d5156e51d2b8bd
Andreas Gampe <agampe@google.com>
2015-03-19 13:53:08 -0700

    [WIP] ART: New class-loading test
    Bug: 19382130
    Change-Id: Id12fcabe194791907109ada9d5a4861a8cfe59fe

---
+++ test/136-classloader2/src/Main.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+/**
+ * Class loader test.
+ */
+public class Main {
+
+    static final String DEX_FILE = System.getenv("DEX_LOCATION") + "/136-classloader2-ex.jar";
+
+    /**
+     * Main entry point.
+     */
+    public static void main(String[] args) throws Exception {
+        SecondMain.main(args);
+//        ClassLoader loader = createClassLoader(null);
+//
+//        Class<?> klass = loader.loadClass("SecondMain");
+//        Method main = klass.getDeclaredMethod("main", String[].class);
+//        main.invoke(null, (String)null);
+    }
+
+    private static ClassLoader createClassLoader(ClassLoader parent) throws Exception {
+        if (parent == null) {
+            // Derive the boot class-loader.
+            parent = ClassLoader.getSystemClassLoader().getParent();
+        }
+
+        // TODO: Reflection for PathClassLoader
+        Class<?> pathClassLoaderClass = parent.loadClass("dalvik.system.PathClassLoader");
+        Constructor constructor = pathClassLoaderClass.getDeclaredConstructor(String.class,
+                ClassLoader.class);
+
+        Object pathClassLoader = constructor.newInstance(DEX_FILE, parent);
+
+        return (ClassLoader)pathClassLoader;
+    }
+}
