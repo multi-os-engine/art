c821402b8c2c47809bd57faa72ce6af854049ea3
Mark Mendell <mark.p.mendell@intel.com>
2015-04-02 20:30:05 -0400

    [optimizing] Add support for switch jump tables
    Implement the HSwitch HInstruction to generate switch tables rather than
    chained if statements.  Implement this for x86_64 with RIP support,
    since it has a constant table.
    
    Add a virtual function to CodeGenerator to allow a backend to implement
    support for switch tables.
    
    Add a pointer to the CodeGenerator to the HGraph, as the builder needs
    to be able to query to see if HSwitch should be generated.
    
    Change-Id: I3c53a447830452fc514d1c7b92ac9d8c8d785d6e
    Signed-off-by: Mark Mendell <mark.p.mendell@intel.com>

--- compiler/optimizing/code_generator_x86_64.h
+++ compiler/optimizing/code_generator_x86_64.h
@@ -287,6 +287,12 @@ class CodeGeneratorX86_64 : public CodeGenerator {
   Address LiteralInt32Address(int32_t v);
   Address LiteralInt64Address(int64_t v);
 
+  Address LiteralCaseTable(HSwitch* switch_instr);
+
+  virtual bool SupportsSwitch() const {
+    return true;
+  }
+
  private:
   // Labels for each block that will be compiled.
   GrowableArray<Label> block_labels_;
