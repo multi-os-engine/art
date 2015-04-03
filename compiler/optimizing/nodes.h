89ddd843c9e87e430c1547047fce9f78dd9cffa8
Razvan A Lupusoru <razvan.a.lupusoru@intel.com>
2015-04-03 16:39:56 -0700

    [optimizing] Enable inlining of throwers under special condition
    The SpecialMethodInliner in Quick can handle inlining of throwing
    getters if they are synthetic accessors. What this means is that Quick's
    inliner can handle cases which Optimizing compiler cannot handle.
    
    In an effort to mitigate that, the Optimizing inliner has been enhanced
    to accept throwers when debugging is disabled. The technique used is
    more general than the one in Quick so now this represents a superset
    of methods inlined by Quick. The allowed throwers are NullCheck,
    BoundCheck, and DivZeroCheck.
    
    Change-Id: I433e78a5f2b965feddbc4de4f38a26febf9a79c7
    Signed-off-by: Razvan A Lupusoru <razvan.a.lupusoru@intel.com>

--- compiler/optimizing/nodes.h
+++ compiler/optimizing/nodes.h
@@ -224,6 +224,16 @@ class HGraph : public ArenaObject<kArenaAllocMisc> {
     has_array_accesses_ = value;
   }
 
+  bool HasCatchHandlers() const {
+    return has_catch_handlers_;
+  }
+
+  void SetHasCatchHandlers(bool value) {
+    has_catch_handlers_ = value;
+  }
+
+  bool HasCatchHandler(int32_t instruction_id) const;
+
   bool IsDebuggable() const { return debuggable_; }
 
   // Returns a constant of the given type and value. If it does not exist
@@ -279,6 +289,9 @@ class HGraph : public ArenaObject<kArenaAllocMisc> {
 
   // Has array accesses. We can totally skip BCE if it's false.
   bool has_array_accesses_;
+
+  // Has catch handlers.
+  bool has_catch_handlers_;
 
   // Indicates whether the graph should be compiled in a way that
   // ensures full debuggability. If false, we can apply more
@@ -1130,6 +1143,9 @@ class HInstruction : public ArenaObject<kArenaAllocMisc> {
   virtual bool IsControlFlow() const { return false; }
   virtual bool CanThrow() const { return false; }
   bool HasSideEffects() const { return side_effects_.HasSideEffects(); }
+  virtual bool CanInlineCheck() const {
+    return IsNullCheck() || IsBoundsCheck() || IsDivZeroCheck();
+  }
 
   virtual bool ActAsNullConstant() const { return false; }
 
