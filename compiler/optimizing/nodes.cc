eaeb2b7d305c19bd5605887c07048488849b7eb5
Razvan A Lupusoru <razvan.a.lupusoru@intel.com>
2015-04-03 16:39:56 -0700

    [optimizing] Enable inlining of throwers under special condition
    The SpecialMethodInliner in Quick can handle inlining of throwing
    getters if they are synthetic accessors. What this means is that Quick's
    inliner can handle cases which Optimizing compiler cannot handle.
    
    In an effort to mitigate that, the Optimizing inliner has been enhanced
    to accept throwers when debugging is disabled. The technique used is
    more general than the one in Quick so now this represents a superset
    of methods inlined by Quick. The allowed throwers are NullCheck,
    BoundCheck, and DivZeroCheck.
    
    Change-Id: I433e78a5f2b965feddbc4de4f38a26febf9a79c7
    Signed-off-by: Razvan A Lupusoru <razvan.a.lupusoru@intel.com>

--- compiler/optimizing/nodes.cc
+++ compiler/optimizing/nodes.cc
@@ -322,6 +322,12 @@ InstructionType* HGraph::CreateConstant(ValueType value,
   return constant;
 }
 
+bool HGraph::HasCatchHandler(int32_t instruction_id ATTRIBUTE_UNUSED) const {
+  // TODO: Until we store catch information for each instruction, be conservative and
+  // assume if there is a catch handler, this instruction is in it.
+  return HasCatchHandlers();
+}
+
 HConstant* HGraph::GetConstant(Primitive::Type type, int64_t value) {
   switch (type) {
     case Primitive::Type::kPrimBoolean:
@@ -974,6 +980,22 @@ static void MakeRoomFor(GrowableArray<HBasicBlock*>* blocks,
 }
 
 void HGraph::InlineInto(HGraph* outer_graph, HInvoke* invoke) {
+  // Now try to fix the environment for the callee instructions.
+  HReversePostOrderIterator callee_it(*this);
+  for (callee_it.Advance(); !callee_it.Done(); callee_it.Advance()) {
+    HBasicBlock* block = callee_it.Current();
+    for (HInstructionIterator instr_it(block->GetInstructions());
+         !instr_it.Done();
+         instr_it.Advance()) {
+      HInstruction* current = instr_it.Current();
+      if (current->CanThrow() && current->NeedsEnvironment()) {
+        // All supported throwers end the callee context and thus it is safe to copy
+        // environment from invoke.
+        current->CopyEnvironmentFrom(invoke->GetEnvironment());
+      }
+    }
+  }
+
   if (GetBlocks().Size() == 3) {
     // Simple case of an entry block, a body block, and an exit block.
     // Put the body block's instruction into `invoke`'s block.
@@ -990,7 +1012,8 @@ void HGraph::InlineInto(HGraph* outer_graph, HInvoke* invoke) {
     if (last->IsReturn()) {
       invoke->ReplaceWith(last->InputAt(0));
     } else {
-      DCHECK(last->IsReturnVoid());
+      DCHECK(last->IsReturnVoid()) << "Did not find a void return and instead found " <<
+          last->DebugName();
     }
 
     invoke->GetBlock()->RemoveInstruction(last);
