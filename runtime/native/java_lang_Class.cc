ab8b938e30afb0bbab8d603ab6b93b82ca715591
Vladimir Marko <vmarko@google.com>
2015-04-09 14:13:13 +0100

    Avoid using dex cache array pointers in libart.
    In preparation for making dex cache arrays native, avoid
    using them in Java code.
    
    Bug: 20134538
    Change-Id: Ibbef3b50043a1311cd40723ed42e1f1c609b8fc1

--- runtime/native/java_lang_Class.cc
+++ runtime/native/java_lang_Class.cc
@@ -91,6 +91,20 @@ static jclass Class_classForName(JNIEnv* env, jclass, jstring javaName, jboolean
   return soa.AddLocalReference<jclass>(c.Get());
 }
 
+static jobject Class_findOverriddenMethodIfProxyNative(JNIEnv* env, jobject java_class,
+                                                       jobject art_method) {
+  ScopedFastNativeObjectAccess soa(env);
+  mirror::Class* klass = soa.Decode<mirror::Class*>(java_class);
+  DCHECK(soa.Decode<mirror::ArtMethod*>(art_method)->GetDeclaringClass() == klass);
+  if (!klass->IsProxyClass()) {
+    return art_method;
+  }
+  mirror::ArtMethod* method = soa.Decode<mirror::ArtMethod*>(art_method);
+  uint32_t dex_method_index = method->GetDexMethodIndex();
+  mirror::ArtMethod* overriden_method = method->GetDexCacheResolvedMethod(dex_method_index);
+  return soa.AddLocalReference<jobject>(overriden_method);
+}
+
 static jstring Class_getNameNative(JNIEnv* env, jobject javaThis) {
   ScopedFastNativeObjectAccess soa(env);
   StackHandleScope<1> hs(soa.Self());
@@ -264,6 +278,8 @@ static jobject Class_getDeclaredField(JNIEnv* env, jobject javaThis, jstring nam
 
 static JNINativeMethod gMethods[] = {
   NATIVE_METHOD(Class, classForName, "!(Ljava/lang/String;ZLjava/lang/ClassLoader;)Ljava/lang/Class;"),
+  NATIVE_METHOD(Class, findOverriddenMethodIfProxyNative,
+                "!(Ljava/lang/reflect/ArtMethod;)Ljava/lang/reflect/ArtMethod;"),
   NATIVE_METHOD(Class, getNameNative, "!()Ljava/lang/String;"),
   NATIVE_METHOD(Class, getProxyInterfaces, "!()[Ljava/lang/Class;"),
   NATIVE_METHOD(Class, getDeclaredFields, "!()[Ljava/lang/reflect/Field;"),
