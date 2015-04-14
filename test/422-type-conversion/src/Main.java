aeaedd7a7daafb5e3b260d085dbf725bf4463874
Roland Levillain <rpl@google.com>
2015-04-14 16:02:41 +0100

    Delegate long-to-float type conversions to the runtime on ARM.
    On ARM, translate long-to-float type conversions (from both
    Quick and Optimizing) as calls to the runtime routine
    art_l2f, instead of generating ad hoc code, so as to improve
    the precision of the conversions.
    
    Change-Id: I8c414ee1c6f4ff1f32ee78f75734cfd3cf579f71

--- test/422-type-conversion/src/Main.java
+++ test/422-type-conversion/src/Main.java
@@ -321,11 +321,7 @@ public class Main {
     assertFloatEquals(9223372036854775807F, $opt$LongToFloat(9223372036854775807L));  // 2^63 - 1
     assertFloatEquals(-9223372036854775807F, $opt$LongToFloat(-9223372036854775807L));  // -(2^63 - 1)
     assertFloatEquals(-9223372036854775808F, $opt$LongToFloat(-9223372036854775808L));  // -(2^63)
-    if (!System.getProperty("os.arch").startsWith("arm")) {
-      // Does not hold on ARM due to the implementation of long-to-float type conversion
-      // (both with Quick and Optimizing).
-      assertFloatEquals(Float.intBitsToFloat(-555858671), $opt$LongToFloat(-8008112895877447681L));
-    }
+    assertFloatEquals(Float.intBitsToFloat(-555858671), $opt$LongToFloat(-8008112895877447681L));
   }
 
   private static void longToDouble() {
