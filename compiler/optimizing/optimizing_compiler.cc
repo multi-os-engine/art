db3c0f91f3d06dc32f86a4ea386a4b715b3c0c55
Mark Mendell <mark.p.mendell@intel.com>
2015-04-17 12:49:27 -0400

    [optimizing] Add support for x86 constant area
    Use the Quick trick of finding the address of the method by calling the
    next instruction and popping the return address into a register.  This
    trick is used because of the lack of PC-relative addressing in 32 bit
    mode on the X86.
    
    Add an optional 'backend optimization' pass that is per code-generator
    that can examine the graph and add instructions before register
    allocation.  Add a HX86ComputeBaseMethodAddress instruction to trigger
    generation of the method address, which is referenced by instructions
    needing access to the constant area.
    
    Add a 'backend input' field to HInstruction that allows the backend to
    specify that this instruction needs an extra input (such as the method
    address).  Treat this an extra input when needed.
    
    When creating Locations, add the method address input as needed.  Code
    generation will use this input to address the constant area.
    
    Currently, this is used only for the basic FP operations.  Other uses
    will be added later.
    
    Port the X86_64 assembler constant area handling to the X86.
    
    Use the new 'per-backend' infrastructure.
    
    Change-Id: I6d235a72238262e4f9ec0f3c88319a187f865932
    Signed-off-by: Mark Mendell <mark.p.mendell@intel.com>

--- compiler/optimizing/optimizing_compiler.cc
+++ compiler/optimizing/optimizing_compiler.cc
@@ -312,6 +312,21 @@ static void RunOptimizations(HOptimization* optimizations[],
   }
 }
 
+/**
+ * Simple pass to allow backends to process the graph after all optimizations.
+ */
+class BackendOptimizer : public HOptimization {
+ public:
+  BackendOptimizer(HGraph* graph, const char* name = kBackendOptimizationPassName)
+    : HOptimization(graph, true, name, nullptr) {}
+
+  static constexpr const char* kBackendOptimizationPassName = "backend_optimization";
+
+  void Run() OVERRIDE {
+    graph_->GetCodeGenerator()->RunBackendOptimization(graph_);
+  }
+};
+
 static void RunOptimizations(HGraph* graph,
                              CompilerDriver* driver,
                              OptimizingCompilerStats* stats,
@@ -346,6 +361,8 @@ static void RunOptimizations(HGraph* graph,
 
   IntrinsicsRecognizer* intrinsics = new (arena) IntrinsicsRecognizer(graph, driver);
 
+  BackendOptimizer* backend = new (arena) BackendOptimizer(graph);
+
   HOptimization* optimizations[] = {
     intrinsics,
     dce1,
@@ -367,6 +384,9 @@ static void RunOptimizations(HGraph* graph,
     bce,
     simplify3,
     dce2,
+
+    // Must be the final optimization in this list.
+    backend
   };
 
   RunOptimizations(optimizations, arraysize(optimizations), pass_info_printer);
@@ -545,6 +565,7 @@ CompiledMethod* OptimizingCompiler::TryCompile(const DexFile::CodeItem* code_ite
     MaybeRecordStat(MethodCompilationStat::kNotCompiledNoCodegen);
     return nullptr;
   }
+  graph->SetCodeGenerator(codegen.get());
   codegen->GetAssembler()->cfi().SetEnabled(
       compiler_driver->GetCompilerOptions().GetGenerateDebugInfo());
 
