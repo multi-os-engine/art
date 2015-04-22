2c4658089de421317c35b49b17c730e6bacf17d9
Roland Levillain <rpl@google.com>
2015-04-22 16:22:50 +0100

    Ensure inlined static calls perform clinit checks in Optimizing.
    Calls to static methods have implicit class initialization
    (clinit) checks of the method's declaring class in
    Optimizing.  However, when such a static call is inlined,
    the implicit clinit check vanishes, possibly leading to an
    incorrect behavior.
    
    To ensure that inlining static methods does not change the
    behavior of a program, add explicit class initialization
    checks (art::HClinitCheck) as well as load class
    instructions (art::HLoadClass) as first input of static
    calls (art::HInvokeStaticOrDirect) in Optimizing' control
    flow graphs, when the declaring class is reachable and not
    known to be already initialized.  Then when considering the
    inlining of a static method call, proceed only if the method
    has no implicit clinit check requirement.
    
    The added explicit clinit checks are already removed by the
    art::PrepareForRegisterAllocation visitor.  This CL also
    extends this visitor to turn explicit clinit checks from
    static invokes into implicit ones after the inlining step,
    by removing the added art::HLoadClass nodes mentioned
    hereinbefore.
    
    Change-Id: I9ba452b8bd09ae1fdd9a3797ef556e3e7e19c651

--- compiler/optimizing/inliner.h
+++ compiler/optimizing/inliner.h
@@ -47,9 +47,11 @@ class HInliner : public HOptimization {
   static constexpr const char* kInlinerPassName = "inliner";
 
  private:
-  bool TryInline(HInvoke* invoke_instruction, uint32_t method_index, InvokeType invoke_type) const;
+  bool TryInline(HInvokeStaticOrDirect* invoke_instruction,
+                 uint32_t method_index,
+                 InvokeType invoke_type) const;
   bool TryBuildAndInline(Handle<mirror::ArtMethod> resolved_method,
-                         HInvoke* invoke_instruction,
+                         HInvokeStaticOrDirect* invoke_instruction,
                          uint32_t method_index,
                          bool can_use_dex_cache) const;
 
