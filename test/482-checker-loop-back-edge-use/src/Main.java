e589377f4174f7b69252bd30df062f3ce19db0b3
David Brazdil <dbrazdil@google.com>
2015-05-15 16:44:05 +0100

    ART: Revert change to LiveRange::Dump
    Changes back the LiveRange printing format to "[start,end)" for better
    clarity. However, it removes the space after comma due to b/1189305.
    
    Bug: 21189305
    Change-Id: Ic83025fa78d6f1edb5e0e39d66160182b0198ab8

--- test/482-checker-loop-back-edge-use/src/Main.java
+++ test/482-checker-loop-back-edge-use/src/Main.java
@@ -5,7 +5,7 @@
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
 *
-*      http://www.apache.org/licenses/LICENSE-2.0
+*      http://www.apache.org/licenses/LICENSE[,2).0
 *
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,15 +17,15 @@
 
 public class Main {
 
-  // CHECK-START: void Main.loop1(boolean) liveness (after)
-  // CHECK:         ParameterValue  liveness:2  ranges:[ 2-22 ] uses:[ 17 22 ]
+  // CHECK[,)START: void Main.loop1(boolean) liveness (after)
+  // CHECK:         ParameterValue  liveness:2  ranges:[ [2,22) ] uses:[ 17 22 ]
   // CHECK:         Goto            liveness:20
   public static void loop1(boolean incoming) {
     while (incoming) {}
   }
 
-  // CHECK-START: void Main.loop2(boolean) liveness (after)
-  // CHECK:         ParameterValue  liveness:2  ranges:[ 2-42 ] uses:[ 33 38 42 ]
+  // CHECK[,)START: void Main.loop2(boolean) liveness (after)
+  // CHECK:         ParameterValue  liveness:2  ranges:[ [2,42) ] uses:[ 33 38 42 ]
   // CHECK:         Goto            liveness:36
   // CHECK:         Goto            liveness:40
   public static void loop2(boolean incoming) {
@@ -35,12 +35,12 @@ public class Main {
     }
   }
 
-  // CHECK-START: void Main.loop3(boolean) liveness (after)
-  // CHECK:         ParameterValue  liveness:2  ranges:[ 2-60 ] uses:[ 56 60 ]
+  // CHECK[,)START: void Main.loop3(boolean) liveness (after)
+  // CHECK:         ParameterValue  liveness:2  ranges:[ [2,60) ] uses:[ 56 60 ]
   // CHECK:         Goto            liveness:58
 
-  // CHECK-START: void Main.loop3(boolean) liveness (after)
-  // CHECK-NOT:     Goto liveness:54
+  // CHECK[,)START: void Main.loop3(boolean) liveness (after)
+  // CHECK[,)NOT:     Goto liveness:54
   public static void loop3(boolean incoming) {
     // 'incoming' only needs a use at the outer loop's back edge.
     while (System.currentTimeMillis() != 42) {
@@ -49,11 +49,11 @@ public class Main {
     }
   }
 
-  // CHECK-START: void Main.loop4(boolean) liveness (after)
-  // CHECK:         ParameterValue  liveness:2  ranges:[ 2-22 ] uses:[ 22 ]
+  // CHECK[,)START: void Main.loop4(boolean) liveness (after)
+  // CHECK:         ParameterValue  liveness:2  ranges:[ [2,22) ] uses:[ 22 ]
 
-  // CHECK-START: void Main.loop4(boolean) liveness (after)
-  // CHECK-NOT:     Goto            liveness:20
+  // CHECK[,)START: void Main.loop4(boolean) liveness (after)
+  // CHECK[,)NOT:     Goto            liveness:20
   public static void loop4(boolean incoming) {
     // 'incoming' has no loop use, so should not have back edge uses.
     System.out.println(incoming);
@@ -62,8 +62,8 @@ public class Main {
     }
   }
 
-  // CHECK-START: void Main.loop5(boolean) liveness (after)
-  // CHECK:         ParameterValue  liveness:2  ranges:[ 2-50 ] uses:[ 33 42 46 50 ]
+  // CHECK[,)START: void Main.loop5(boolean) liveness (after)
+  // CHECK:         ParameterValue  liveness:2  ranges:[ [2,50) ] uses:[ 33 42 46 50 ]
   // CHECK:         Goto            liveness:44
   // CHECK:         Goto            liveness:48
   public static void loop5(boolean incoming) {
@@ -75,12 +75,12 @@ public class Main {
     }
   }
 
-  // CHECK-START: void Main.loop6(boolean) liveness (after)
-  // CHECK          ParameterValue  liveness:2  ranges:[ 2-46 ] uses:[ 24 46 ]
+  // CHECK[,)START: void Main.loop6(boolean) liveness (after)
+  // CHECK          ParameterValue  liveness:2  ranges:[ [2,46) ] uses:[ 24 46 ]
   // CHECK:         Goto            liveness:44
 
-  // CHECK-START: void Main.loop6(boolean) liveness (after)
-  // CHECK-NOT:     Goto            liveness:22
+  // CHECK[,)START: void Main.loop6(boolean) liveness (after)
+  // CHECK[,)NOT:     Goto            liveness:22
   public static void loop6(boolean incoming) {
     // 'incoming' must have a use only at the first loop's back edge.
     while (true) {
@@ -89,8 +89,8 @@ public class Main {
     }
   }
 
-  // CHECK-START: void Main.loop7(boolean) liveness (after)
-  // CHECK:         ParameterValue  liveness:2  ranges:[ 2-50 ] uses:[ 32 41 46 50 ]
+  // CHECK[,)START: void Main.loop7(boolean) liveness (after)
+  // CHECK:         ParameterValue  liveness:2  ranges:[ [2,50) ] uses:[ 32 41 46 50 ]
   // CHECK:         Goto            liveness:44
   // CHECK:         Goto            liveness:48
   public static void loop7(boolean incoming) {
@@ -101,8 +101,8 @@ public class Main {
     }
   }
 
-  // CHECK-START: void Main.loop8() liveness (after)
-  // CHECK:         StaticFieldGet  liveness:12 ranges:[ 12-44 ] uses:[ 35 40 44 ]
+  // CHECK[,)START: void Main.loop8() liveness (after)
+  // CHECK:         StaticFieldGet  liveness:12 ranges:[ [12,44) ] uses:[ 35 40 44 ]
   // CHECK:         Goto            liveness:38
   // CHECK:         Goto            liveness:42
   public static void loop8() {
@@ -113,8 +113,8 @@ public class Main {
     }
   }
 
-  // CHECK-START: void Main.loop9() liveness (after)
-  // CHECK:         StaticFieldGet  liveness:22 ranges:[ 22-36 ] uses:[ 31 36 ]
+  // CHECK[,)START: void Main.loop9() liveness (after)
+  // CHECK:         StaticFieldGet  liveness:22 ranges:[ [22,36) ] uses:[ 31 36 ]
   // CHECK:         Goto            liveness:38
   public static void loop9() {
     while (Runtime.getRuntime() != null) {
