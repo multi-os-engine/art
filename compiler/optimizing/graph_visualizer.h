955760037c025d9a2d178aeb3164d0b1136c03e2
Alexandre Rames <alexandre.rames@linaro.org>
2015-06-01 10:41:39 +0100

    Opt compiler: Add disassembly to the '.cfg' output.
    This is automatically added to the '.cfg' output when using the usual
    `--dump-cfg` option.
    
    Change-Id: I864bfc3a8299c042e72e451cc7730ad8271e4deb

--- compiler/optimizing/graph_visualizer.h
+++ compiler/optimizing/graph_visualizer.h
@@ -18,14 +18,20 @@
 #define ART_COMPILER_OPTIMIZING_GRAPH_VISUALIZER_H_
 
 #include <ostream>
+#include <vector>
 
+#include "arch/instruction_set.h"
+#include "base/arena_containers.h"
 #include "base/value_object.h"
 
 namespace art {
 
 class CodeGenerator;
 class DexCompilationUnit;
+class DisassemblyInformation;
 class HGraph;
+class HInstruction;
+class SlowPathCode;
 
 /**
  * This class outputs the HGraph in the C1visualizer format.
@@ -39,6 +45,9 @@ class HGraphVisualizer : public ValueObject {
 
   void PrintHeader(const char* method_name) const;
   void DumpGraph(const char* pass_name, bool is_after_pass = true) const;
+  void DumpGraphWithDisassembly(DisassemblyInformation* disasm_info) const;
+
+  const CodeGenerator& GetCodegen() const { return codegen_; }
 
  private:
   std::ostream* const output_;
@@ -46,6 +55,58 @@ class HGraphVisualizer : public ValueObject {
   const CodeGenerator& codegen_;
 
   DISALLOW_COPY_AND_ASSIGN(HGraphVisualizer);
+};
+
+struct GeneratedCodeInterval {
+  size_t start;
+  size_t end;
+};
+
+struct SlowPathCodeInfo {
+  const SlowPathCode* slow_path;
+  GeneratedCodeInterval code_interval;
+};
+
+// This information is filled by the code generator. It will be used by the
+// graph visualizer to associate disassembly of the generated code with the
+// instructions and slow paths. We assume that the generated code follows the
+// following structure:
+//   - frame entry
+//   - instructions
+//   - slow paths
+class DisassemblyInformation {
+ public:
+  explicit DisassemblyInformation(ArenaAllocator* allocator)
+      : function_frame_entry_code_info_({0, 0}),
+        instruction_code_offsets_(std::less<const HInstruction*>(), allocator->Adapter()),
+        slow_paths_(allocator->Adapter()) {}
+
+  void SetEndOfFrameEntry(size_t end_of_frame) {
+    function_frame_entry_code_info_ = {0, end_of_frame};
+  }
+
+  void AddInstructionCodeOffsets(HInstruction* instr, size_t start, size_t end) {
+    instruction_code_offsets_.Put(instr, {start, end});
+  }
+
+  void AddSlowPathCodeInfo(SlowPathCode* slow_path, size_t start, size_t end) {
+    slow_paths_.push_back({slow_path, {start, end}});
+  }
+
+  GeneratedCodeInterval GetFunctionFrameEntryCodeInfo() const {
+    return function_frame_entry_code_info_;
+  }
+
+  const ArenaSafeMap<const HInstruction*, GeneratedCodeInterval>& InstructionCodeOffsets() const {
+    return instruction_code_offsets_;
+  }
+
+  const ArenaVector<SlowPathCodeInfo>& GetSlowPaths() const { return slow_paths_; }
+
+ private:
+  GeneratedCodeInterval function_frame_entry_code_info_;
+  ArenaSafeMap<const HInstruction*, GeneratedCodeInterval> instruction_code_offsets_;
+  ArenaVector<SlowPathCodeInfo> slow_paths_;
 };
 
 }  // namespace art
