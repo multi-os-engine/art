b675b1cc71388c0b98e92492cc22eefe1e25da3d
David Brazdil <dbrazdil@google.com>
2015-06-11 11:17:49 +0100

    ART: Implement try/catch blocks in Builder
    This patch enables the GraphBuilder to generate blocks and edges which
    represent the exceptional control flow when try/catch blocks are
    present in the code. Actual compilation is still delegated to Quick
    and Baseline ignores the additional code.
    
    To represent the relationship between try and catch blocks, Builder
    splits the edges which enter/exit a try block and links the newly
    created blocks to the corresponding exception handlers. This layout
    will later enable the SsaBuilder to correctly infer the dominators of
    the catch blocks and to produce the appropriate reverse post ordering.
    It will not, however, allow for building the complete SSA form of the
    catch blocks and consequently optimizing such blocks.
    
    To this end, a new TryBoundary control-flow instruction is introduced.
    Codegen treats it the same as a Goto but it allows for additional
    successors (the handlers).
    
    Change-Id: I415b985596d5bebb7b1bb358a46e08b7b04bb53a

--- compiler/optimizing/nodes.cc
+++ compiler/optimizing/nodes.cc
@@ -174,15 +174,20 @@ void HGraph::TransformToSsa() {
   ssa_builder.BuildSsa();
 }
 
-void HGraph::SplitCriticalEdge(HBasicBlock* block, HBasicBlock* successor) {
-  // Insert a new node between `block` and `successor` to split the
-  // critical edge.
+HBasicBlock* HGraph::SplitEdge(HBasicBlock* block, HBasicBlock* successor) {
   HBasicBlock* new_block = new (arena_) HBasicBlock(this, successor->GetDexPc());
   AddBlock(new_block);
-  new_block->AddInstruction(new (arena_) HGoto());
   // Use `InsertBetween` to ensure the predecessor index and successor index of
   // `block` and `successor` are preserved.
   new_block->InsertBetween(block, successor);
+  return new_block;
+}
+
+void HGraph::SplitCriticalEdge(HBasicBlock* block, HBasicBlock* successor) {
+  // Insert a new node between `block` and `successor` to split the
+  // critical edge.
+  HBasicBlock* new_block = SplitEdge(block, successor);
+  new_block->AddInstruction(new (arena_) HGoto());
   if (successor->IsLoopHeader()) {
     // If we split at a back edge boundary, make the new block the back edge.
     HLoopInformation* info = successor->GetLoopInformation();
@@ -441,6 +446,12 @@ size_t HLoopInformation::GetLifetimeEnd() const {
     last_position = std::max(back_edges_.Get(i)->GetLifetimeEnd(), last_position);
   }
   return last_position;
+}
+
+bool HBasicBlock::IsExceptionHandlerSuccessor(size_t idx) const {
+  return instructions_.last_instruction_ != nullptr
+      && instructions_.last_instruction_->IsTryBoundary()
+      && instructions_.last_instruction_->AsTryBoundary()->IsExceptionHandlerSuccessor(idx);
 }
 
 bool HBasicBlock::Dominates(HBasicBlock* other) const {
@@ -1039,7 +1050,7 @@ bool HBasicBlock::IsSingleGoto() const {
   return GetFirstInstruction() != nullptr
          && GetPhis().IsEmpty()
          && GetFirstInstruction() == GetLastInstruction()
-         && GetLastInstruction()->IsGoto()
+         && (GetLastInstruction()->IsGoto() || GetLastInstruction()->IsTryBoundary())
          // Back edges generate the suspend check.
          && (loop_info == nullptr || !loop_info->IsBackEdge(*this));
 }
