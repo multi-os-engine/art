a89425b665ca7f3458a7c9ce4e1da3a97f4d8c3b
Man Cao <manc@google.com>
2015-07-14 22:31:42 -0700

    WIP: Add read barrier support to the entrypoints.
    Also remove "THIS_LOAD_REQUIRES_READ_BARRIER" since reading
    an ArtMethod* no longer needs read barrier.
    
    stub_test should also work with read barriers now.
    
    Change-Id: I3fba18042de2f867a18dbdc38519986212bd9769

--- runtime/arch/x86/entrypoints_init_x86.cc
+++ runtime/arch/x86/entrypoints_init_x86.cc
@@ -141,6 +141,7 @@ void InitEntryPoints(InterpreterEntryPoints* ipoints, JniEntryPoints* jpoints,
 
   // Read barrier
   qpoints->pReadBarrierJni = ReadBarrierJni;
+  qpoints->pReadBarrierSlow = artReadBarrierSlow;
 };
 
 }  // namespace art
