5202e4e86fdae676375f5b60aaa9b589855e77bd
Alexandre Rames <alexandre.rames@linaro.org>
2015-08-10 16:06:15 +0100

    Add a GVN dependency 'GC' for garbage collection.
    This will be used by incoming architecture specific optimizations. The
    dependencies must be conservative. When an HInstruction is created we
    may not be sure whether it can trigger GC. In that case the
    'ChangesGC' dependency must be set. We control at code-generation time
    that HInstructions that can call have the 'ChangesGC' dependency
    set.
    
    Change-Id: Iea6a7f430009f37a9599b0a0039207049906e45d

--- compiler/optimizing/code_generator.h
+++ compiler/optimizing/code_generator.h
@@ -292,6 +292,9 @@ class CodeGenerator {
     return type == Primitive::kPrimNot && !value->IsNullConstant();
   }
 
+  void ValidateInvokeRuntime(HInstruction* instruction,
+                             SlowPathCode* slow_path);
+
   void AddAllocatedRegister(Location location) {
     allocated_registers_.Add(location);
   }
