7099ab541b6befb40b8cd1fe878c76cb679eea1f
Alexandre Rames <alexandre.rames@linaro.org>
2015-08-10 15:55:27 +0100

    ARM64: Instruction simplification for array accesses.
    HArrayGet and HArraySet with variable indexes generate two
    instructions on arm64, like
    
        add temp, obj, #data_offset
        ldr out, [temp, index LSL #shift_amount]
    
    When we have multiple accesses to the same array, the initial `add`
    instruction is redundant.
    
    This patch introduces the first instruction simplification in the
    arm64-specific instruction simplification pass. It splits HArrayGet
    and HArraySet using the new arm64-specific IR HIntermediateAddress.
    After that we run GVN again to squash the multiple occurrences of
    HIntermediateAddress.
    
    Change-Id: I2e3d12fbb07fed07b2cb2f3f47f99f5a032f8312

--- compiler/optimizing/code_generator_arm64.cc
+++ compiler/optimizing/code_generator_arm64.cc
@@ -1460,6 +1460,20 @@ void InstructionCodeGeneratorARM64::VisitAnd(HAnd* instruction) {
   HandleBinaryOp(instruction);
 }
 
+void LocationsBuilderARM64::VisitIntermediateAddress(HIntermediateAddress* instruction) {
+  LocationSummary* locations =
+      new (GetGraph()->GetArena()) LocationSummary(instruction, LocationSummary::kNoCall);
+  locations->SetInAt(0, Location::RequiresRegister());
+  locations->SetInAt(1, ARM64EncodableConstantOrRegister(instruction->GetOffset(), instruction));
+  locations->SetOut(Location::RequiresRegister());
+}
+
+void InstructionCodeGeneratorARM64::VisitIntermediateAddress(HIntermediateAddress* instruction) {
+  __ Add(OutputRegister(instruction),
+         InputRegisterAt(instruction, 0),
+         Operand(InputOperandAt(instruction, 1)));
+}
+
 void LocationsBuilderARM64::VisitArrayGet(HArrayGet* instruction) {
   LocationSummary* locations =
       new (GetGraph()->GetArena()) LocationSummary(instruction, LocationSummary::kNoCall);
@@ -1473,14 +1487,16 @@ void LocationsBuilderARM64::VisitArrayGet(HArrayGet* instruction) {
 }
 
 void InstructionCodeGeneratorARM64::VisitArrayGet(HArrayGet* instruction) {
-  LocationSummary* locations = instruction->GetLocations();
   Primitive::Type type = instruction->GetType();
   Register obj = InputRegisterAt(instruction, 0);
-  Location index = locations->InAt(1);
+  Location index = instruction->GetLocations()->InAt(1);
   size_t offset = mirror::Array::DataOffset(Primitive::ComponentSize(type)).Uint32Value();
   MemOperand source = HeapOperand(obj);
+  CPURegister dest = OutputCPURegister(instruction);
+
   MacroAssembler* masm = GetVIXLAssembler();
   UseScratchRegisterScope temps(masm);
+  // Block pools between `Load` and `MaybeRecordImplicitNullCheck`.
   BlockPoolsScope block_pools(masm);
 
   if (index.IsConstant()) {
@@ -1488,15 +1504,22 @@ void InstructionCodeGeneratorARM64::VisitArrayGet(HArrayGet* instruction) {
     source = HeapOperand(obj, offset);
   } else {
     Register temp = temps.AcquireSameSizeAs(obj);
-    __ Add(temp, obj, offset);
+    if (instruction->GetArray()->IsIntermediateAddress()) {
+      // We do not need to compute the intermediate address from the array: the
+      // input instruction has done it already. See the comment in
+      // `InstructionSimplifierArm64::TryExtractArrayAccessAddress()`.
+      temp = obj;
+    } else {
+      __ Add(temp, obj, offset);
+    }
     source = HeapOperand(temp, XRegisterFrom(index), LSL, Primitive::ComponentSizeShift(type));
   }
 
-  codegen_->Load(type, OutputCPURegister(instruction), source);
+  codegen_->Load(type, dest, source);
   codegen_->MaybeRecordImplicitNullCheck(instruction);
 
-  if (type == Primitive::kPrimNot) {
-    GetAssembler()->MaybeUnpoisonHeapReference(OutputCPURegister(instruction).W());
+  if (instruction->GetType() == Primitive::kPrimNot) {
+    GetAssembler()->MaybeUnpoisonHeapReference(dest.W());
   }
 }
 
@@ -1539,6 +1562,9 @@ void InstructionCodeGeneratorARM64::VisitArraySet(HArraySet* instruction) {
   LocationSummary* locations = instruction->GetLocations();
   bool needs_runtime_call = locations->WillCall();
 
+  // Only the real array must be passed to a runtime call.
+  DCHECK(!needs_runtime_call || !instruction->GetArray()->IsIntermediateAddress());
+
   if (needs_runtime_call) {
     // Note: if heap poisoning is enabled, pAputObject takes cares
     // of poisoning the reference.
@@ -1553,6 +1579,8 @@ void InstructionCodeGeneratorARM64::VisitArraySet(HArraySet* instruction) {
     size_t offset = mirror::Array::DataOffset(Primitive::ComponentSize(value_type)).Uint32Value();
     MemOperand destination = HeapOperand(obj);
     MacroAssembler* masm = GetVIXLAssembler();
+
+    // Block pools between `Store` and `MaybeRecordImplicitNullCheck`.
     BlockPoolsScope block_pools(masm);
     {
       // We use a block to end the scratch scope before the write barrier, thus
@@ -1572,7 +1600,14 @@ void InstructionCodeGeneratorARM64::VisitArraySet(HArraySet* instruction) {
         destination = HeapOperand(obj, offset);
       } else {
         Register temp = temps.AcquireSameSizeAs(obj);
-        __ Add(temp, obj, offset);
+        if (instruction->GetArray()->IsIntermediateAddress()) {
+          // We do not need to compute the intermediate address from the array: the
+          // input instruction has done it already. See the comment in
+          // `InstructionSimplifierArm64::TryExtractArrayAccessAddress()`.
+          temp = obj;
+        } else {
+          __ Add(temp, obj, offset);
+        }
         destination = HeapOperand(temp,
                                   XRegisterFrom(index),
                                   LSL,
