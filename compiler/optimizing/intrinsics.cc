f35e54cfebccdf671795cfdbca82303750c9096a
Jean-Philippe Halimi <jean-philippe.halimi@intel.com>
2015-09-03 17:28:38 +0200

    Add stats support for existing optimizations
    This patch adds support for the --dump-stats facility with existing
    optimizations.
    
    Change-Id: I68751b119a030952a11057cb651a3c63e87e73ea
    Signed-off-by: Jean-Philippe Halimi <jean-philippe.halimi@intel.com>

--- compiler/optimizing/intrinsics.cc
+++ compiler/optimizing/intrinsics.cc
@@ -441,6 +441,7 @@ void IntrinsicsRecognizer::Run() {
                   << invoke->DebugName();
             } else {
               invoke->SetIntrinsic(intrinsic, NeedsEnvironmentOrCache(intrinsic));
+              MaybeRecordStat(MethodCompilationStat::kIntrinsicsRecognizer);
             }
           }
         }
