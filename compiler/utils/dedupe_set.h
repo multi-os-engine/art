738493f4c0c1227f615c8527560cf33be1c629c1
Vladimir Marko <vmarko@google.com>
2015-09-11 11:59:18 +0100

    Reduce memory used by CompiledMethods.
    Use LengthPrefixedArray<>s instead of SwapVector<>s to store
    CompiledMethod data and get rid of the unnecessary members
    of CompiledMethod to reduce dex2oat memory usage. Refactor
    the deduplication from CompilerDriver to a new class.
    
    Use HashSet<> instead of std::set<> for the DedupeSet<> to
    further decrease the memory usage and improve performance.
    
    This reduces the dex2oat memory usage when compiling boot
    image on Nexus 5 (with Quick) by ~7.5MiB (6%) and also
    reduces the compile time by ~2% (~1% overall dex2oat time)
    when measured with userspace governor at frequency 1036800.
    (The compile time improvement looks higher when we don't
    restrict the frequency but the results are very random due
    to thermal throttling.)
    
    Change-Id: I974f1f5e58350de2bf487a2bca3907fa05fb80ea

--- compiler/utils/dedupe_set.h
+++ compiler/utils/dedupe_set.h
@@ -22,8 +22,10 @@
 #include <memory>
 #include <set>
 #include <string>
+#include <unordered_map>
 
 #include "base/mutex.h"
+#include "base/hash_set.h"
 #include "base/stl_util.h"
 #include "base/stringprintf.h"
 #include "base/time_utils.h"
@@ -34,44 +36,11 @@ namespace art {
 // A set of Keys that support a HashFunc returning HashType. Used to find duplicates of Key in the
 // Add method. The data-structure is thread-safe through the use of internal locks, it also
 // supports the lock being sharded.
-template <typename InKey, typename StoreKey, typename HashType, typename HashFunc,
+template <typename InKey, typename StoreKey, typename Alloc, typename HashType, typename HashFunc,
           HashType kShard = 1>
 class DedupeSet {
-  typedef std::pair<HashType, const InKey*> HashedInKey;
-  struct HashedKey {
-    StoreKey* store_ptr;
-    union {
-      HashType store_hash;        // Valid if store_ptr != null.
-      const HashedInKey* in_key;  // Valid if store_ptr == null.
-    };
-  };
-
-  class Comparator {
-   public:
-    bool operator()(const HashedKey& a, const HashedKey& b) const {
-      HashType a_hash = (a.store_ptr != nullptr) ? a.store_hash : a.in_key->first;
-      HashType b_hash = (b.store_ptr != nullptr) ? b.store_hash : b.in_key->first;
-      if (a_hash != b_hash) {
-        return a_hash < b_hash;
-      }
-      if (a.store_ptr != nullptr && b.store_ptr != nullptr) {
-        return std::lexicographical_compare(a.store_ptr->begin(), a.store_ptr->end(),
-                                            b.store_ptr->begin(), b.store_ptr->end());
-      } else if (a.store_ptr != nullptr && b.store_ptr == nullptr) {
-        return std::lexicographical_compare(a.store_ptr->begin(), a.store_ptr->end(),
-                                            b.in_key->second->begin(), b.in_key->second->end());
-      } else if (a.store_ptr == nullptr && b.store_ptr != nullptr) {
-        return std::lexicographical_compare(a.in_key->second->begin(), a.in_key->second->end(),
-                                            b.store_ptr->begin(), b.store_ptr->end());
-      } else {
-        return std::lexicographical_compare(a.in_key->second->begin(), a.in_key->second->end(),
-                                            b.in_key->second->begin(), b.in_key->second->end());
-      }
-    }
-  };
-
  public:
-  StoreKey* Add(Thread* self, const InKey& key) {
+  const StoreKey* Add(Thread* self, const InKey& key) {
     uint64_t hash_start;
     if (kIsDebugBuild) {
       hash_start = NanoTime();
@@ -83,85 +52,166 @@ class DedupeSet {
     }
     HashType shard_hash = raw_hash / kShard;
     HashType shard_bin = raw_hash % kShard;
-    HashedInKey hashed_in_key(shard_hash, &key);
-    HashedKey hashed_key;
-    hashed_key.store_ptr = nullptr;
-    hashed_key.in_key = &hashed_in_key;
-    MutexLock lock(self, *lock_[shard_bin]);
-    auto it = keys_[shard_bin].find(hashed_key);
-    if (it != keys_[shard_bin].end()) {
-      DCHECK(it->store_ptr != nullptr);
-      return it->store_ptr;
-    }
-    hashed_key.store_ptr = CreateStoreKey(key);
-    hashed_key.store_hash = shard_hash;
-    keys_[shard_bin].insert(hashed_key);
-    return hashed_key.store_ptr;
+    return shards_[shard_bin]->Add(self, shard_hash, key);
   }
 
-  DedupeSet(const char* set_name, SwapAllocator<void>& alloc)
+  DedupeSet(const char* set_name, const Alloc& alloc)
       : allocator_(alloc), hash_time_(0) {
     for (HashType i = 0; i < kShard; ++i) {
       std::ostringstream oss;
       oss << set_name << " lock " << i;
-      lock_name_[i] = oss.str();
-      lock_[i].reset(new Mutex(lock_name_[i].c_str()));
+      shards_[i].reset(new Shard(&allocator_, oss.str()));
     }
   }
 
   ~DedupeSet() {
-    // Have to manually free all pointers.
-    for (auto& shard : keys_) {
-      for (const auto& hashed_key : shard) {
-        DCHECK(hashed_key.store_ptr != nullptr);
-        DeleteStoreKey(hashed_key.store_ptr);
-      }
-    }
+    // Everything done by member destructors.
   }
 
-  std::string DumpStats() const {
-    size_t collision_sum = 0;
-    size_t collision_max = 0;
+  std::string DumpStats(Thread* self) const {
+    Stats stats;
     for (HashType shard = 0; shard < kShard; ++shard) {
-      HashType last_hash = 0;
-      size_t collision_cur_max = 0;
-      for (const HashedKey& key : keys_[shard]) {
-        DCHECK(key.store_ptr != nullptr);
-        if (key.store_hash == last_hash) {
-          collision_cur_max++;
-          if (collision_cur_max > 1) {
-            collision_sum++;
-            if (collision_cur_max > collision_max) {
-              collision_max = collision_cur_max;
-            }
-          }
-        } else {
-          collision_cur_max = 1;
-          last_hash = key.store_hash;
-        }
-      }
+      shards_[shard]->UpdateStats(self, &stats);
     }
-    return StringPrintf("%zu collisions, %zu max bucket size, %" PRIu64 " ns hash time",
-                        collision_sum, collision_max, hash_time_);
+    return StringPrintf("%zu collisions, %zu max hash collisions, "
+                        "%zu/%zu probe distance, %" PRIu64 " ns hash time",
+                        stats.collision_sum,
+                        stats.collision_max,
+                        stats.total_probe_distance,
+                        stats.total_size,
+                        hash_time_);
   }
 
  private:
-  StoreKey* CreateStoreKey(const InKey& key) {
-    StoreKey* ret = allocator_.allocate(1);
-    allocator_.construct(ret, key.begin(), key.end(), allocator_);
-    return ret;
-  }
+  struct Stats {
+    size_t collision_sum = 0u;
+    size_t collision_max = 0u;
+    size_t total_probe_distance = 0u;
+    size_t total_size = 0u;
+  };
 
-  void DeleteStoreKey(StoreKey* key) {
-    SwapAllocator<StoreKey> alloc(allocator_);
-    alloc.destroy(key);
-    alloc.deallocate(key, 1);
-  }
+  template <typename T>
+  class HashedKey {
+   public:
+    HashedKey() : hash_(0u), key_(nullptr) { }
+    HashedKey(size_t hash, const T* key) : hash_(hash), key_(key) { }
 
-  std::string lock_name_[kShard];
-  std::unique_ptr<Mutex> lock_[kShard];
-  std::set<HashedKey, Comparator> keys_[kShard];
-  SwapAllocator<StoreKey> allocator_;
+    size_t Hash() const {
+      return hash_;
+    }
+
+    const T* Key() const {
+      return key_;
+    }
+
+    bool IsEmpty() const {
+      return Key() == nullptr;
+    }
+
+    void MakeEmpty() {
+      key_ = nullptr;
+    }
+
+   private:
+    size_t hash_;
+    const T* key_;
+  };
+
+  class ShardEmptyFn {
+   public:
+    bool IsEmpty(const HashedKey<StoreKey>& key) const {
+      return key.IsEmpty();
+    }
+
+    void MakeEmpty(HashedKey<StoreKey>& key) {
+      key.MakeEmpty();
+    }
+  };
+
+  struct ShardHashFn {
+    template <typename T>
+    size_t operator()(const HashedKey<T>& key) const {
+      return key.Hash();
+    }
+  };
+
+  struct ShardPred {
+    template <typename LeftT, typename RightT>
+    bool operator()(const HashedKey<LeftT>& lhs, const HashedKey<RightT>& rhs) const {
+      DCHECK(lhs.Key() != nullptr);
+      DCHECK(rhs.Key() != nullptr);
+      return lhs.Hash() == rhs.Hash() &&
+          lhs.Key()->size() == rhs.Key()->size() &&
+          std::equal(lhs.Key()->begin(), lhs.Key()->end(), rhs.Key()->begin());
+    }
+  };
+
+  class Shard {
+   public:
+    Shard(Alloc* alloc, const std::string& lock_name)
+        : alloc_(alloc),
+          lock_name_(lock_name),
+          lock_(lock_name_.c_str()),
+          keys_() {
+    }
+
+    ~Shard() {
+      for (const HashedKey<StoreKey>& key : keys_) {
+        DCHECK(key.Key() != nullptr);
+        alloc_->Destroy(key.Key());
+      }
+    }
+
+    const StoreKey* Add(Thread* self, size_t hash, const InKey& in_key) REQUIRES(!lock_) {
+      MutexLock lock(self, lock_);
+      HashedKey<InKey> hashed_in_key(hash, &in_key);
+      auto it = keys_.Find(hashed_in_key);
+      if (it != keys_.end()) {
+        DCHECK(it->Key() != nullptr);
+        return it->Key();
+      }
+      const StoreKey* store_key = alloc_->Copy(in_key);
+      keys_.Insert(HashedKey<StoreKey> { hash, store_key });
+      return store_key;
+    }
+
+    void UpdateStats(Thread* self, Stats* global_stats) REQUIRES(!lock_) {
+      // HashSet<> doesn't keep entries ordered by hash, so we actually allocate memory
+      // for bookkeeping while collecting the stats.
+      std::unordered_map<HashType, size_t> stats;
+      {
+        MutexLock lock(self, lock_);
+        // Note: The total_probe_distance will be updated with the current state.
+        // It may have been higher before a re-hash.
+        global_stats->total_probe_distance += keys_.TotalProbeDistance();
+        global_stats->total_size += keys_.Size();
+        for (const HashedKey<StoreKey>& key : keys_) {
+          auto it = stats.find(key.Hash());
+          if (it == stats.end()) {
+            stats.insert({key.Hash(), 1u});
+          } else {
+            ++it->second;
+          }
+        }
+      }
+      for (const auto& entry : stats) {
+        size_t number_of_entries = entry.second;
+        if (number_of_entries > 1u) {
+          global_stats->collision_sum += number_of_entries - 1u;
+          global_stats->collision_max = std::max(global_stats->collision_max, number_of_entries);
+        }
+      }
+    }
+
+   private:
+    Alloc* const alloc_;
+    const std::string lock_name_;
+    Mutex lock_;
+    HashSet<HashedKey<StoreKey>, ShardEmptyFn, ShardHashFn, ShardPred> keys_ GUARDED_BY(lock_);
+  };
+
+  Alloc allocator_;
+  std::unique_ptr<Shard> shards_[kShard];
   uint64_t hash_time_;
 
   DISALLOW_COPY_AND_ASSIGN(DedupeSet);
--- runtime/class_linker.cc
+++ runtime/class_linker.cc
@@ -4879,7 +4879,7 @@ bool ClassLinker::LinkInterfaceMethods(Thread* self,
     ScopedArenaUnorderedMap<ArtMethod*, ArtMethod*> move_table(allocator.Adapter());
     if (virtuals != old_virtuals) {
       // Maps from heap allocated miranda method to linear alloc miranda method.
-      StrideIterator<ArtMethod> out = virtuals->Begin(method_size, method_alignment);
+      StrideIterator<ArtMethod> out = virtuals->begin(method_size, method_alignment);
       // Copy over the old methods + miranda methods.
       for (auto& m : klass->GetVirtualMethods(image_pointer_size_)) {
         move_table.emplace(&m, &*out);
@@ -4889,7 +4889,7 @@ bool ClassLinker::LinkInterfaceMethods(Thread* self,
         ++out;
       }
     }
-    StrideIterator<ArtMethod> out(virtuals->Begin(method_size, method_alignment) + old_method_count);
+    StrideIterator<ArtMethod> out(virtuals->begin(method_size, method_alignment) + old_method_count);
     // Copy over miranda methods before copying vtable since CopyOf may cause thread suspension and
     // we want the roots of the miranda methods to get visited.
     for (ArtMethod* mir_method : miranda_methods) {
@@ -4912,7 +4912,7 @@ bool ClassLinker::LinkInterfaceMethods(Thread* self,
       self->AssertPendingOOMException();
       return false;
     }
-    out = virtuals->Begin(method_size, method_alignment) + old_method_count;
+    out = virtuals->begin(method_size, method_alignment) + old_method_count;
     size_t vtable_pos = old_vtable_count;
     for (size_t i = old_method_count; i < new_method_count; ++i) {
       // Leave the declaring class alone as type indices are relative to it
