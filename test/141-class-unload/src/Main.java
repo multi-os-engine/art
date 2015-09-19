db414a3c6eca1b1e2b39e53cda421b7ac38241be
Mathieu Chartier <mathieuc@google.com>
2015-09-19 12:44:38 -0700

    Enable class unloading
    Also added class unloading test.
    
    Bug: 22720414
    Change-Id: Iff615d69b574a4438e91d4c844279d202f4f2736

---
+++ test/141-class-unload/src/Main.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Main {
+    static final String DEX_FILE = System.getenv("DEX_LOCATION") + "/141-class-unload-ex.jar";
+
+    public static void main(String[] args) throws Exception {
+        Class pathClassLoader = Class.forName("dalvik.system.PathClassLoader");
+        if (pathClassLoader == null) {
+            throw new AssertionError("Couldn't find path class loader class");
+        }
+        Constructor constructor =
+            pathClassLoader.getDeclaredConstructor(String.class, ClassLoader.class);
+        try {
+            WeakReference<ClassLoader> loader = testUnloadLoader(constructor);
+            WeakReference<Class> klass = testUnloadClass(constructor);
+            // No strong refernces to class loader, should get unloaded.
+            Runtime.getRuntime().gc();
+            // If the weak reference is cleared, then it was unloaded.
+            System.out.println(klass.get());
+            System.out.println(loader.get());
+            // Stress test
+            for (int i = 0; i < 1000; ++i) {
+                testUnloadLoader(constructor);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private static WeakReference<Class> testUnloadClass(Constructor constructor)
+        throws Exception {
+        ClassLoader loader = (ClassLoader) constructor.newInstance(
+            DEX_FILE, ClassLoader.getSystemClassLoader());
+        Class intHolder = loader.loadClass("IntHolder");
+        Method getValue = intHolder.getDeclaredMethod("getValue");
+        Method setValue = intHolder.getDeclaredMethod("setValue", Integer.TYPE);
+        System.out.println((int) getValue.invoke(intHolder));
+        setValue.invoke(intHolder, 2);
+        System.out.println((int) getValue.invoke(intHolder));
+        return new WeakReference(intHolder);
+    }
+
+    private static WeakReference<ClassLoader> testUnloadLoader(Constructor constructor)
+        throws Exception {
+        ClassLoader loader = (ClassLoader) constructor.newInstance(
+            DEX_FILE, ClassLoader.getSystemClassLoader());
+        Class intHolder = loader.loadClass("IntHolder");
+        Method getValue = intHolder.getDeclaredMethod("getValue");
+        Method setValue = intHolder.getDeclaredMethod("setValue", Integer.TYPE);
+        setValue.invoke(intHolder, 2);
+        return new WeakReference(loader);
+    }
+
+}
