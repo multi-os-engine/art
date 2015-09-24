053a7bf1a51f5737b0183c201057ba92a76e99d7
Mark Mendell <mark.p.mendell@intel.com>
2015-09-24 08:43:40 -0400

    Address HPackedSwitch issues raised after merge
    There were some stylistic comments about the merged files.  Fix those.
    
    Add a test that PackedSwitch can be removed by DCE.
    
    Change-Id: Idf45833956e9b58051f942a52b06a1e416606e2e
    Signed-off-by: Mark Mendell <mark.p.mendell@intel.com>

---
+++ test/485-checker-dce-switch/src/Main.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class Main {
+
+  public static int $inline$method() {
+    return 5;
+  }
+
+  /// CHECK-START: int Main.whole_switch_dead(int) dead_code_elimination_final (before)
+  /// CHECK-DAG:                      PackedSwitch
+
+  /// CHECK-START: int Main.whole_switch_dead(int) dead_code_elimination_final (after)
+  /// CHECK-DAG:    <<Const100:i\d+>> IntConstant 100
+  /// CHECK-NOT:                      PackedSwitch
+  /// CHECK-DAG:                      Return [<<Const100>>]
+
+  /// CHECK-START: int Main.whole_switch_dead(int) dead_code_elimination_final (after)
+  /// CHECK-NOT:                      PackedSwitch
+  public static int whole_switch_dead(int j) {
+    int i = $inline$method();
+    int l = 100;
+    if (i > 100) {
+      switch(j) {
+        case 1:
+          i++;
+          break;
+        case 2:
+          i = 99;
+          break;
+        case 3:
+          i = 100;
+          break;
+        case 4:
+          i = -100;
+          break;
+        case 5:
+          i = 7;
+          break;
+        case 6:
+          i = -9;
+          break;
+      }
+      l += i;
+    }
+
+    return l;
+  }
+
+  /// CHECK-START: int Main.constant_switch() dead_code_elimination_final (before)
+  /// CHECK-DAG:                      PackedSwitch
+
+  /// CHECK-START: int Main.constant_switch() dead_code_elimination_final (after)
+  /// CHECK-DAG:     <<Const7:i\d+>>  IntConstant 7
+  /// CHECK-NOT:                      PackedSwitch
+  /// CHECK-DAG:                      Return [<<Const7>>]
+
+  /// CHECK-START: int Main.constant_switch() dead_code_elimination_final (after)
+  /// CHECK-NOT:                      PackedSwitch
+  public static int constant_switch() {
+    int i = $inline$method();
+    switch(i) {
+      case 1:
+        i++;
+        break;
+      case 2:
+        i = 99;
+        break;
+      case 3:
+        i = 100;
+        break;
+      case 4:
+        i = -100;
+        break;
+      case 5:
+        i = 7;
+        break;
+      case 6:
+        i = -9;
+        break;
+    }
+
+    return i;
+  }
+
+  /// CHECK-START: int Main.default_constant_switch() dead_code_elimination_final (before)
+  /// CHECK-DAG:                      PackedSwitch
+
+  /// CHECK-START: int Main.default_constant_switch() dead_code_elimination_final (after)
+  /// CHECK-DAG:     <<Const15:i\d+>>  IntConstant 15
+  /// CHECK-NOT:                      PackedSwitch
+  /// CHECK-DAG:                      Return [<<Const15>>]
+
+  /// CHECK-START: int Main.default_constant_switch() dead_code_elimination_final (after)
+  /// CHECK-NOT:                      PackedSwitch
+  public static int default_constant_switch() {
+    int i = $inline$method() + 10;
+    switch(i) {
+      case 1:
+        i++;
+        break;
+      case 2:
+        i = 99;
+        break;
+      case 3:
+        i = 100;
+        break;
+      case 4:
+        i = -100;
+        break;
+      case 5:
+        i = 7;
+        break;
+      case 6:
+        i = -9;
+        break;
+    }
+
+    return i;
+  }
+
+  public static void main(String[] args) throws Exception {
+    int ret_val = whole_switch_dead(10);
+    if (ret_val != 100) {
+      throw new Error("Incorrect return value from whole_switch_dead:" + ret_val);
+    }
+
+    ret_val = constant_switch();
+    if (ret_val != 7) {
+      throw new Error("Incorrect return value from constant_switch:" + ret_val);
+    }
+
+    ret_val = default_constant_switch();
+    if (ret_val != 15) {
+      throw new Error("Incorrect return value from default_constant_switch:" + ret_val);
+    }
+  }
+}
