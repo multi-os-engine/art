8c9032ec82e84850dcb4c2d0f04c4aca33b76699
Alex Light <allight@google.com>
2015-10-09 15:59:48 -0700

    Correct exception behavior for default methods
    Default methods are defined supposed to throw an
    IncompatibleClassChangeError (ICCE) when they are called and there is
    no "best" implementation. Previously we would simply throw an ICCE
    during class loading as soon as we noticed that this would happen if
    called. This makes us wait until we actually attempt to execute the
    method. Furthermore, this allows us to use other, non-conflicting,
    methods on the object as normal.
    
    Furthermore, this makes us correctly throw AbstractMethodErrors in
    cases where all default implementations of a method are overridden by
    abstract declarations.
    
    Adds 3 tests for this new behavior.
    
    Bug: 24618811
    
    Change-Id: Id891958a81f9b3862b2ce5919636aabef7d3422e

--- runtime/instrumentation.cc
+++ runtime/instrumentation.cc
@@ -111,7 +111,7 @@ static void UpdateEntrypoints(ArtMethod* method, const void* quick_code)
 }
 
 void Instrumentation::InstallStubsForMethod(ArtMethod* method) {
-  if (method->IsAbstract() || method->IsProxyMethod()) {
+  if (!method->IsInvokable() || method->IsProxyMethod()) {
     // Do not change stubs for these methods.
     return;
   }
@@ -728,7 +728,7 @@ bool Instrumentation::IsDeoptimizedMethodsEmpty() const {
 void Instrumentation::Deoptimize(ArtMethod* method) {
   CHECK(!method->IsNative());
   CHECK(!method->IsProxyMethod());
-  CHECK(!method->IsAbstract());
+  CHECK(method->IsInvokable());
 
   Thread* self = Thread::Current();
   {
@@ -751,7 +751,7 @@ void Instrumentation::Deoptimize(ArtMethod* method) {
 void Instrumentation::Undeoptimize(ArtMethod* method) {
   CHECK(!method->IsNative());
   CHECK(!method->IsProxyMethod());
-  CHECK(!method->IsAbstract());
+  CHECK(method->IsInvokable());
 
   Thread* self = Thread::Current();
   bool empty;
