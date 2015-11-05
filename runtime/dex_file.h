b3a4019ab34f46b7b566a590c1835ccfe0f684d1
Shinichiro Hamaji <hamaji@google.com>
2015-11-05 16:51:33 +0900

    Allow static tool to iterate over static field values
    EncodedStaticFieldValueIterator is designed to be used at
    runtime. This adds a constructor which doesn't require
    ClassLoader/Linker so that dextoc can dump values of static
    fields.
    
    Bug: 24597504
    Change-Id: I37c8fc3ad6eddc6df96138cf6897ab015953e4ef

--- runtime/dex_file.h
+++ runtime/dex_file.h
@@ -1510,6 +1510,9 @@ class ClassDataItemIterator {
 
 class EncodedStaticFieldValueIterator {
  public:
+  EncodedStaticFieldValueIterator(const DexFile& dex_file,
+                                  const DexFile::ClassDef& class_def)
+      SHARED_REQUIRES(Locks::mutator_lock_);
   EncodedStaticFieldValueIterator(const DexFile& dex_file, Handle<mirror::DexCache>* dex_cache,
                                   Handle<mirror::ClassLoader>* class_loader,
                                   ClassLinker* linker, const DexFile::ClassDef& class_def)
@@ -1541,7 +1544,12 @@ class EncodedStaticFieldValueIterator {
     kBoolean = 0x1f
   };
 
+  ValueType GetValueType() const { return type_; }
+  const jvalue& GetJavaValue() const { return jval_; }
+
  private:
+  void Init(const DexFile::ClassDef& class_def);
+
   static constexpr uint8_t kEncodedValueTypeMask = 0x1f;  // 0b11111
   static constexpr uint8_t kEncodedValueArgShift = 5;
 
