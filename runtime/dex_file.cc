c6505db9bea672d69035ac76bee3afd3221b42ea
Shinichiro Hamaji <hamaji@google.com>
2015-11-05 16:51:33 +0900

    Output static field values in dexdump
    This is necessary to generate table-of-contents of .dex files
    to prevent unnecessary rebuilds for implementation-only
    changes because these values can be used while compilin other
    modules.
    
    Also modify EncodedStaticFieldValueIterator so it can be used
    without ClassLoader/Linker.
    
    Bug: 24597504
    Change-Id: Ida0c839f9dd6961e1c1b3a380e2092042fad03bb

--- runtime/dex_file.cc
+++ runtime/dex_file.cc
@@ -2227,6 +2227,15 @@ void ClassDataItemIterator::ReadClassDataMethod() {
 }
 
 EncodedStaticFieldValueIterator::EncodedStaticFieldValueIterator(
+    const DexFile& dex_file,
+    const DexFile::ClassDef& class_def)
+    : dex_file_(dex_file), dex_cache_(nullptr),
+      class_loader_(nullptr), linker_(nullptr),
+      array_size_(), pos_(-1), type_(kByte) {
+  Init(class_def);
+}
+
+EncodedStaticFieldValueIterator::EncodedStaticFieldValueIterator(
     const DexFile& dex_file, Handle<mirror::DexCache>* dex_cache,
     Handle<mirror::ClassLoader>* class_loader, ClassLinker* linker,
     const DexFile::ClassDef& class_def)
@@ -2234,7 +2243,11 @@ EncodedStaticFieldValueIterator::EncodedStaticFieldValueIterator(
       array_size_(), pos_(-1), type_(kByte) {
   DCHECK(dex_cache != nullptr);
   DCHECK(class_loader != nullptr);
-  ptr_ = dex_file.GetEncodedStaticFieldValuesArray(class_def);
+  Init(class_def);
+}
+
+void EncodedStaticFieldValueIterator::Init(const DexFile::ClassDef& class_def) {
+  ptr_ = dex_file_.GetEncodedStaticFieldValuesArray(class_def);
   if (ptr_ == nullptr) {
     array_size_ = 0;
   } else {
@@ -2307,6 +2320,8 @@ void EncodedStaticFieldValueIterator::Next() {
 
 template<bool kTransactionActive>
 void EncodedStaticFieldValueIterator::ReadValueToField(ArtField* field) const {
+  DCHECK(dex_cache_ != nullptr);
+  DCHECK(class_loader_ != nullptr);
   switch (type_) {
     case kBoolean: field->SetBoolean<kTransactionActive>(field->GetDeclaringClass(), jval_.z);
         break;
