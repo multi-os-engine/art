7f268cdc61415844c7f3e85e0b6d7833afbf68ad
Mathieu Chartier <mathieuc@google.com>
2015-11-09 11:16:49 -0800

    Load app images
    Support in-place patching of the app image based on boot image
    location and app oat location. Only loads for art run test so far
    since we do not automatically generate app images for app installs.
    
    N5 maps launch time (~200 runs):
    Before: 930ms
    After: 878.18ms
    After + image class table: 864.57ms
    
    TODO:
    Oatdump support.
    Store class loaders as class roots in image.
    
    Bug: 22858531
    
    Change-Id: I9cbc645645e62ea2ed1ad8e139e91af7d88514c1

--- runtime/art_method.h
+++ runtime/art_method.h
@@ -477,6 +477,15 @@ class ArtMethod FINAL {
   // Returns whether the method has any compiled code, JIT or AOT.
   bool HasAnyCompiledCode() SHARED_REQUIRES(Locks::mutator_lock_);
 
+  // Update heap and native objects by the passed in visitor. Does not use read barrier.
+  template <typename Visitor>
+  ALWAYS_INLINE void UpdateObjects(const Visitor& visitor)
+      SHARED_REQUIRES(Locks::mutator_lock_);
+
+  // Update entry points by passing them through the visitor.
+  template <typename Visitor>
+  ALWAYS_INLINE void UpdateEntrypoints(const Visitor& visitor);
+
  protected:
   // Field order required by test "ValidateFieldOrderOfJavaCppUnionClasses".
   // The class we are a part of.
