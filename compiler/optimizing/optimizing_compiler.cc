1629b34904dc87c5b16b2c163cf3d11176369530
Roland Levillain <rpl@google.com>
2015-11-09 19:21:20 +0000

    ARM read barrier support for concurrent GC in Optimizing.
    This first implementation uses slow paths to instrument heap
    reference loads and GC root loads for the concurrent copying
    collector, respectively calling the artReadBarrierSlow and
    artReadBarrierForRootSlow runtime entry points.
    
    Notes:
    - This implementation does not instrument HInvokeVirtual
      nor HInvokeInterface instructions (for class reference
      loads), as the corresponding read barriers are not stricly
      required with the current concurrent copying collector.
    - Intrinsics which may eventually call (on slow path) are
      disabled when read barriers are enabled, as the current
      slow path infrastructure does not support this case.
    - When read barriers are enabled, the code generated for a
      HArraySet instruction always go into the array set slow
      path for object arrays (delegating the operation to the
      runtime), as we are lacking a mechanism to keep a
      temporary register live accross a runtime call (needed for
      the instrumentation of type checking code, which requires
      two successive read barriers).
    
    Bug: 12687968
    Change-Id: I92e8db414d029f952c07f3d3a98069e46dfdbc2a

--- compiler/optimizing/optimizing_compiler.cc
+++ compiler/optimizing/optimizing_compiler.cc
@@ -661,8 +661,8 @@ CodeGenerator* OptimizingCompiler::TryCompile(ArenaAllocator* arena,
   CompilerDriver* compiler_driver = GetCompilerDriver();
   InstructionSet instruction_set = compiler_driver->GetInstructionSet();
 
-  // Always use the thumb2 assembler: some runtime functionality (like implicit stack
-  // overflow checks) assume thumb2.
+  // Always use the Thumb-2 assembler: some runtime functionality
+  // (like implicit stack overflow checks) assume Thumb-2.
   if (instruction_set == kArm) {
     instruction_set = kThumb2;
   }
@@ -670,6 +670,7 @@ CodeGenerator* OptimizingCompiler::TryCompile(ArenaAllocator* arena,
   // Read barrier are supported only on x86 and x86-64 at the moment.
   // TODO: Add support for other architectures and remove this case.
   if (kEmitCompilerReadBarrier &&
+      instruction_set != kThumb2 &&
       instruction_set != kX86 &&
       instruction_set != kX86_64) {
     return nullptr;
