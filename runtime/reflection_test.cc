42758a707d310b82fe467ed0c3c5ce239c1a8929
Roland Levillain <rpl@google.com>
2015-11-09 19:17:00 +0000

    x86/x86-64 read barrier support for concurrent GC in Optimizing.
    This first implementation uses slow paths to instrument heap
    reference loads and GC root loads for the concurrent copying
    collector, respectively calling the artReadBarrierSlow and
    artReadBarrierForRootSlow (new) runtime entry points.
    
    Notes:
    - This implementation does not instrument HInvokeVirtual
      nor HInvokeInterface instructions (for class reference
      loads), as the corresponding read barriers are not stricly
      required with the current concurrent copying collector.
    - Intrinsics which may eventually call (on slow path) are
      disabled when read barriers are enabled, as the current
      slow path infrastructure does not support this case.
    - When read barriers are enabled, the code generated for a
      HArraySet instruction always go into the array set slow
      path for object arrays (delegating the operation to the
      runtime), as we are lacking a mechanism to keep a
      temporary register live accross a runtime call (needed for
      the instrumentation of type checking code, which requires
      two successive read barriers).
    
    Bug: 12687968
    Change-Id: I14cd6107233c326389120336f93955b28ffbb329

--- runtime/reflection_test.cc
+++ runtime/reflection_test.cc
@@ -507,6 +507,7 @@ class ReflectionTest : public CommonCompilerTest {
 
 TEST_F(ReflectionTest, StaticMainMethod) {
   TEST_DISABLED_FOR_HEAP_REFERENCE_POISONING_WITH_QUICK();
+  TEST_DISABLED_FOR_READ_BARRIER_WITH_QUICK();
   ScopedObjectAccess soa(Thread::Current());
   jobject jclass_loader = LoadDex("Main");
   StackHandleScope<1> hs(soa.Self());
