0ad4238e4be7dcf88bb931208c058733f3cff210
Vladimir Marko <vmarko@google.com>
2015-11-10 20:54:22 +0000

    ART: Fix arena allocation for valgrind.
    Move the zero-initialization check after marking the newly
    allocated chunk as defined and check only the allocated
    space without the red zone. Also mark unallocated space as
    inaccessible instead of just undefined.
    
    Change-Id: I74fc65f5b53acb74cec4e5a0146f41dacf4a1470

--- runtime/base/arena_allocator.cc
+++ runtime/base/arena_allocator.cc
@@ -321,17 +321,14 @@ void* ArenaAllocator::AllocWithMemoryTool(size_t bytes, ArenaAllocKind kind) {
     // Obtain a new block.
     ObtainNewArenaForAllocation(rounded_bytes);
     CHECK(ptr_ != nullptr);
-    MEMORY_TOOL_MAKE_UNDEFINED(ptr_, end_ - ptr_);
+    MEMORY_TOOL_MAKE_NOACCESS(ptr_, end_ - ptr_);
   }
   ArenaAllocatorStats::RecordAlloc(rounded_bytes, kind);
   uint8_t* ret = ptr_;
   ptr_ += rounded_bytes;
-  // Check that the memory is already zeroed out.
-  for (uint8_t* ptr = ret; ptr < ptr_; ++ptr) {
-    CHECK_EQ(*ptr, 0U);
-  }
   MEMORY_TOOL_MAKE_DEFINED(ret, bytes);
-  MEMORY_TOOL_MAKE_NOACCESS(ret + bytes, rounded_bytes - bytes);
+  // Check that the memory is already zeroed out.
+  DCHECK(std::all_of(ret, ret + bytes, [](uint8_t val) { return val == 0u; }));
   return ret;
 }
 
