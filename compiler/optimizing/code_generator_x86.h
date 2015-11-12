a15d968a913afe454dfba1268e2ace0e8a378ebd
David Brazdil <dbrazdil@google.com>
2015-11-12 18:37:00 +0000

    ART: Refactor GenerateTestAndBranch
    Each code generator implements a method for generating condition
    evaluation and branching to arbitrary labels. This patch refactors
    it for better clarity but also to generate fewer jumps when the true
    branch is the fallthrough successor.
    
    This is preliminary work for implementing HSelect.
    
    Change-Id: Iaa545a5ecbacb761c5aa241fa69140cf6eb5952f

--- compiler/optimizing/code_generator_x86.h
+++ compiler/optimizing/code_generator_x86.h
@@ -227,14 +227,15 @@ class InstructionCodeGeneratorX86 : public HGraphVisitor {
   void GenerateImplicitNullCheck(HNullCheck* instruction);
   void GenerateExplicitNullCheck(HNullCheck* instruction);
   void GenerateTestAndBranch(HInstruction* instruction,
+                             size_t condition_input_index,
                              Label* true_target,
                              Label* false_target,
-                             Label* always_true_target);
-  void GenerateCompareTestAndBranch(HIf* if_inst,
-                                    HCondition* condition,
+                             bool true_fallthrough,
+                             bool false_fallthrough);
+  void GenerateCompareTestAndBranch(HCondition* condition,
                                     Label* true_target,
                                     Label* false_target,
-                                    Label* always_true_target);
+                                    bool false_fallthrough);
   void GenerateFPJumps(HCondition* cond, Label* true_label, Label* false_label);
   void GenerateLongComparesAndJumps(HCondition* cond, Label* true_label, Label* false_label);
   void HandleGoto(HInstruction* got, HBasicBlock* successor);
