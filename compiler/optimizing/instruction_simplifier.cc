64225ac0bb9bf9663a408327e86e25884dc34afd
Mark Mendell <mark.p.mendell@intel.com>
2015-11-17 11:16:56 -0500

    Simplify boolean condition compared to 0
    CaffeineMarkRR Logic has some boolean flipping which can be helped by
    some simplification.
    
    Simplify non-FP (A COND_OP B) != 0 to A OPPOSITE_COND_OP B.
    This is better than the original code, which would use a HBooleanNot
    after the condition.
    
    Move GetOppositeCondition to nodes.h/nodes.cc to share with Boolean
    Simplification.
    
    Change-Id: I34ded7758836e375de0d6fdba9239d2d451928d0
    Signed-off-by: Mark Mendell <mark.p.mendell@intel.com>

--- compiler/optimizing/instruction_simplifier.cc
+++ compiler/optimizing/instruction_simplifier.cc
@@ -372,9 +372,22 @@ void InstructionSimplifierVisitor::VisitEqual(HEqual* equal) {
         block->RemoveInstruction(equal);
         RecordSimplification();
       } else if (input_const->AsIntConstant()->IsZero()) {
-        // Replace (bool_value == false) with !bool_value
-        block->ReplaceAndRemoveInstructionWith(
-            equal, new (block->GetGraph()->GetArena()) HBooleanNot(input_value));
+        // Can we rewrite a condition?
+        HInstruction* replacement;
+        bool try_remove_input_value = false;
+        if (input_value->IsCondition() &&
+            !Primitive::IsFloatingPointType(input_value->InputAt(0)->GetType())) {
+          // Replace non-FP (a OP b) != 0 with a OPPOSITE_OP b
+          replacement = GetOppositeCondition(input_value);
+          try_remove_input_value = true;
+        } else {
+          // Replace (bool_value == false) with !bool_value
+          replacement = new (block->GetGraph()->GetArena()) HBooleanNot(input_value);
+        }
+        block->ReplaceAndRemoveInstructionWith(equal, replacement);
+        if (try_remove_input_value && !input_value->HasUses()) {
+          input_value->GetBlock()->RemoveInstruction(input_value);
+        }
         RecordSimplification();
       } else {
         // Replace (bool_value == integer_not_zero_nor_one_constant) with false
