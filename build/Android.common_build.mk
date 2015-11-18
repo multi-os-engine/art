c4e03fb6d0f4a3acabe81aaaaae972f29bf9c40c
Mingyao Yang <mingyao@google.com>
2015-11-18 15:20:37 -0800

    Fix an assert in lse.
    Wrong assert. Just deleted it.
    
    Change-Id: I6f051609d87b2c6da081ee63f783ce99462f8a09

--- build/Android.common_build.mk
+++ build/Android.common_build.mk
@@ -35,7 +35,7 @@ ART_BUILD_HOST_NDEBUG ?= true
 ART_BUILD_HOST_DEBUG ?= true
 
 # Set this to change what opt level Art is built at.
-ART_DEBUG_OPT_FLAG ?= -O2
+ART_DEBUG_OPT_FLAG ?= -O0
 ART_NDEBUG_OPT_FLAG ?= -O3
 
 # Enable the static builds only for checkbuilds.
