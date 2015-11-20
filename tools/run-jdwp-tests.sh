2804fa62003c6f20e20de52ce02c4b87ff408b75
Hiroshi Yamauchi <yamauchi@google.com>
2015-11-19 16:24:31 -0800

    Kill stale dalvikvm processes at the end of the jdwp test.
    Bug: 25800335
    Change-Id: I938437d6e1ecd362b30a1dfc4db7a25538cf3211

--- tools/run-jdwp-tests.sh
+++ tools/run-jdwp-tests.sh
@@ -43,9 +43,11 @@ image="-Ximage:/data/art-test/core-jit.art"
 vm_args=""
 # By default, we run the whole JDWP test suite.
 test="org.apache.harmony.jpda.tests.share.AllTests"
+host="no"
 
 while true; do
   if [[ "$1" == "--mode=host" ]]; then
+    host="yes"
     # Specify bash explicitly since the art script cannot, since it has to run on the device
     # with mksh.
     art="bash ${OUT_DIR-out}/host/linux-x86/bin/art"
@@ -118,3 +120,17 @@ vogar $vm_command \
       --classpath $test_jar \
       --vm-arg -Xcompiler-option --vm-arg --debuggable \
       $test
+
+vogar_exit_status=$?
+
+echo "Killing stalled dalvikvm processes..."
+if [[ $host == "yes" ]]; then
+  processes=$(ps u| grep dalvikvm | awk '{print $1}')
+  for pid in $processes; do kill -9 $pid; done
+else
+  processes=$(adb shell "ps" | grep dalvikvm | awk '{print $2}')
+  for pid in $processes; do adb shell kill -9 $pid; done
+fi
+echo "Done."
+
+exit $vogar_exit_status
