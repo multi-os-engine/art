766cec8a1c442e84b04d53bf03cecd0508c358fa
Goran Jakovljevic <Goran.Jakovljevic@imgtec.com>
2015-11-20 17:22:57 +0100

    MIPS32: Record missing implicit null checks
    ImplicitNullChecks are recorded for HandleFieldGet and HandleFieldSet
    instructions for longs after accessing first word.
    
    This fixes following CTS tests:
    * dot.junit.opcodes.iget_wide.JUnit_Test_iget_wide#testE2
    * dot.junit.opcodes.iput_wide.JUnit_Test_iput_wide#testE2
    
    Change-Id: I0a93370d4a78d36805a8d5a6c304ca0329da2363

---
+++ test/551-implicit-null-checks/src/Main.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class Main {
+
+  public static void main(String args[]) {
+    try {
+      testGetLong();
+    } catch (NullPointerException ex) {
+      // good
+    }
+    try {
+      testPutLong();
+    } catch (NullPointerException ex) {
+      // good
+    }
+  }
+
+  public static void testGetLong() {
+    TestCase tc = new TestCase();
+    long result = tc.get();
+    System.out.println("Shouldn't see this! result = " + result);
+  }
+
+  public static void testPutLong() {
+    TestCase tc = new TestCase();
+    tc.put(778899112233L);
+    System.out.println("Shouldn't see this!");
+  }
+}
