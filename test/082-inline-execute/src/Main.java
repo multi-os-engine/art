f21d78e492e322001f48c4f3c1fbffe011b1172a
Chris Larsen <chris.larsen@imgtec.com>
2015-11-20 09:07:09 -0800

    MIPS32: Implement intrinsics from java.lang.Math:
    - abs(double)           - abs(float)            - abs(int)
    - abs(long)             - max(double, double)   - max(float, float)
    - max(int, int)         - max(long, long)       - min(double, double)
    - min(float, float)     - min(int, int)         - min(long, long)
    - sqrt(double)
    
    The math intrinsics:
    
    - ceil(double)          - floor(double)         - rint(double)
    - round(double)         - round(float)
    
    aren't implemented because they require instructions which only exist
    for MIPS64, or for MIPS32r6.
    
    Change-Id: I943be3592b52a423fcb7ac40f46f38a5e2a58c50

--- test/082-inline-execute/src/Main.java
+++ test/082-inline-execute/src/Main.java
@@ -825,6 +825,8 @@ public class Main {
     Assert.assertEquals(Math.round(-2.5f), -2);
     Assert.assertEquals(Math.round(-2.9f), -3);
     Assert.assertEquals(Math.round(-3.0f), -3);
+    // 0.4999999701976776123046875
+    Assert.assertEquals(Math.round(Float.intBitsToFloat(0x3EFFFFFF)), (int)+0.0f);
     Assert.assertEquals(Math.round(Float.NaN), (int)+0.0f);
     Assert.assertEquals(Math.round(Integer.MAX_VALUE + 1.0f), Integer.MAX_VALUE);
     Assert.assertEquals(Math.round(Integer.MIN_VALUE - 1.0f), Integer.MIN_VALUE);
@@ -1056,6 +1058,8 @@ public class Main {
     Assert.assertEquals(StrictMath.round(-2.5f), -2);
     Assert.assertEquals(StrictMath.round(-2.9f), -3);
     Assert.assertEquals(StrictMath.round(-3.0f), -3);
+    // 0.4999999701976776123046875
+    Assert.assertEquals(Math.round(Float.intBitsToFloat(0x3EFFFFFF)), (int)+0.0f);
     Assert.assertEquals(StrictMath.round(Float.NaN), (int)+0.0f);
     Assert.assertEquals(StrictMath.round(Integer.MAX_VALUE + 1.0f), Integer.MAX_VALUE);
     Assert.assertEquals(StrictMath.round(Integer.MIN_VALUE - 1.0f), Integer.MIN_VALUE);
