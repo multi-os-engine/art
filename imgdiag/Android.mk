2606c5b3a975fa019191bbbac9009c26ab80e287
Roland Levillain <rpl@google.com>
2015-12-08 18:31:37 +0000

    Fix imgdiag build rules.
    Adjust build rules to avoid building a 64-bit imgdiag
    executable on host when HOST_PREFER_32_BIT is true.
    
    Bug: 26051370
    Change-Id: I8f3b3c89fb8da0c37a1ffb21b6d835e7ede1e71a

--- imgdiag/Android.mk
+++ imgdiag/Android.mk
@@ -21,8 +21,42 @@ include art/build/Android.executable.mk
 IMGDIAG_SRC_FILES := \
 	imgdiag.cc
 
+# TODO: Remove this when the framework (installd) supports pushing the
+# right instruction-set parameter for the primary architecture.
+ifneq ($(filter ro.zygote=zygote64,$(PRODUCT_DEFAULT_PROPERTY_OVERRIDES)),)
+  imgdiag_target_arch := 64
+else
+  imgdiag_target_arch := 32
+endif
+
+ifeq ($(HOST_PREFER_32_BIT),true)
+  # We need to explicitly restrict the host arch to 32-bit only, as
+  # giving 'both' would make build-art-executable generate a build
+  # rule for a 64-bit imgdiag executable too.
+  imgdiag_host_arch := 32
+else
+  imgdiag_host_arch := both
+endif
+
 # Note that this tool needs to be built for both 32-bit and 64-bit since it requires
 # that the image it's analyzing be the same ISA as the runtime ISA.
 
-# Build variants {target,host} x {debug,ndebug} x {32,64}
-$(eval $(call build-art-multi-executable,imgdiag,$(IMGDIAG_SRC_FILES),libart-compiler libbacktrace,libcutils,libziparchive-host,art/compiler,both))
+ifeq ($(ART_BUILD_TARGET_NDEBUG),true)
+  $(eval $(call build-art-executable,imgdiag,$(IMGDIAG_SRC_FILES),libart-compiler libbacktrace libcutils,art/compiler,target,ndebug,$(imgdiag_target_arch)))
+endif
+
+ifeq ($(ART_BUILD_TARGET_DEBUG),true)
+  $(eval $(call build-art-executable,imgdiag,$(IMGDIAG_SRC_FILES),libartd-compiler libbacktrace libcutils,art/compiler,target,debug,$(imgdiag_target_arch)))
+endif
+
+ifeq ($(ART_BUILD_HOST_NDEBUG),true)
+  $(eval $(call build-art-executable,imgdiag,$(IMGDIAG_SRC_FILES),libart-compiler libziparchive-host libbacktrace,art/compiler,host,ndebug,$(imgdiag_host_arch)))
+endif
+
+ifeq ($(ART_BUILD_HOST_DEBUG),true)
+  $(eval $(call build-art-executable,imgdiag,$(IMGDIAG_SRC_FILES),libartd-compiler libziparchive-host libbacktrace,art/compiler,host,debug,$(imgdiag_host_arch)))
+endif
+
+# Clear locals now they've served their purpose.
+imgdiag_target_arch :=
+imgdiag_host_arch :=
