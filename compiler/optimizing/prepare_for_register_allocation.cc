55af5d5d41b02fadb0d1c788da8c987ac9d9e560
David Brazdil <dbrazdil@google.com>
2015-12-14 11:44:01 +0000

    ART: Implement HSelect
    This patch adds a new HIR instruction to Optimizing. HSelect returns
    one of two inputs based on the outcome of a condition.
    
    This is only initial implementation which:
     - defines the new instruction,
     - repurposes BooleanSimplifier to emit it,
     - extends InstructionSimplifier to statically resolve it,
     - updates existing code and tests accordingly.
    
    Code generators currently emit fallback if/then/else code and will be
    updated in follow-up CLs to use platform-specific conditional moves
    when possible.
    
    Change-Id: Ib61b17146487ebe6b55350c2b589f0b971dcaaee

--- compiler/optimizing/prepare_for_register_allocation.cc
+++ compiler/optimizing/prepare_for_register_allocation.cc
@@ -133,7 +133,9 @@ void PrepareForRegisterAllocation::VisitCondition(HCondition* condition) {
     needs_materialization = true;
   } else {
     HInstruction* user = condition->GetUses().GetFirst()->GetUser();
-    if (!user->IsIf() && !user->IsDeoptimize()) {
+    if (!user->IsIf() &&
+        !user->IsDeoptimize() &&
+        !(user->IsSelect() && user->AsSelect()->GetCondition() == condition)) {
       needs_materialization = true;
     } else {
       // TODO: if there is no intervening instructions with side-effect between this condition
