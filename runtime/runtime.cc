d92eaf7935a062a27bf28a5a3a18b7d2289c80f2
Calin Juravle <calin@google.com>
2015-12-18 14:10:00 +0200

    Assume the profile file was created before saving.
    bug: 26080105
    Change-Id: I9969a4abd8533614922076551fcbae2cdf695525

--- runtime/runtime.cc
+++ runtime/runtime.cc
@@ -1631,9 +1631,20 @@ void Runtime::SetCalleeSaveMethod(ArtMethod* method, CalleeSaveType type) {
 void Runtime::RegisterAppInfo(const std::vector<std::string>& code_paths,
                               const std::string& profile_output_filename) {
   LOG(INFO) << "Register app with " << profile_output_filename_ << " " << Join(code_paths, ':');
-  DCHECK(!profile_output_filename.empty());
+
+  if (profile_output_filename.empty()) {
+    LOG(WARNING) << "JIT profile information will not be recorded: profile filename is empty.";
+  }
+  if (!FileExists(profile_output_filename)) {
+    LOG(WARNING) << "JIT profile information will not be recorded: profile file does not exits.";
+  }
+  if (code_paths.empty()) {
+    LOG(WARNING) << "JIT profile information will not be recorded: code paths is empty.";
+  }
+
   profile_output_filename_ = profile_output_filename;
-  if (jit_.get() != nullptr && !profile_output_filename.empty() && !code_paths.empty()) {
+  if (jit_.get() != nullptr && !profile_output_filename.empty() && !code_paths.empty() &&
+      FileExists(profile_output_filename)) {
     jit_->StartProfileSaver(profile_output_filename, code_paths);
   }
 }
