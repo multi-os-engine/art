2f46dd9a13b82c1a0d232ef654e06a2ca6fb4b41
Calin Juravle <calin@google.com>
2016-01-05 14:29:29 +0000

    Improve profile processing
    - allow file descriptors in addition to file names for profiles
    - fix some minor issues (wrong comparison signs, unhandled errors)
    - added gtests for profile_compilation_info, profile_assistant
    and compiler_driver
    
    Bug: 26080105
    Change-Id: I136039fa1f25858399000049e48b01eafae54eb1

--- runtime/base/scoped_flock.h
+++ runtime/base/scoped_flock.h
@@ -33,9 +33,12 @@ class ScopedFlock {
   // at filename, and blocks until it can do so.
   //
   // Returns true if the lock could be acquired, or false if an error
-  // occurred. It is an error if the file does not exist, or if its inode
-  // changed (usually due to a new file being created at the same path)
-  // between attempts to lock it.
+  // occurred. It is an error if if its inode changed (usually due to a new file
+  // being created at the same path) between attempts to lock it.
+  //
+  // The file is opened with the provided flags.
+  bool Init(const char* filename, int flags, bool block, std::string* error_msg);
+  // Calls Init(filename, O_CREAT | O_RDWR, true, errror_msg)
   bool Init(const char* filename, std::string* error_msg);
   // Attempt to acquire an exclusive file lock (see flock(2)) on 'file'.
   // Returns true if the lock could be acquired or false if an error
@@ -43,7 +46,7 @@ class ScopedFlock {
   bool Init(File* file, std::string* error_msg);
 
   // Returns the (locked) file associated with this instance.
-  File* GetFile();
+  File* GetFile() const;
 
   // Returns whether a file is held.
   bool HasFile();
