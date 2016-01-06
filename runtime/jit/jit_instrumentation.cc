15f21f9c93b77b8723a323d690d5f8a547dca310
Siva Chandra <sivachandra@google.com>
2016-01-05 17:43:17 -0800

    Add option to never interpret.
    Change-Id: Ib6d6170fa60c77c2e6a8844964f14fa0de4c9e7b

--- runtime/jit/jit_instrumentation.cc
+++ runtime/jit/jit_instrumentation.cc
@@ -162,6 +162,17 @@ void JitInstrumentationListener::MethodEntered(Thread* thread,
                                                mirror::Object* /*this_object*/,
                                                ArtMethod* method,
                                                uint32_t /*dex_pc*/) {
+  if (UNLIKELY(instrumentation_cache_->HotMethodThreshold() == 0)) {
+    bool success = ProfilingInfo::Create(thread, method, /* retry_allocation */ true);
+    if (success) {
+      VLOG(jit) << "Start profiling " << PrettyMethod(method);
+    }
+
+    JitCompileTask compile_task(method, JitCompileTask::kCompile);
+    compile_task.Run(thread);
+    return;
+  }
+
   instrumentation_cache_->AddSamples(thread, method, 1);
 }
 
