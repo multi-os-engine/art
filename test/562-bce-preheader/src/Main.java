fac5228207bb4efb6a0e8262ca403cf73f5f10f6
Aart Bik <ajcbik@google.com>
2016-01-12 14:12:47 -0800

    Fixed bug with hoisting/deopting in taken-block instead of preheader. With a fail-before pass-after regression test.
    Rationale:
    Hoisting and deopting should be done in the taken-block for safety
    of the evaluation *unless* the code was in the original loop header,
    and thus always taken.
    
    bug: 26506884
    Change-Id: I1cf121292559fd2570ad67587ec3bc8e8a85a92a

---
+++ test/562-bce-preheader/src/Main.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class Main {
+
+  /**
+   * Method with an outer loop countable loop and an inner do-while loop.
+   * Since all work is done in the loop header, any invariant hoisting
+   * and deopting should be done in a loop header proper, not the true-block
+   * of the newly generated taken-test after dynamice BCE.
+   */
+  public static int doit(int[][] x, int j) {
+    float f = 0;
+    int acc = 0;
+    for (int i = 0; i < 2; i++) {
+      do {
+        f++;  // A bit noise to avoid early hoisting.
+        acc += x[i][i];
+      } while (++j < i);
+    }
+    return acc;
+  }
+
+  public static void main(String args[]) {
+    int[][] x = new int[2][2];
+    int y;
+
+    x[0][0] = 1;
+    x[1][1] = 2;
+
+    expectEquals(8, doit(x, -6));
+    expectEquals(7, doit(x, -5));
+    expectEquals(6, doit(x, -4));
+    expectEquals(5, doit(x, -3));
+    expectEquals(4, doit(x, -2));
+    expectEquals(3, doit(x, -1));
+    expectEquals(3, doit(x,  0));
+    expectEquals(3, doit(x,  1));
+    expectEquals(3, doit(x, 22));
+
+    System.out.println("passed");
+  }
+
+  private static void expectEquals(int expected, int result) {
+    if (expected != result) {
+      throw new Error("Expected: " + expected + ", found: " + result);
+    }
+  }
+}
