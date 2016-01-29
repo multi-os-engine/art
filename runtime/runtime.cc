d039524d61f0ee2fcfc67462274b2cd683c0605c
Nicolas Geoffray <ngeoffray@google.com>
2016-01-29 12:40:13 +0000

    Implement on-stack replacement for arm/arm64/x86/x86_64.
    High-level overview:
    - osr_method_threshold is used to know when to compile a method
      in osr mode (-> treat all loops as irreducible).
    - branch instructions in the compiler query whether they can
      jump to an osr method.
    - An osr entry point is found through the stack maps: if a stack
      map is duplicated in the CodeInfo, it is an osr entry point.
    
    Change-Id: Ifb39338cd281e2c7eccce67f4e18d46428be71e4

--- runtime/runtime.cc
+++ runtime/runtime.cc
@@ -1883,7 +1883,8 @@ void Runtime::CreateJit() {
   if (jit_.get() != nullptr) {
     compiler_callbacks_ = jit_->GetCompilerCallbacks();
     jit_->CreateInstrumentationCache(jit_options_->GetCompileThreshold(),
-                                     jit_options_->GetWarmupThreshold());
+                                     jit_options_->GetWarmupThreshold(),
+                                     jit_options_->GetOsrThreshold());
     jit_->CreateThreadPool();
 
     // Notify native debugger about the classes already loaded before the creation of the jit.
---
+++ test/570-checker-osr/src/Main.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class Main {
+  public static void main(String[] args) {
+    System.loadLibrary(args[0]);
+    if ($noinline$returnInt() != 53) {
+      throw new Error("Unexpected return value");
+    }
+    if ($noinline$returnFloat() != 42.2f) {
+      throw new Error("Unexpected return value");
+    }
+    if ($noinline$returnDouble() != 53.3) {
+      throw new Error("Unexpected return value");
+    }
+  }
+
+  public static int $noinline$returnInt() {
+    if (doThrow) {
+      throw new Error("");
+    }
+    int i = 0;
+    for (; i < 100000000; ++i) {
+    }
+    while (!ensureInOsrCode()) {}
+    System.out.println(i);
+    return 53;
+  }
+
+  public static float $noinline$returnFloat() {
+    if (doThrow) {
+      throw new Error("");
+    }
+    int i = 0;
+    for (; i < 200000000; ++i) {
+    }
+    while (!ensureInOsrCode()) {}
+    System.out.println(i);
+    return 42.2f;
+  }
+
+  public static double $noinline$returnDouble() {
+    if (doThrow) {
+      throw new Error("");
+    }
+    int i = 0;
+    for (; i < 300000000; ++i) {
+    }
+    while (!ensureInOsrCode()) {}
+    System.out.println(i);
+    return 53.3;
+  }
+
+  public static native boolean ensureInOsrCode();
+
+  public static boolean doThrow = false;
+}
