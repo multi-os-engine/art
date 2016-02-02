7b91525f9f8ac4bd98f11eb82c092c93b016fbdf
Aart Bik <ajcbik@google.com>
2016-02-01 18:57:58 -0800

    Implemented x86 intrinsics.
    Change-Id: I6e2ee0311784e5e336847346f7f3c4faef4fd17e

--- compiler/optimizing/code_generator_x86.h
+++ compiler/optimizing/code_generator_x86.h
@@ -450,6 +450,9 @@ class CodeGeneratorX86 : public CodeGenerator {
   Address LiteralInt32Address(int32_t v, Register reg);
   Address LiteralInt64Address(int64_t v, Register reg);
 
+  // Load a 32-bit value into a register in the most efficient manner.
+  void Load32BitValue(Register dest, int32_t value);
+
   Address LiteralCaseTable(HX86PackedSwitch* switch_instr, Register reg, Register value);
 
   void Finalize(CodeAllocator* allocator) OVERRIDE;
