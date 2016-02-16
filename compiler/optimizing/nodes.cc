c57413ef22319dfbbadf9a8abc4c693f9f7f8a44
Roland Levillain <rpl@google.com>
2016-02-16 12:21:02 +0000

    Extend constant folding to float and double operations.
    Change-Id: I2837064b2ceea587bc171fc520507f13355292c6

--- compiler/optimizing/nodes.cc
+++ compiler/optimizing/nodes.cc
@@ -15,6 +15,8 @@
  */
 #include "nodes.h"
 
+#include <cfloat>
+
 #include "code_generator.h"
 #include "common_dominator.h"
 #include "ssa_builder.h"
@@ -1159,6 +1161,15 @@ HConstant* HUnaryOperation::TryStaticEvaluation() const {
     return Evaluate(GetInput()->AsIntConstant());
   } else if (GetInput()->IsLongConstant()) {
     return Evaluate(GetInput()->AsLongConstant());
+  } else if (FLT_EVAL_METHOD == 0) {
+    // All floating-point operations and constants evaluate in the
+    // range and precision of the type used (i.e., 32-bit float,
+    // 64-bit double).
+    if (GetInput()->IsFloatConstant()) {
+      return Evaluate(GetInput()->AsFloatConstant());
+    } else if (GetInput()->IsDoubleConstant()) {
+      return Evaluate(GetInput()->AsDoubleConstant());
+    }
   }
   return nullptr;
 }
@@ -1178,6 +1189,15 @@ HConstant* HBinaryOperation::TryStaticEvaluation() const {
     }
   } else if (GetLeft()->IsNullConstant() && GetRight()->IsNullConstant()) {
     return Evaluate(GetLeft()->AsNullConstant(), GetRight()->AsNullConstant());
+  } else if (FLT_EVAL_METHOD == 0) {
+    // All floating-point operations and constants evaluate in the
+    // range and precision of the type used (i.e., 32-bit float,
+    // 64-bit double).
+    if (GetLeft()->IsFloatConstant() && GetRight()->IsFloatConstant()) {
+      return Evaluate(GetLeft()->AsFloatConstant(), GetRight()->AsFloatConstant());
+    } else if (GetLeft()->IsDoubleConstant() && GetRight()->IsDoubleConstant()) {
+      return Evaluate(GetLeft()->AsDoubleConstant(), GetRight()->AsDoubleConstant());
+    }
   }
   return nullptr;
 }
@@ -1202,6 +1222,20 @@ HInstruction* HBinaryOperation::GetLeastConstantLeft() const {
     return GetRight();
   } else {
     return GetLeft();
+  }
+}
+
+std::ostream& operator<<(std::ostream& os, const ComparisonBias& rhs) {
+  switch (rhs) {
+    case ComparisonBias::kNoBias:
+      return os << "no_bias";
+    case ComparisonBias::kGtBias:
+      return os << "gt_bias";
+    case ComparisonBias::kLtBias:
+      return os << "lt_bias";
+    default:
+      LOG(FATAL) << "Unknown ComparisonBias: " << static_cast<int>(rhs);
+      UNREACHABLE();
   }
 }
 
