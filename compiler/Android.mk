0bda7d61db9f3284fffea9d1a0d6e98aa7a78798
Vladimir Marko <vmarko@google.com>
2016-02-22 10:39:50 +0000

    Optimizing: Improve const-string code generation.
    For strings in the boot image, use either direct pointers
    or pc-relative addresses. For other strings, use PC-relative
    access to the dex cache arrays for AOT and direct address of
    the string's dex cache slot for JIT.
    
    For aosp_flounder-userdebug:
      - 32-bit boot.oat: -692KiB (-0.9%)
      - 64-bit boot.oat: -948KiB (-1.1%)
      - 32-bit dalvik cache total: -900KiB (-0.9%)
      - 64-bit dalvik cache total: -3672KiB (-1.5%)
        (contains more files than the 32-bit dalvik cache)
    For aosp_flounder-userdebug forced to compile PIC:
      - 32-bit boot.oat: -380KiB (-0.5%)
      - 64-bit boot.oat: -928KiB (-1.0%)
      - 32-bit dalvik cache total: -468KiB (-0.4%)
      - 64-bit dalvik cache total: -1928KiB (-0.8%)
        (contains more files than the 32-bit dalvik cache)
    
    Bug: 26884697
    Change-Id: Iec7266ce67e6fedc107be78fab2e742a8dab2696

--- compiler/Android.mk
+++ compiler/Android.mk
@@ -200,6 +200,7 @@ LIBART_COMPILER_SRC_FILES_x86_64 := \
 LIBART_COMPILER_CFLAGS :=
 
 LIBART_COMPILER_ENUM_OPERATOR_OUT_HEADER_FILES := \
+  compiled_method.h \
   dex/quick/resource_mask.h \
   dex/compiler_enums.h \
   dex/dex_to_dex_compiler.h \
