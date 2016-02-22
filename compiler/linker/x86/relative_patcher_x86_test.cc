4370d7044d14d5f3e55c1457a01dcfe9d9b35201
Vladimir Marko <vmarko@google.com>
2016-02-22 10:39:50 +0000

    Optimizing: Improve const-string code generation.
    For strings in the boot image, use either direct pointers
    or pc-relative addresses. For other strings, use PC-relative
    access to the dex cache arrays for AOT and direct address of
    the string's dex cache slot for JIT.
    
    For aosp_flounder-userdebug:
      - 32-bit boot.oat: -692KiB (0.9%)
      - 64-bit boot.oat: -948KiB (1.1%)
      - 32-bit dalvik cache total: -900KiB (0.9%)
      - 64-bit dalvik cache total: -3672KiB (1.5%)
        (contains more files than the 32-bit dalvik cache)
    For aosp_flounder-userdebug forced to compile PIC:
      - 32-bit boot.oat: -380KiB (0.5%)
      - 64-bit boot.oat: -928KiB (1.0%)
      - 32-bit dalvik cache total: -468KiB (0.4%)
      - 64-bit dalvik cache total: -1928KiB (0.8%)
        (contains more files than the 32-bit dalvik cache)
    
    Bug: 26884697
    Change-Id: Iec7266ce67e6fedc107be78fab2e742a8dab2696

--- compiler/linker/x86/relative_patcher_x86_test.cc
+++ compiler/linker/x86/relative_patcher_x86_test.cc
@@ -131,5 +131,35 @@ TEST_F(X86RelativePatcherTest, DexCacheReference) {
   EXPECT_TRUE(CheckLinkedMethod(MethodRef(1u), ArrayRef<const uint8_t>(expected_code)));
 }
 
+TEST_F(X86RelativePatcherTest, StringReference) {
+  constexpr uint32_t kStringIndex = 1u;
+  constexpr uint32_t kStringOffset = 0x12345678;
+  string_index_to_offset_map_.Put(kStringIndex, kStringOffset);
+  static const uint8_t raw_code[] = {
+      0xe8, 0x00, 0x00, 0x00, 0x00,         // call +0
+      0x5b,                                 // pop ebx
+      0x8d, 0x83, 0x00, 0x01, 0x00, 0x00,   // lea eax, [ebx + 256 (kDummy32BitValue)]
+  };
+  constexpr uint32_t anchor_offset = 5u;  // After call +0.
+  ArrayRef<const uint8_t> code(raw_code);
+  LinkerPatch patches[] = {
+      LinkerPatch::RelativeStringPatch(code.size() - 4u, nullptr, anchor_offset, kStringIndex),
+  };
+  AddCompiledMethod(MethodRef(1u), code, ArrayRef<const LinkerPatch>(patches));
+  Link();
+
+  auto result = method_offset_map_.FindMethodOffset(MethodRef(1u));
+  ASSERT_TRUE(result.first);
+  uint32_t diff = kStringOffset - (result.second + anchor_offset);
+  static const uint8_t expected_code[] = {
+      0xe8, 0x00, 0x00, 0x00, 0x00,         // call +0
+      0x5b,                                 // pop ebx
+      0x8d, 0x83,                           // lea eax, [ebx + diff]
+      static_cast<uint8_t>(diff), static_cast<uint8_t>(diff >> 8),
+      static_cast<uint8_t>(diff >> 16), static_cast<uint8_t>(diff >> 24)
+  };
+  EXPECT_TRUE(CheckLinkedMethod(MethodRef(1u), ArrayRef<const uint8_t>(expected_code)));
+}
+
 }  // namespace linker
 }  // namespace art
