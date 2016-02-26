dd78e656a13cdfc91b44e3e88c8e8b7b75bacfbf
Serguei Katkov <serguei.i.katkov@intel.com>
2016-02-26 13:00:40 +0600

    ART: Enable JitProfiling for x86_64 Mterp
    Adds branch profiling and enables for x86_64.
    Support interpreter switching in x86_64 mterp.
    
    Change-Id: I0cb9fcf3e2a01e411d84efc78449e86c10e6bcac
    Signed-off-by: Serguei Katkov <serguei.i.katkov@intel.com>

--- runtime/interpreter/mterp/mterp.cc
+++ runtime/interpreter/mterp/mterp.cc
@@ -149,7 +149,8 @@ extern "C" bool MterpShouldSwitchInterpreters()
       Runtime::Current()->GetInstrumentation();
   bool unhandled_instrumentation;
   // TODO: enable for other targets after more extensive testing.
-  if ((kRuntimeISA == kArm64) || (kRuntimeISA == kArm) || (kRuntimeISA == kX86)) {
+  if ((kRuntimeISA == kArm64) || (kRuntimeISA == kArm) ||
+      (kRuntimeISA == kX86) || (kRuntimeISA == kX86_64)) {
     unhandled_instrumentation = instrumentation->NonJitProfilingActive();
   } else {
     unhandled_instrumentation = instrumentation->IsActive();
