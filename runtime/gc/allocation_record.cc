0b2655ba69bf008e979398e2f53b28370c52d166
Mathieu Chartier <mathieuc@google.com>
2016-03-02 12:52:37 -0800

    Delete alloc tracking map outside of critical section
    There can be lock order violations otherwise due to runtime shutdown
    lock that may get acquired in the condition variable destructor.
    
    Change-Id: I23cb2dfe241f5cc6c42bf6766e89042cf06069b6

--- runtime/gc/allocation_record.cc
+++ runtime/gc/allocation_record.cc
@@ -245,6 +245,7 @@ void AllocRecordObjectMap::SetAllocTrackingEnabled(bool enable) {
       heap->SetAllocTrackingEnabled(true);
     }
   } else {
+    std::unique_ptr<AllocRecordObjectMap> map;
     {
       MutexLock mu(self, *Locks::alloc_tracker_lock_);
       if (!heap->IsAllocTrackingEnabled()) {
@@ -252,8 +253,11 @@ void AllocRecordObjectMap::SetAllocTrackingEnabled(bool enable) {
       }
       heap->SetAllocTrackingEnabled(false);
       LOG(INFO) << "Disabling alloc tracker";
-      heap->SetAllocationRecords(nullptr);
+      map = heap->ReleaseAllocationRecords();
     }
+    // Delete outside of the critical section to avoid possible lock violations like the runtime
+    // shutdown lock.
+    map.reset();
     // If an allocation comes in before we uninstrument, we will safely drop it on the floor.
     Runtime::Current()->GetInstrumentation()->UninstrumentQuickAllocEntryPoints();
   }
