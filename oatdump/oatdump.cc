5be442b7b3ba717d2832090e21a889b6598af34a
Nicolas Geoffray <ngeoffray@google.com>
2016-03-13 22:22:31 +0000

    Add an ImtConflictTable to better resolve IMT conflicts.
    - Attach a ImtConflictTable to conflict runtime ArtMethod.
    - Initially 0, a new one will be created at the first hit of
      the conflict method.
    - If the assembly code does not find a target method in the table,
      we will create a new one again, copying the data from the previous
      table and adding the new mapping.
    
    Implemented for arm/arm64/x86/x64.
    
    bug:27556801
    bug:24769046
    
    Change-Id: Ie74d1c77cf73d451a1142bdc5e3683f9f84bb4e7

--- oatdump/oatdump.cc
+++ oatdump/oatdump.cc
@@ -1967,9 +1967,13 @@ class ImageDumper {
             method->GetEntryPointFromQuickCompiledCodePtrSize(image_pointer_size)) {
         indent_os << StringPrintf("OAT CODE: %p\n", quick_oat_code_begin);
       }
-    } else if (method->IsAbstract() || method->IsCalleeSaveMethod() ||
-      method->IsResolutionMethod() || method->IsImtConflictMethod() ||
-      method->IsImtUnimplementedMethod() || method->IsClassInitializer()) {
+    } else if (method->IsAbstract() ||
+               method->IsCalleeSaveMethod() ||
+               method->IsResolutionMethod() ||
+               (method == Runtime::Current()->GetImtConflictMethod()) ||
+               method->IsImtUnimplementedMethod() ||
+               method->IsClassInitializer()) {
+      // Don't print information for these.
     } else {
       const DexFile::CodeItem* code_item = method->GetCodeItem();
       size_t dex_instruction_bytes = code_item->insns_size_in_code_units_ * 2;
