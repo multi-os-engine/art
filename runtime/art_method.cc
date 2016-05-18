b7418e4d54f3bfb859961b6ee88b34d7fb8d0258
Evgenii Stepanov <eugenis@google.com>
2016-05-18 14:50:38 -0700

    Stack overflow handling for SafeStack.
    With SafeStack, the overflow can happen on either of the stacks.
    This change extends the overflow handling logic to the unsafe stack.
    
    Change-Id: If496af28f871e8fcb892850c528fd3031a581360

--- runtime/art_method.cc
+++ runtime/art_method.cc
@@ -235,7 +235,7 @@ uint32_t ArtMethod::FindCatchBlock(Handle<mirror::Class> exception_type,
 
 void ArtMethod::Invoke(Thread* self, uint32_t* args, uint32_t args_size, JValue* result,
                        const char* shorty) {
-  if (UNLIKELY(__builtin_frame_address(0) < self->GetStackEnd())) {
+  if (self->IsBelowStackEnd()) {
     ThrowStackOverflowError(self);
     return;
   }
