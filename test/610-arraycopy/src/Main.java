23029a47688048fa4eb639a004eb52a6823b42bb
Nicolas Geoffray <ngeoffray@google.com>
2016-07-07 12:07:42 +0100

    Fix System.arraycopy when doing same array copying.
    We also need to check constant source < constant destination in
    case the two arrays are found to be the same at runtime.
    
    test:610-arraycopy
    
    Change-Id: Ida67993d472b0ba4056d9c21c68f6e5239421f7d

---
+++ test/610-arraycopy/src/Main.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class Main {
+  public static void main(String[] args) {
+    Object[] a = new Object[4];
+    for (int i = 0; i < 4; i++) {
+      a[i] = new Integer(i);
+    }
+    $noinline$callArrayCopy(a, a);
+
+    expectEquals(0, ((Integer)a[0]).intValue());
+    expectEquals(0, ((Integer)a[1]).intValue());
+    expectEquals(1, ((Integer)a[2]).intValue());
+    expectEquals(3, ((Integer)a[3]).intValue());
+  }
+
+  public static void expectEquals(int expected, int actual) {
+    if (expected != actual) {
+      throw new Error("Expected " + expected + ", got " + actual);
+    }
+  }
+
+  public static void $noinline$callArrayCopy(Object[] a, Object[] b) {
+    System.arraycopy(a, 0, b, 1, 2);
+    if (doThrow) { throw new Error(""); }
+  }
+
+  static boolean doThrow = false;
+}
