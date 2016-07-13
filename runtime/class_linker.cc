8e0aa45a21e413b18f4382e5539e38b4654ce569
Mathieu Chartier <mathieuc@google.com>
2016-07-13 09:53:35 -0700

    Use try lock to fix class resolution race
    There was some possible deadlocks related to EnsureResolved caused by
    acquiring an object lock.
    
    Scenario:
    Thread 1 acquires lock on obj1
    Thread 1 begins to resolve / initialize class1
    Thread 1 blocks since it sees that class1 is already being resolved and
    gets preempted before it can acquire the object lock on class1
    Thread 2 finishes resolving and initializing class1 and locks class1
    Thread 2 blocks attempting to lock obj1
    Thread 1 blocks attempting to lock class1
    Deadlock
    
    Fixed the deadlock by changing EnsureResolved to use a try lock for the
    unresolved case.
    
    Added a test.
    
    Test: Device boot, test-art-host, monitor_test
    
    Bug: 27417671
    Change-Id: Ic6e1c3ca6f45490cf8a7bf8e137dee71ac83ff64

--- runtime/class_linker.cc
+++ runtime/class_linker.cc
@@ -2179,20 +2179,21 @@ mirror::Class* ClassLinker::EnsureResolved(Thread* self,
   }
 
   // Wait for the class if it has not already been linked.
-  if (!klass->IsResolved() && !klass->IsErroneous()) {
+  while (!klass->IsResolved() && !klass->IsErroneous()) {
     StackHandleScope<1> hs(self);
     HandleWrapper<mirror::Class> h_class(hs.NewHandleWrapper(&klass));
-    ObjectLock<mirror::Class> lock(self, h_class);
-    // Check for circular dependencies between classes.
-    if (!h_class->IsResolved() && h_class->GetClinitThreadId() == self->GetTid()) {
-      ThrowClassCircularityError(h_class.Get());
-      mirror::Class::SetStatus(h_class, mirror::Class::kStatusError, self);
-      return nullptr;
+    ObjectTryLock<mirror::Class> lock(self, h_class);
+    // Can not use a monitor wait here since it may block when returning and deadlock if another
+    // thread has locked klass.
+    if (lock.Acquired()) {
+      // Check for circular dependencies between classes, the lock is required for SetStatus.
+      if (!h_class->IsResolved() && h_class->GetClinitThreadId() == self->GetTid()) {
+        ThrowClassCircularityError(h_class.Get());
+        mirror::Class::SetStatus(h_class, mirror::Class::kStatusError, self);
+        return nullptr;
+      }
     }
-    // Wait for the pending initialization to complete.
-    while (!h_class->IsResolved() && !h_class->IsErroneous()) {
-      lock.WaitIgnoringInterrupts();
-    }
+    sched_yield();
   }
 
   if (klass->IsErroneous()) {
