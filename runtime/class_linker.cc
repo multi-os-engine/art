9d0ab112fd38ce0baa1a15c2dc1508bf2d798146
Mathieu Chartier <mathieuc@google.com>
2016-07-13 09:53:35 -0700

    Use try lock to fix class resolution race
    There was some possible deadlocks related to EnsureResolved caused by
    acquiring an object lock.
    
    Scenario:
    Thread 1 acquires lock on obj1
    Thread 1 begins to resolve obj2
    Thread 2 finishes resolving obj2 and locks obj2
    Thread 2 blocks attempting to lock obj1
    Thread 1 blocks attempting to lock obj2 to finish resolving
    Deadlock
    
    Fixed the deadlock by changing EnsureResolved to use a try lock for the
    unresolved case.
    
    Added a test.
    
    Bug: 27417671
    Change-Id: Ic6e1c3ca6f45490cf8a7bf8e137dee71ac83ff64

--- runtime/class_linker.cc
+++ runtime/class_linker.cc
@@ -2177,20 +2177,23 @@ mirror::Class* ClassLinker::EnsureResolved(Thread* self,
   }
 
   // Wait for the class if it has not already been linked.
-  if (!klass->IsResolved() && !klass->IsErroneous()) {
+  while (!klass->IsResolved() && !klass->IsErroneous()) {
     StackHandleScope<1> hs(self);
     HandleWrapper<mirror::Class> h_class(hs.NewHandleWrapper(&klass));
-    ObjectLock<mirror::Class> lock(self, h_class);
-    // Check for circular dependencies between classes.
-    if (!h_class->IsResolved() && h_class->GetClinitThreadId() == self->GetTid()) {
-      ThrowClassCircularityError(h_class.Get());
-      mirror::Class::SetStatus(h_class, mirror::Class::kStatusError, self);
-      return nullptr;
+    ObjectTryLock<mirror::Class> lock(self, h_class);
+    if (lock.Acquired()) {
+      // Check for circular dependencies between classes.
+      if (!h_class->IsResolved() && h_class->GetClinitThreadId() == self->GetTid()) {
+        ThrowClassCircularityError(h_class.Get());
+        mirror::Class::SetStatus(h_class, mirror::Class::kStatusError, self);
+        return nullptr;
+      }
+      // Can't use WaitIgnoringInterrupts here since that will unlock and relock the object lock.
+      // This could deadlock when relocking.
     }
-    // Wait for the pending initialization to complete.
-    while (!h_class->IsResolved() && !h_class->IsErroneous()) {
-      lock.WaitIgnoringInterrupts();
-    }
+    // Wait for the pending initialization to complete, we do not have the trylock so we can not
+    // use a condition wait.
+    sched_yield();
   }
 
   if (klass->IsErroneous()) {
